7a6d7e06a094a4acdc186bbde0cd73e2112ee624
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2018-05-29 16:01:32 +0200

    Slår sammen utils og date utils

--- src/utils.js
+++ src/utils.js
@@ -15,3 +15,43 @@ export function getUrlParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
+export const months = ['januar', 'februar', 'mars', 'april', 'mai', 'juni', 'juli', 'august', 'september',
+    'oktober', 'november', 'desember'];
+
+const ISO_8601_DATE = /^\d{4}(-\d\d(-\d\d(T\d\d:\d\d(:\d\d)?(\.\d+)?(([+-]\d\d:\d\d)|Z)?)?)?)?$/i;
+
+export function isValidISOString(isoString) {
+    return ISO_8601_DATE.test(isoString);
+}
+
+export function toDate(isoString) {
+    if (!isValidISOString(isoString)) {
+        throw Error(`${isoString} is not a valid ISO 8601 date`);
+    }
+    return new Date(isoString);
+}
+
+export function toIsoDateString(year, month) {
+    const monthAsInteger = parseInt(month, 10);
+    const zeroPaddedMonth = monthAsInteger > 9 ? monthAsInteger : `0${monthAsInteger}`;
+    const isoDateString = `${year}-${zeroPaddedMonth}`;
+    if (!isValidISOString(isoDateString)) {
+        throw Error(`One or more args are invalid: year=${year} month=${month}`);
+    }
+    return isoDateString;
+}
+
+export function formatISOString(isoString, format = 'MMMM YYYY') {
+    if (isValidISOString(isoString)) {
+        const dt = isoString.split('-');
+        if (format === 'D. MMMM YYYY') {
+            const day = dt[2].split('T')[0];
+            return `${day}.${dt[1]}.${dt[0]}`;
+        } else if (format === 'MMMM YYYY') {
+            return `${months[dt[1] - 1]} ${dt[0]}`;
+        }
+        throw Error(`Unknown date format: ${format}`);
+    }
+    return '';
+}
+
