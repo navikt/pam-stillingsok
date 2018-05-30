eb1d0dcafee36c455abc1c0c22a36546765cdefd
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2018-05-30 10:57:28 +0200

    Browser router virket ikke i testmiljø, pga av context path /pam-stillingsok. Legger til context path i fasitProperties filen.

--- src/common/fasitProperties.js
+++ src/common/fasitProperties.js
@@ -4,3 +4,4 @@ export const LOGG_UT_API = __PAM_LOGOUT__;
 export const FORSIDE_LINK = __PAM_FORSIDE__;
 export const MIN_SIDE_LINK = __PAM_MINSIDE__;
 export const STILLING = __PAM_STILLING__;
+export const CONTEXT_PATH = __PAM_CONTEXT_PATH__;
