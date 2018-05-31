918d554a533446159c3e7fa0a918940ad1148202
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2018-05-31 14:33:53 +0200

    Update dependencies

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import { applyMiddleware, createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
