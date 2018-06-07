d20f9c5b20a7279238add2836d14fbd66c5f1efb
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2018-06-07 15:11:03 +0200

    Fixer linting

--- src/stilling/backToSearch/BackToSearch.js
+++ src/stilling/backToSearch/BackToSearch.js
@@ -3,18 +3,16 @@ import { Link } from 'react-router-dom';
 import Chevron from 'nav-frontend-chevron';
 import './BackToSearch.less';
 
-export default class BackToSearch extends React.Component {
-    render() {
-        return (
-            <div className="BackToSearch">
-                <Link
-                    to="/"
-                    className="knapp knapp--green knapp--standard"
-                >
-                    <Chevron type="venstre" className="BackToSearch__chevron" />
+export default function BackToSearch() {
+    return (
+        <div className="BackToSearch">
+            <Link
+                to="/"
+                className="knapp knapp--green knapp--standard"
+            >
+                <Chevron type="venstre" className="BackToSearch__chevron"/>
                 Tilbake til søk
-                </Link>
-            </div>
-        );
-    }
+            </Link>
+        </div>
+    );
 }
