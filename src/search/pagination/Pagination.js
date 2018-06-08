6b24286b706530e39afbff9794600427bf477b14
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2018-06-08 10:42:09 +0200

    Fikser layout ved ingen treff

--- src/search/pagination/Pagination.js
+++ src/search/pagination/Pagination.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Knapp } from 'nav-frontend-knapper';
 import { connect } from 'react-redux';
-import { LOAD_MORE, PAGE_SIZE } from '../searchReducer';
+import { LOAD_MORE } from '../searchReducer';
 import './Pagination.less';
 
 class Pagination extends React.Component {
@@ -11,35 +11,28 @@ class Pagination extends React.Component {
     };
 
     render() {
-        const { from, total, isLoadingMore } = this.props;
-        const hasMore = (from === undefined && total > PAGE_SIZE) || (from + PAGE_SIZE < total);
+        const { isLoadingMore } = this.props;
         return (
             <div className="Pagination">
-                {hasMore && (
-                    <Knapp
-                        disabled={isLoadingMore}
-                        spinner={isLoadingMore}
-                        className="Pagination__button"
-                        onClick={this.onLoadMoreClick}
-                    >
-                        Se flere
-                    </Knapp>
-                )}
+                <Knapp
+                    disabled={isLoadingMore}
+                    spinner={isLoadingMore}
+                    className="Pagination__button"
+                    onClick={this.onLoadMoreClick}
+                >
+                    Se flere
+                </Knapp>
             </div>
         );
     }
 }
 
 Pagination.propTypes = {
-    from: PropTypes.number.isRequired,
-    total: PropTypes.number.isRequired,
     isLoadingMore: PropTypes.bool.isRequired,
     loadMore: PropTypes.func.isRequired
 };
 
 const mapStateToProps = (state) => ({
-    from: state.search.from,
-    total: state.search.searchResult.total,
     isLoadingMore: state.search.isLoadingMore
 });
 
--- src/search/searchResults/SearchResults.js
+++ src/search/searchResults/SearchResults.js
@@ -5,10 +5,14 @@ import SearchResultItem from './SearchResultsItem';
 import SearchResultsCount from './SearchResultsCount';
 import Pagination from '../pagination/Pagination';
 import NoResults from '../noResults/NoResults';
+import { PAGE_SIZE } from '../searchReducer';
 import './SearchResults.less';
 
-function SearchResults({ searchResult, isSearching, searchResultTotal, restoreFocusToUuid }) {
+function SearchResults({
+    searchResult, isSearching, searchResultTotal, restoreFocusToUuid, from, total
+}) {
     const { stillinger } = searchResult;
+    const hasMore = (from === undefined && total > PAGE_SIZE) || (from + PAGE_SIZE < total);
     return (
         <div className="SearchResults" role="region" aria-live="polite">
             <SearchResultsCount />
@@ -19,7 +23,9 @@ function SearchResults({ searchResult, isSearching, searchResultTotal, restoreFo
                     shouldFocus={stilling.uuid === restoreFocusToUuid}
                 />
             ))}
-            <Pagination />
+            {hasMore && (
+                <Pagination />
+            )}
             {!isSearching && searchResultTotal === 0 && (
                 <NoResults />
             )}
@@ -37,14 +43,18 @@ SearchResults.propTypes = {
     }).isRequired,
     isSearching: PropTypes.bool.isRequired,
     searchResultTotal: PropTypes.number.isRequired,
-    restoreFocusToUuid: PropTypes.string
+    restoreFocusToUuid: PropTypes.string,
+    from: PropTypes.number.isRequired,
+    total: PropTypes.number.isRequired
 };
 
 const mapStateToProps = (state) => ({
     searchResultTotal: state.search.searchResult.total,
     isSearching: state.search.isSearching,
     searchResult: state.search.searchResult,
-    restoreFocusToUuid: state.focus.restoreFocusToUuid
+    restoreFocusToUuid: state.focus.restoreFocusToUuid,
+    from: state.search.from,
+    total: state.search.searchResult.total
 });
 
 export default connect(mapStateToProps)(SearchResults);
