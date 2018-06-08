951388b21356a9521a14f066893a23acb3c7b77b
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2018-06-08 13:09:42 +0200

    [Bugfix] Viser feilmelding hvis bruker mister nettverksforbindelse når stillingsannonse lastes fra backend.

--- src/stilling/api.js
+++ src/stilling/api.js
@@ -3,36 +3,41 @@ import { SEARCH_API } from '../fasitProperties';
 
 const filteredSource = {
     excludes: [
-        "administration",
-        "categoryList",
-        "contactList",
-        "created",
-        "createdBy",
-        "employer",
-        "expires",
-        "geopoint",
-        "id",
-        "location",
-        "mediaList",
-        "medium",
-        "privacy",
-        "properties.author",
-        "properties.industry",
-        "properties.keywords",
-        "properties.occupation",
-        "properties.searchtags",
-        "properties.sourceupdated",
-        "published",
-        "updatedBy",
-        "uuid"
+        'administration',
+        'categoryList',
+        'contactList',
+        'created',
+        'createdBy',
+        'employer',
+        'expires',
+        'geopoint',
+        'id',
+        'location',
+        'mediaList',
+        'medium',
+        'privacy',
+        'properties.author',
+        'properties.industry',
+        'properties.keywords',
+        'properties.occupation',
+        'properties.searchtags',
+        'properties.sourceupdated',
+        'published',
+        'updatedBy',
+        'uuid'
     ]
 };
 
 export async function fetchStilling(id) {
-    let excludeParamValue = filteredSource.excludes.join(',');
-    const response = await fetch(`${SEARCH_API}/stillingsok/ad/ad/${id}?_source_exclude=${excludeParamValue}`, {
-        method: 'GET'
-    });
+    const excludeParamValue = filteredSource.excludes.join(',');
+    let response;
+    try {
+        response = await fetch(`${SEARCH_API}/stillingsok/ad/ad/${id}?_source_exclude=${excludeParamValue}`, {
+            method: 'GET'
+        });
+    } catch (e) {
+        throw new SearchApiError(e.message, 0);
+    }
     if (response.status !== 200) {
         throw new SearchApiError(response.statusText, response.status);
     }
