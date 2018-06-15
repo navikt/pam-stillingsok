57a5acd4d7dd8114be5dca781946cc4ba7563358
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2018-06-15 11:24:26 +0200

    Tweaker

--- src/discalimer/Disclaimer.js
+++ src/discalimer/Disclaimer.js
@@ -2,42 +2,50 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Knapp } from 'nav-frontend-knapper';
-import { Normaltekst, Element } from 'nav-frontend-typografi';
+import { Element, Normaltekst } from 'nav-frontend-typografi';
 import { HIDE_DISCLAIMER } from './disclaimerReducer';
 import './Disclaimer.less';
 
 function Disclaimer({ shouldShow, hideDisclaimer }) {
     if (shouldShow) {
         return (
-            <div role="alert" className="Disclaimer typo-normal">
-                <Element className="blokk-xxs">Tidlig versjon av nytt stillingssøk</Element>
-                <Normaltekst>
-                    Vi utvikler et nytt stillingsøk, og vil at du skal få prøve
-                    det underveis.
-                </Normaltekst>
-                <Normaltekst className="blokk-xxs">
-                    <a href="https://insights.hotjar.com/s?siteId=148751&surveyId=71116" className="lenke">
-                        Gi tilbakemelding på feil og mangler
-                    </a>
-                </Normaltekst>
-                <Normaltekst className="blokk-xs">
-                    <a href="https://tjenester.nav.no/stillinger/" className="lenke">
-                        Gå tilbake til dagens stillingssøk
-                    </a>
-                </Normaltekst>
-                <Knapp mini onClick={hideDisclaimer}>Skjul</Knapp>
+            <div className="DisclaimerWrapper">
+                <div className="container">
+                    <div role="alert" className="Disclaimer typo-normal">
+                        <Element className="blokk-xxs">Tidlig versjon av nytt stillingssøk</Element>
+                        <Normaltekst className="blokk-xxs">
+                            Vi utvikler et nytt stillingsøk, og vil at du skal få prøve
+                            det underveis.
+                        </Normaltekst>
+                        <Normaltekst className="blokk-xxs">
+                            <a href="https://insights.hotjar.com/s?siteId=148751&surveyId=71116" className="lenke">
+                                Meld om feil og mangler
+                            </a>
+                        </Normaltekst>
+                        <Normaltekst className="blokk-xs">
+                            <a href="https://tjenester.nav.no/stillinger/" className="lenke">
+                                Gå tilbake til dagens stillingssøk
+                            </a>
+                        </Normaltekst>
+                        <Knapp mini onClick={hideDisclaimer}>Skjul</Knapp>
+                    </div>
+                </div>
             </div>
         );
     } else {
         return (
-            <div role="alert" className="Feedback typo-normal">
-            <Normaltekst className="blokk-xxs">
-                <a href="https://insights.hotjar.com/s?siteId=148751&surveyId=71116" className="lenke">
-                    Gi tilbakemelding på feil og mangler
-                </a>
-            </Normaltekst>
+            <div className="DisclaimerWrapper">
+                <div className="container">
+                    <div role="alert" className="Feedback typo-normal">
+                        <Normaltekst className="blokk-xxs">
+                            <a href="https://insights.hotjar.com/s?siteId=148751&surveyId=71116" className="lenke">
+                                Meld om feil og mangler
+                            </a>
+                        </Normaltekst>
+                    </div>
+                </div>
             </div>
-         );
+        );
     }
     return null;
 }
