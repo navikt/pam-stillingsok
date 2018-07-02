1431d44f2ed9a7009570c72269b52cfcac046479
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2018-07-02 13:20:33 +0200

    Dropper "..." og viser hele teksten på søknadsfrist.

--- src/search/searchResults/SearchResultsItem.js
+++ src/search/searchResults/SearchResultsItem.js
@@ -18,11 +18,7 @@ export default class SearchResultItem extends React.Component {
         const { stilling, urlQuery } = this.props;
         let frist;
         if (stilling.properties.applicationdue !== undefined) {
-            if (stilling.properties.applicationdue.length <= 10) {
-                frist = stilling.properties.applicationdue;
-            } else {
-                frist = stilling.properties.applicationdue.slice(0, 10) + '...';
-            }
+            frist = stilling.properties.applicationdue;
         } else {
             frist = 'Ikke oppgitt';
         }
@@ -43,9 +39,13 @@ export default class SearchResultItem extends React.Component {
                         )}
                     </Column>
                     <Column xs="12" md="8">
-                        {stilling.updated && (
+                        {stilling.updated ? (
                             <Undertekst className="SearchResultItem__updated">
                                 Registrert: {formatISOString(stilling.updated, 'D. MMMM YYYY')}{' - '}
+                                Søknadsfrist: {frist}
+                            </Undertekst>
+                        ) : (
+                            <Undertekst className="SearchResultItem__updated">
                                 Søknadsfrist: {frist}
                             </Undertekst>
                         )}
