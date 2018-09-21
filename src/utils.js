03dd4ffb98a6ad6d3a6f24ab611a0e7d8d97da34
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2018-09-21 11:30:44 +0200

    Legger til integrasjon mot backend / lagrede søk

--- src/utils.js
+++ src/utils.js
@@ -10,14 +10,23 @@ export function isValidISOString(isoString) {
 export function formatISOString(isoString, format = 'MMMM YYYY') {
     if (isValidISOString(isoString)) {
         const dt = isoString.split('-');
+        const year = dt[0];
+        const month = dt[1];
+        const monthName = months[month - 1];
+        const dayAndTime = dt[2].split('T');
+        const day = dayAndTime[0];
+        const dayWithoutZero = day.substring(0, 1) === '0' ? day.substring(1) : day;
+        const time = dayAndTime[1].split(':');
+        const hours = time[0];
+        const mins = time[1];
         if (format === 'D. MMMM YYYY') {
-            const day = dt[2].split('T')[0];
             return `${day}.${dt[1]}.${dt[0]}`;
         } else if (format === 'MMMM YYYY') {
             return `${months[dt[1] - 1]} ${dt[0]}`;
         } else if (format === 'DD.MMM') {
-            const day = dt[2].split('T')[0];
             return `${day}. ${months[dt[1] - 1].substring(0, 3)}.`;
+        } else if (format === 'D. MMM YYYY HH:MM') {
+            return `${dayWithoutZero}. ${monthName.substring(0, 3)} ${year} ${hours}:${mins}`;
         }
     }
     return isoString;
