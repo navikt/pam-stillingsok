742a57effa2a9714f15ce8fea272e93a96ffebb1
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2018-09-21 15:57:23 +0200

    Legger til favoritter i kompaktvisning

--- src/search/searchResults/SearchResultsItem.js
+++ src/search/searchResults/SearchResultsItem.js
@@ -4,16 +4,13 @@ import FavouriteButton from '../../favourites/FavouriteButton';
 import SearchResultsItemDetails from './SearchResultsItemDetails';
 import './SearchResultsItem.less';
 
-export default class SearchResultItem extends React.Component {
-    render() {
-        const { stilling } = this.props;
-        return (
-            <div className="SearchResultItem">
-                <FavouriteButton uuid={stilling.uuid} className="SearchResultItem__favourite" />
-                <SearchResultsItemDetails stilling={stilling} />
-            </div>
-        );
-    }
+export default function SearchResultItem({ stilling }) {
+    return (
+        <div className="SearchResultItem">
+            <FavouriteButton uuid={stilling.uuid} className="SearchResultItem__favourite" />
+            <SearchResultsItemDetails stilling={stilling} />
+        </div>
+    );
 }
 
 SearchResultItem.propTypes = {
