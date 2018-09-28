1e88ec435a22a74f0196d0977291b8334b0c8e5e
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2018-09-28 12:28:02 +0200

    Fix

---
+++ src/search/searchResults/SearchResultCount.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Undertittel } from 'nav-frontend-typografi';
+
+function SearchResultCount({ searchResult }) {
+    if (searchResult) {
+        return (
+            <Undertittel
+                className="SearchResultCount"
+                aria-live="assertive"
+            >
+                {searchResult.total > 0 ? `${searchResult.total} treff` : 'Ingen treff'}
+            </Undertittel>
+        );
+    }
+    return (
+        <div />
+    );
+}
+
+SearchResultCount.defaultProps = {
+    searchResult: undefined
+}
+
+SearchResultCount.propTypes = {
+    searchResult: PropTypes.shape({
+        total: PropTypes.number
+    })
+};
+
+const mapStateToProps = (state) => ({
+    searchResult: state.search.searchResult
+});
+
+
+export default connect(mapStateToProps)(SearchResultCount);
