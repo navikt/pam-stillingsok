bf6e0dfa7538f248688deef788d1d1b98a8850f2
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2018-10-01 09:59:10 +0200

    [PAM-1568] Unngå unødvending søk ved initialSearch

--- src/search/searchReducer.js
+++ src/search/searchReducer.js
@@ -113,31 +113,34 @@ export default function searchReducer(state = initialState, action) {
 }
 
 export function toSearchQuery(state) {
-    return {
-        q: state.searchBox.q,
-        from: state.search.from,
-        size: state.search.to - state.search.from,
-        sort: state.sorting.sort,
-        counties: state.counties.checkedCounties.filter((county) => {
-            // Hvis man filtrerer på en kommune, må man droppe fylket når man søker.
-            // Altså om man krysser av på Bergen, skal man ikke ha med Hordaland i søket.
-            const countyObject = state.counties.counties.find((c) => c.key === county);
-            const found = countyObject.municipals.find((m) => state.counties.checkedMunicipals.includes(m.key));
-            return !found;
-        }),
-        municipals: state.counties.checkedMunicipals,
-        published: state.published.checkedPublished,
-        engagementType: state.engagement.checkedEngagementType,
-        sector: state.sector.checkedSector,
-        extent: state.extent.checkedExtent,
-        occupationFirstLevels: state.occupations.checkedFirstLevels.filter((firstLevel) => {
-            // Hvis man filtrerer på en yrke på andre nivå, må man droppe yrkeskategorien på første nivå.
-            const firstLevelObject = state.occupations.occupationFirstLevels.find((c) => c.key === firstLevel);
-            const found = firstLevelObject.occupationSecondLevels.find((m) => state.occupations.checkedSecondLevels.includes(m.key));
-            return !found;
-        }),
-        occupationSecondLevels: state.occupations.checkedSecondLevels
-    };
+    const query = {};
+    if (state.searchBox.q.length > 0) query.q = state.searchBox.q;
+    if (state.search.from > 0) query.from = state.search.from;
+    if (state.search.to > PAGE_SIZE) query.size = state.search.to - state.search.from;
+    if (state.sorting.sort.length > 0) query.sort = state.sorting.sort;
+
+    // Hvis man filtrerer på en kommune, må man droppe fylket når man søker.
+    // Altså om man krysser av på Bergen, skal man ikke ha med Hordaland i søket.
+    const counties = state.counties.checkedCounties.filter((county) => {
+        const countyObject = state.counties.counties.find((c) => c.key === county);
+        const found = countyObject.municipals.find((m) => state.counties.checkedMunicipals.includes(m.key));
+        return !found;
+    });
+    if (counties.length > 0) query.counties = counties;
+    if (state.counties.checkedMunicipals.length > 0) query.municipals = state.counties.checkedMunicipals;
+    if (state.published.checkedPublished.length > 0) query.published = state.published.checkedPublished;
+    if (state.engagement.checkedEngagementType.length > 0) query.engagementType = state.engagement.checkedEngagementType;
+    if (state.sector.checkedSector.length > 0) query.sector = state.sector.checkedSector;
+    if (state.extent.checkedExtent.length > 0) query.extent = state.extent.checkedExtent;
+    // Hvis man filtrerer på en yrke på andre nivå, må man droppe yrkeskategorien på første nivå.
+    const occupationFirstLevels = state.occupations.checkedFirstLevels.filter((firstLevel) => {
+        const firstLevelObject = state.occupations.occupationFirstLevels.find((c) => c.key === firstLevel);
+        const found = firstLevelObject.occupationSecondLevels.find((m) => state.occupations.checkedSecondLevels.includes(m.key));
+        return !found;
+    });
+    if (occupationFirstLevels.length > 0) query.occupationFirstLevels = occupationFirstLevels;
+    if (state.occupations.checkedSecondLevels.length > 0) query.occupationSecondLevels = state.occupations.checkedSecondLevels;
+    return query;
 }
 
 /**
@@ -149,19 +152,20 @@ function* initialSearch() {
     try {
         let response;
         if (!state.search.initialSearchDone) {
-            // For å hente alle tilgjengelige fasetter, gjør vi først
+            // For å hente alle tilgjengelige fasetter (yrke, sted ), gjør vi først
             // et søk uten noen søkekriterier.
             response = yield call(fetchSearch, { size: 0 });
             yield put({ type: FETCH_INITIAL_FACETS_SUCCESS, response });
 
-            // Gjør et nytt søk med søkekriterier
+            // Gjør et nytt søk hvis det finnes noen krysset av noen
+            // fasetter/søkekriterier
             state = yield select();
             const query = toSearchQuery(state);
-            response = yield call(fetchSearch, query);
-
-            if (response) {
-                yield put({ type: SEARCH_SUCCESS, response });
+            if (Object.keys(query).length > 0) {
+                response = yield call(fetchSearch, query);
             }
+
+            yield put({ type: SEARCH_SUCCESS, response });
         }
     } catch (e) {
         if (e instanceof SearchApiError) {
