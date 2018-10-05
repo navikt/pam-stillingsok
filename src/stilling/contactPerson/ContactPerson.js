5f0139547ac59051aee32f56e442495e630f099d
Ida Onshus <Ida.Onshus@nav.no>
2018-10-05 12:46:56 +0200

    [PAM-1617]: Viser info om kontaktperson (For stillinger fra FINN er disse dataene excludert fra elastic search.)

--- src/stilling/contactPerson/ContactPerson.js
+++ src/stilling/contactPerson/ContactPerson.js
@@ -2,28 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Undertittel } from 'nav-frontend-typografi';
 
-export default function ContactPerson({ properties }) {
-    if (properties.contactperson || properties.contactpersontitle
-        || properties.contactpersonphone || properties.contactpersonemail) {
+export default function ContactPerson({ contactList }) {
+    if (contactList && contactList.length > 0) {
         return (
-            <div className="ContactPerson detail-section">
-                <Undertittel className="ContactPerson__head detail-section__head">Kontaktperson for stillingen</Undertittel>
+            <div className="detail-section">
+                <Undertittel className="detail-section__head">Kontaktperson for stillingen</Undertittel>
                 <dl className="dl-flex typo-normal">
-                    {properties.contactperson && [
+                    {contactList[0].name && [
                         <dt key="dt">Kontaktperson:</dt>,
-                        <dd key="dd">{properties.contactperson}</dd>
+                        <dd key="dd">{contactList[0].name}</dd>
                     ]}
-                    {properties.contactpersontitle && [
+                    {contactList[0].title && [
                         <dt key="dt">Stillingstittel:</dt>,
-                        <dd key="dd">{properties.contactpersontitle}</dd>
+                        <dd key="dd">{contactList[0].title}</dd>
                     ]}
-                    {properties.contactpersonphone && [
+                    {contactList[0].phone && [
                         <dt key="dt">Telefon:</dt>,
-                        <dd key="dd">{properties.contactpersonphone}</dd>
+                        <dd key="dd">{contactList[0].phone}</dd>
                     ]}
-                    {properties.contactpersonemail && [
+                    {contactList[0].email && [
                         <dt key="dt">Epost:</dt>,
-                        <dd key="dd">{properties.contactpersonemail}</dd>
+                        <dd key="dd">{contactList[0].email}</dd>
                     ]}
                 </dl>
             </div>
@@ -33,11 +32,10 @@ export default function ContactPerson({ properties }) {
 }
 
 ContactPerson.propTypes = {
-    properties: PropTypes.shape({
-        contactperson: PropTypes.string,
-        contactpersontitle: PropTypes.string,
-        contactpersonphone: PropTypes.string,
-        contactpersonemail: PropTypes.string
-    }).isRequired
+    contactList: PropTypes.arrayOf(PropTypes.shape({
+        person: PropTypes.string,
+        title: PropTypes.string,
+        phone: PropTypes.string,
+        email: PropTypes.string
+    })).isRequired
 };
-
