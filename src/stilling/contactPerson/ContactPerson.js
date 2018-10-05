c3ebd89e443be2f44f2ee97ce934e322419165b0
Ida Onshus <Ida.Onshus@nav.no>
2018-10-05 13:39:11 +0200

    satte contactList som ikke required i proptypes

--- src/stilling/contactPerson/ContactPerson.js
+++ src/stilling/contactPerson/ContactPerson.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Undertittel } from 'nav-frontend-typografi';
 
 export default function ContactPerson({ contactList }) {
+    console.log(contactList);
     if (contactList && contactList.length > 0) {
         return (
             <div className="detail-section">
@@ -31,11 +32,15 @@ export default function ContactPerson({ contactList }) {
     return null;
 }
 
+ContactPerson.defaultProps = {
+    contactList: undefined
+};
+
 ContactPerson.propTypes = {
     contactList: PropTypes.arrayOf(PropTypes.shape({
         person: PropTypes.string,
         title: PropTypes.string,
         phone: PropTypes.string,
         email: PropTypes.string
-    })).isRequired
+    }))
 };
