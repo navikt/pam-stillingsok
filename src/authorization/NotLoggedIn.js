f2d49b1fa4a4586083bb42145d31b42232e10e69
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2018-10-11 14:39:48 +0200

    Legger til logg inn knapp i not logged in modal

--- src/authorization/NotLoggedIn.js
+++ src/authorization/NotLoggedIn.js
@@ -1,12 +1,14 @@
+import { Flatknapp, Hovedknapp } from 'nav-frontend-knapper';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Element } from 'nav-frontend-typografi';
+import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
 import Modal from 'nav-frontend-modal';
+import AuthorizationEnum from './AuthorizationEnum';
 import { HIDE_AUTHORIZATION_ERROR_MODAL } from './authorizationReducer';
+import './NotLoggedIn.less';
 
 class NotLoggedIn extends React.Component {
-
     closeModal = () => {
         this.props.hideError();
     };
@@ -21,10 +23,20 @@ class NotLoggedIn extends React.Component {
                     appElement={document.getElementById('app')}
 
                 >
-                    <div className="SavedSearchModal">
-                        <Element>
-                            {this.props.authorizationError}
-                        </Element>
+                    <div className="NotLoggedIn">
+                        <Undertittel className="NotLoggedIn__title">Krever innlogging</Undertittel>
+                        <Normaltekst className="NotLoggedIn__message">
+                            {this.props.authorizationError === AuthorizationEnum.ADD_FAVORITE_ERROR && (
+                                <span>For å lagre en favoritt må du logge inn</span>
+                            )}
+                            {this.props.authorizationError === AuthorizationEnum.SAVE_SEARCH_ERROR && (
+                                <span>For å lagre et søk må du logge inn</span>
+                            )}
+                        </Normaltekst>
+                        <div className="NotLoggedIn__buttons">
+                            <Hovedknapp onClick={this.onRemoveClick}>Logg inn</Hovedknapp>
+                            <Flatknapp onClick={this.closeModal}>Avbryt</Flatknapp>
+                        </div>
                     </div>
                 </Modal>
             );
