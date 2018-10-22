81229c1efbee7b4605b412b43b997927f54531b7
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2018-10-22 09:05:45 +0200

    Viser både login og logut knapp

--- src/authentication/AuthenticationButton.js
+++ src/authentication/AuthenticationButton.js
@@ -1,37 +1,24 @@
 import { Container } from 'nav-frontend-grid';
-import PropTypes from 'prop-types';
 import React from 'react';
-import { connect } from 'react-redux';
 import { LOGIN_URL, LOGOUT_URL } from '../fasitProperties';
 import './AuthenticationButton.less';
 
-function AuthenticationButton({ isAuthenticated }) {
+export default function AuthenticationButton() {
     return (
         <div className="AuthenticationButton-wrapper no-print">
             <Container>
-                {isAuthenticated ? (
-                    <a className="AuthenticationButton typo-element" href={LOGOUT_URL}>
-                        Logg ut
-                    </a>
-                ) : (
-                    <a
-                        className="AuthenticationButton typo-element"
-                        href={`${LOGIN_URL}?redirect=${window.location.href}`}
-                    >
-                        Logg inn
-                    </a>
-                )}
+                <a className="AuthenticationButton typo-element" href={LOGOUT_URL}>
+                    Logg ut
+                </a>
+                &nbsp;
+                <a
+                    className="AuthenticationButton typo-element"
+                    href={`${LOGIN_URL}?redirect=${window.location.href}`}
+                >
+                    Logg inn
+                </a>
             </Container>
         </div>
     );
 }
 
-AuthenticationButton.propTypes = {
-    isAuthenticated: PropTypes.bool.isRequired
-};
-
-const mapStateToProps = (state) => ({
-    isAuthenticated: state.authentication.isAuthenticated
-});
-
-export default connect(mapStateToProps)(AuthenticationButton);
