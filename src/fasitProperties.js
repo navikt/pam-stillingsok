2ecad4a0198202d98debbd39131c6003bc9ff02e
Terje Heen <terje.heen@nav.no>
2018-10-29 10:52:51 +0100

    [PAM-1831] Renames LOGIN_URL
    LOGIN_URL is renamed to LOGIN_URL_WITHOUT_LEVEL to enable LOGIN_URL to be used with LEVEL for minId.

--- src/fasitProperties.js
+++ src/fasitProperties.js
@@ -2,5 +2,5 @@ export const SEARCH_API = __PAM_SEARCH_API__;
 export const AD_USER_API = __PAM_AD_USER_API__;
 export const STILLING = __PAM_STILLING__;
 export const CONTEXT_PATH = __PAM_CONTEXT_PATH__;
-export const LOGIN_URL = __LOGIN_URL__;
+export const LOGIN_URL_WITHOUT_LEVEL = __LOGIN_URL__;
 export const LOGOUT_URL = __LOGOUT_URL__;
