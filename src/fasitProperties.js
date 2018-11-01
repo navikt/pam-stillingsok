05ad85c3f7466edd50821bc938437bf62e89068e
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2018-11-01 11:55:06 +0100

    Fikser typo i login url

--- src/fasitProperties.js
+++ src/fasitProperties.js
@@ -5,4 +5,4 @@ export const CONTEXT_PATH = __PAM_CONTEXT_PATH__;
 export const LOGIN_URL_WITHOUT_LEVEL = __LOGIN_URL__;
 export const LOGOUT_URL = __LOGOUT_URL__;
 export const PAM_STILLINGSOK_URL = __PAM_STILLINGSOK_URL__;
-export const LOGIN_URL = `${LOGIN_URL_WITHOUT_LEVEL}?level=Level3?redirect=${PAM_STILLINGSOK_URL}`;
+export const LOGIN_URL = `${LOGIN_URL_WITHOUT_LEVEL}?level=Level3&redirect=${PAM_STILLINGSOK_URL}`;
