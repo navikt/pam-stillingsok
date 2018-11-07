c1ff3ef6776e16153e12ca9711b6f6eeaefa08f5
Stephen Ramthun <stephen.ramthun@nav.no>
2018-11-07 14:17:02 +0100

    PAM-1360 Fjern unødvendig console.log

--- scripts/searchApiConsumer.js
+++ scripts/searchApiConsumer.js
@@ -1,7 +1,6 @@
 const rp = require('request-promise');
 
 exports.search = (templateFunc, query = {}) => {
-    console.log(query);
     const host = process.env.DEV_PROFILE === 'true' ? 'http://localhost:9000' : 'http://pam-search-api';
     const url = `${host}/ad/_search`;
     const options = {
