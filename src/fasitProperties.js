4ff7a834e62428fd2d57307154d7739ab530f476
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2018-11-09 13:37:59 +0100

    Rydder i fasitProperties

--- src/fasitProperties.js
+++ src/fasitProperties.js
@@ -1,7 +1,5 @@
+export const CONTEXT_PATH = __PAM_CONTEXT_PATH__;
 export const SEARCH_API = __PAM_SEARCH_API__;
 export const AD_USER_API = __PAM_AD_USER_API__;
-export const CONTEXT_PATH = __PAM_CONTEXT_PATH__;
-export const LOGIN_URL_WITHOUT_LEVEL = __LOGIN_URL__;
+export const LOGIN_URL = `${__LOGIN_URL__}?level=Level3&redirect=${__PAM_STILLINGSOK_URL__}`;
 export const LOGOUT_URL = __LOGOUT_URL__;
-export const PAM_STILLINGSOK_URL = __PAM_STILLINGSOK_URL__;
-export const LOGIN_URL = `${LOGIN_URL_WITHOUT_LEVEL}?level=Level3&redirect=${PAM_STILLINGSOK_URL}`;
