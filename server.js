8b4800049b6420f284a7b033b1b6a0824e1548eb
Stephen Ramthun <stephen.ramthun@nav.no>
2018-11-29 12:04:29 +0100

    [PAM-2212] Vis feilmelding om søk feiler

--- server.js
+++ server.js
@@ -91,17 +91,21 @@ const startServer = (htmlPages) => {
     );
 
     server.get('/api/search', async (req, res) => {
-        const result = await searchApiConsumer.search(req.query)
-            .catch((err) => console.warn('Failed to query search api', err));
-
-        res.send(result);
+        await searchApiConsumer.search(req.query)
+            .catch((err) => {
+                console.warn('Failed to query search api', err);
+                res.status(err.statusCode);
+            })
+            .then((val) => res.send(val));
     });
 
     server.post('/api/search', async (req, res) => {
-        const result = await searchApiConsumer.search(req.body)
-            .catch((err) => { console.warn('Failed to query search api', err); });
-
-        res.send(result);
+        await searchApiConsumer.search(req.body)
+            .catch((err) => {
+                console.warn('Failed to query search api', err);
+                res.status(err.statusCode);
+            })
+            .then((val) => res.send(val));
     });
 
     server.get('/api/suggestions', async (req, res) => {
@@ -122,10 +126,9 @@ const startServer = (htmlPages) => {
         await searchApiConsumer.fetchStilling(req.params.uuid)
             .catch((err) => {
                 console.warn('Failed to fetch stilling with uuid', req.params.uuid);
-                res.status(err.statusCode)
-                    .send(err.message);
+                res.status(err.statusCode);
             })
-            .then((val) => { res.send(val); });
+            .then((val) => res.send(val));
     });
 
     server.get(
