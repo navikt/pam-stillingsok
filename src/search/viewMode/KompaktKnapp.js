a4cae7c597eb84a91b1eb4c5ce4828d62f6d7688
Stephen Ramthun <stephen.ramthun@nav.no>
2019-01-03 12:02:18 +0100

    [PAM-2357] Endre kompaktknapp

--- src/search/viewMode/KompaktKnapp.js
+++ src/search/viewMode/KompaktKnapp.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import './ViewMode.less';
+import './KompaktKnapp.less';
 
 const createLines = (status, count) => {
     return Array(count)
