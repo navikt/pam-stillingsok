96d86f009639b368da3023f9037d55b488e1127b
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2019-01-24 13:10:25 +0100

    [Fix] Gjør feedback-link grønn og la den åpne i ny fane

--- src/feedback/Feedback.js
+++ src/feedback/Feedback.js
@@ -7,6 +7,8 @@ export default function Feedback() {
             <a
                 className="Feedback__link"
                 href="https://surveys.hotjar.com/s?siteId=118350&surveyId=124493"
+                target="_blank"
+                rel="noopener noreferrer"
             >
                 <span className="Feedback__link__inner">
                     Gi tilbakemelding
