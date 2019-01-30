fe778fa79488f5087b4689079d430af77aae7098
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2019-01-30 14:57:39 +0100

    [PAM-2611] Legg til footer

---
+++ src/common/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.less';
+
+const Footer = () => (
+    <div className="Footer">
+        <a className="Footer__logo lenke" href="https://www.nav.no/Forsiden" title="Gå til nav.no">
+            <i className="Footer__logo__nav-icon" aria-label="NAV logo" />
+        </a>
+    </div>
+);
+
+export default Footer;
