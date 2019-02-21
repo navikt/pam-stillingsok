b3d585b9b0b8aee82f921de58a7ee0ef7fa4c9d1
Øyvind Stegard <oyvind.stegard@nav.no>
2019-02-21 17:08:13 +0100

    [QA] Use a slightly non-default Selenium port for slightly less chance of conflict

--- qa/nightwatch.conf.js
+++ qa/nightwatch.conf.js
@@ -8,6 +8,8 @@ require('nightwatch-cucumber')({
         'features'
     ]
 });
+
+const selenium_port = 4445;
 module.exports = {
     "output_folder": "./reports",
     "page_objects_path" : "./pages",
@@ -15,7 +17,7 @@ module.exports = {
         "start_process": true,
         "server_path": seleniumServer.path,
         "host": "127.0.0.1",
-        "port": 4444, // standard selenium port
+        "port": selenium_port
     },
     "test_workers" : {
         "enabled" : true,
@@ -30,6 +32,8 @@ module.exports = {
                 "on_error" : true,
                 "path" : "./screenshots"
             },
+            "selenium_port": selenium_port,
+            "selenium_host": "127.0.0.1",
             "globals" : {
                 "waitForConditionTimeout" : 5000, // sometimes internet is slow so wait.
                 "environment" : "local"
