4211607efb54f6afe4926986442119c9be4aa826
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2019-03-12 15:16:28 +0100

    Legger inn footer

--- src/common/footer/Footer.js
+++ src/common/footer/Footer.js
@@ -1,27 +1,71 @@
-import { Column, Container, Row } from 'nav-frontend-grid';
-import { Normaltekst } from 'nav-frontend-typografi';
+/* eslint-disable jsx-a11y/href-no-hash */
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import './Footer.less';
 
-const Footer = () => (
+const InternalOrExternalLink = ({ href, isInternalLink, children }) => {
+    if (isInternalLink) {
+        return (
+            <Link to={href} className="Footer__links__ul__li__link">
+                {children}
+            </Link>
+        );
+    }
+    return (
+        <a href={href} className="Footer__links__ul__li__link">
+            {children}
+        </a>
+    );
+};
+
+InternalOrExternalLink.propTypes = {
+    href: PropTypes.string.isRequired,
+    isInternalLink: PropTypes.bool.isRequired,
+    children: PropTypes.string.isRequired
+};
+
+const Footer = ({ useInternalLinks }) => (
     <div className="Footer">
-        <Container>
-            <Row>
-                <Column xs="12" md="1" />
-                <Column xs="12" md="11">
-                    <Normaltekst className="Footer__slogan">Alt av arbeid på ett sted</Normaltekst>
-                </Column>
-            </Row>
-            <Row>
-                <Column xs="12" md="1" />
-                <Column xs="12" md="11">
-                    <a className="Footer__logo lenke" href="https://www.nav.no/Forsiden" title="Gå til nav.no">
-                        <i className="Footer__logo__nav-icon" aria-label="NAV logo" />
-                    </a>
-                </Column>
-            </Row>
-        </Container>
+        <div className="Footer__inner">
+            <p className="Footer__slogan">Alt av arbeid på ett sted.</p>
+            <nav className="Footer__links">
+                <a className="Footer__logo lenke" href="https://www.nav.no/Forsiden" title="Gå til nav.no">
+                    <i className="Footer__logo__nav-icon" aria-label="NAV logo" />
+                </a>
+                <ul className="Footer__links__ul">
+                    <li className="Footer__links__ul__li">
+                        <InternalOrExternalLink isInternalLink={useInternalLinks} href="/om-arbeidsplassen">
+                            Om arbeidsplassen
+                        </InternalOrExternalLink>
+                    </li>
+                    <li className="Footer__links__ul__li">
+                        <InternalOrExternalLink isInternalLink={useInternalLinks} href="/kontaktinfo">
+                            Kontaktinfo
+                        </InternalOrExternalLink>
+                    </li>
+                    <li className="Footer__links__ul__li">
+                        <InternalOrExternalLink isInternalLink={useInternalLinks} href="/tilgjengelighet">
+                            Tilgjengelighet
+                        </InternalOrExternalLink>
+                    </li>
+                    <li className="Footer__links__ul__li">
+                        <InternalOrExternalLink isInternalLink={useInternalLinks} href="/personvern">
+                            Personvern
+                        </InternalOrExternalLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </div>
 );
+
+Footer.propTypes = {
+    useInternalLinks: PropTypes.bool
+};
+
+Footer.defaultProps = {
+    useInternalLinks: false
+};
 
 export default Footer;
