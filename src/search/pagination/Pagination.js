fad5237c5624e8b94ee15b6b21d3f257e254e452
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2019-03-14 15:37:52 +0100

    Skjuler lenker i footer, justerer Se flere knapp

--- src/search/pagination/Pagination.js
+++ src/search/pagination/Pagination.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
-import { PrimaryButton } from '../../common/button';
+import { Button } from '../../common/button';
 import { LOAD_MORE } from '../searchReducer';
 import './Pagination.less';
 
@@ -14,14 +14,14 @@ class Pagination extends React.Component {
         const { isLoadingMore } = this.props;
         return (
             <div className="Pagination">
-                <PrimaryButton
+                <Button
                     disabled={isLoadingMore}
                     spinner={isLoadingMore}
                     className="Pagination__button"
                     onClick={this.onLoadMoreClick}
                 >
                     Se flere
-                </PrimaryButton>
+                </Button>
             </div>
         );
     }
