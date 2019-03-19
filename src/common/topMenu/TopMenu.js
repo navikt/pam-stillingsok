c0b46aa6b16e3a7bf7b49dcd758b12f70b5fbf14
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2019-03-19 13:52:52 +0100

    Sender riktige props til header

--- src/common/topMenu/TopMenu.js
+++ src/common/topMenu/TopMenu.js
@@ -6,34 +6,21 @@ import { LOGIN_URL, LOGOUT_URL } from '../../fasitProperties';
 import { authenticationEnum } from '../../authentication/authenticationReducer';
 import { getRedirect } from '../../redirect';
 
-class TopMenu extends React.Component {
-    onLoginClick = () => {
-        window.location.href = `${LOGIN_URL}${getRedirect()}`;
-    };
-
-    onLogoutClick = () => {
-        window.location.href = LOGOUT_URL;
-    };
-
-    render() {
-        return (
-            <div className="no-print">
-                {this.props.isAuthenticated === authenticationEnum.IS_AUTHENTICATED
-                    || this.props.isAuthenticated === authenticationEnum.NOT_AUTHENTICATED ? (
-                        <PersonbrukerHeaderMeny
-                            onLoggUt={this.onLogoutClick}
-                            onLoggInn={this.onLoginClick}
-                            personbruker={{ navn: 'Innstillinger' }}
-                            erInnlogget={this.props.isAuthenticated === authenticationEnum.IS_AUTHENTICATED}
-                            applikasjon={PersonbrukerApplikasjon.STILLINGSSOK}
-                        />
-                    ) : (
-                        <PersonbrukerHeader />
-                    )}
-            </div>
-        );
-    }
-}
+const TopMenu = ({ isAuthenticated }) => (
+    <div className="no-print">
+        {isAuthenticated === authenticationEnum.IS_AUTHENTICATED || isAuthenticated === authenticationEnum.NOT_AUTHENTICATED ? (
+            <PersonbrukerHeaderMeny
+                loggInnUrl={`${LOGIN_URL}${getRedirect()}`}
+                loggUtUrl={LOGOUT_URL}
+                personbruker={{ navn: 'Innstillinger' }}
+                erInnlogget={this.props.isAuthenticated === authenticationEnum.IS_AUTHENTICATED}
+                applikasjon={PersonbrukerApplikasjon.STILLINGSSOK}
+            />
+        ) : (
+            <PersonbrukerHeader />
+        )}
+    </div>
+);
 
 TopMenu.propTypes = {
     isAuthenticated: PropTypes.string.isRequired
