ac81d08e9cbb81e1b59a96a04f0a08571ddc98ca
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2019-04-01 13:02:37 +0200

    [Fix] Ikke åpne feedback-skjema i ny fane pga UU

--- src/feedback/Feedback.js
+++ src/feedback/Feedback.js
@@ -7,8 +7,6 @@ export default function Feedback() {
             <a
                 className="Feedback__link"
                 href="https://surveys.hotjar.com/s?siteId=118350&surveyId=124493"
-                target="_blank"
-                rel="noopener noreferrer"
             >
                 <span className="Feedback__link__inner">
                     Gi tilbakemelding
