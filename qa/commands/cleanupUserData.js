0c5eb6dc1fa1812ab970a861840c88b72d691b31
Ida Onshus <Ida.Onshus@nav.no>
2019-05-20 14:54:33 +0200

    qa:fjerner unødvendig url-kall for å få mozillatest til å fungere.

--- qa/commands/cleanupUserData.js
+++ qa/commands/cleanupUserData.js
@@ -2,9 +2,6 @@ exports.command = function () {
     const favouritPage = this.page.FavoritterPage();
     const savedSearchesPage = this.page.LagredeSokPage();
 
-    this.url(this.launch_url)
-        .maximizeWindow();
-
     favouritPage.click('@goToFavourites')
         .waitForElementPresent('@favouritesContent', 20000)
         .deleteAllFavourites();
