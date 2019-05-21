79fdb7b7c41521504fd6f81a90744ebbcda48772
Ida Onshus <Ida.Onshus@nav.no>
2019-05-21 07:20:15 +0200

    qa: forsøker å få firefoxtest til å passere

--- qa/commands/cleanupUserData.js
+++ qa/commands/cleanupUserData.js
@@ -1,6 +1,7 @@
 exports.command = function () {
     const favouritPage = this.page.FavoritterPage();
     const savedSearchesPage = this.page.LagredeSokPage();
+    const forside = this.page.SokForside();
 
     favouritPage.click('@goToFavourites')
         .waitForElementPresent('@favouritesContent', 20000)
@@ -9,6 +10,9 @@ exports.command = function () {
     savedSearchesPage.click('@gotoSavedSearches')
         .waitForElementPresent('@savedSearchesContent', 20000)
         .deleteAllItems();
+
+    forside.click('@goToForside')
+        .waitForElementPresent('@searchResult', 20000);
 
     return this;
 };
