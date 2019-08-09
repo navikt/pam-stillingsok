2906aba139588648b8d5649f1f6df835d4a39c22
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2019-08-09 15:37:56 +0200

    Rydder i common og utils

--- src/common/utils/getEmployer.js
+++ src/common/utils/getEmployer.js
@@ -1,37 +1,4 @@
-import capitalizeLocation from './capitalizeLocation';
-
-function capitalizeEmployerName(text) {
-    const separators = [' ', '-', '(', '/'];
-
-    const ignore = [
-        'i', 'og', 'for', 'på', 'avd', 'av'
-    ];
-
-    const keep = [
-        'as', 'ab', 'asa', 'ba', 'sa'
-    ];
-
-    if (text) {
-        let capitalized = text.toLowerCase();
-
-        for (let i = 0, len = separators.length; i < len; i += 1) {
-            const fragments = capitalized.split(separators[i]);
-            for (let j = 0, x = fragments.length; j < x; j += 1) {
-                if (keep.includes(fragments[j])) {
-                    fragments[j] = fragments[j].toUpperCase();
-                } else if (!ignore.includes(fragments[j])) {
-                    if (fragments[j][0] !== undefined) {
-                        fragments[j] = fragments[j][0].toUpperCase() + fragments[j].substr(1);
-                    }
-                }
-            }
-            capitalized = fragments.join(separators[i]);
-        }
-
-        return capitalized;
-    }
-    return text;
-}
+import capitalizeEmployerName from './capitalizeEmployerName';
 
 export default function getEmployer(stilling) {
     if (stilling && stilling.properties && stilling.properties.employer) {
@@ -45,23 +12,4 @@ export default function getEmployer(stilling) {
     }
 
     return null;
-}
-
-
-export function getEmployerLocation(employer) {
-    let employerLocation = null;
-
-    if (employer && employer.location) {
-        const { location } = employer;
-
-        if (location.postalCode) {
-            employerLocation = location.address ? `${location.address}, ` : '';
-            employerLocation += `${location.postalCode} ${capitalizeLocation(location.city)}`;
-        } else if (location.municipal) {
-            employerLocation = `${capitalizeLocation(location.municipal)}`;
-        } else if (location.country) {
-            employerLocation = `${capitalizeLocation(location.country)}`;
-        }
-    }
-    return employerLocation;
 }
