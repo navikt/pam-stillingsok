3a83620e7e780a248b1a4e0d34698cc4e638803f
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2019-08-19 11:32:13 +0200

    Fjerner dev variabler

--- src/fasitProperties.js
+++ src/fasitProperties.js
@@ -1,5 +1,5 @@
-export const CONTEXT_PATH = window.__PAM_CONTEXT_PATH__ || '/stillinger';
-export const AD_USER_API = window.__PAM_AD_USER_API__ || 'http://localhost:9017/aduser';
-export const STILLINGSOK_URL = window.__PAM_STILLINGSOK_URL__ || 'http://localhost:8080/stillinger';
-export const LOGIN_URL = window.__LOGIN_URL__ ? `${window.__LOGIN_URL__}?level=Level3&redirect=` : 'http://localhost:9017/aduser/local/cookie?subject=12345';
-export const LOGOUT_URL = window.__LOGOUT_URL__ ||'http://localhost:9017/aduser/';
+export const CONTEXT_PATH = window.__PAM_CONTEXT_PATH__;
+export const AD_USER_API = window.__PAM_AD_USER_API__;
+export const STILLINGSOK_URL = window.__PAM_STILLINGSOK_URL__;
+export const LOGIN_URL = `${window.__LOGIN_URL__}?level=Level3&redirect=`;
+export const LOGOUT_URL = window.__LOGOUT_URL__;
