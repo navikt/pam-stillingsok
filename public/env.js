f9c5baa122068bca644b9f994faa7a841f8e9d45
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2019-08-19 11:53:51 +0200

    Dropper

--- public/env.js
+++ public/env.js
@@ -1,6 +1,5 @@
 // Denne filen blir overskrevet av server.js med variabler fra nais.yml
 window.__PAM_STILLINGSOK_URL__="http://localhost:8080/stillinger";
-window.__PAM_CONTEXT_PATH__="/stillinger";
 window.__PAM_AD_USER_API__="http://localhost:9017/aduser";
 window.__LOGIN_URL__="http://localhost:9017/aduser/local/cookie?subject=12345";
 window.__LOGOUT_URL__="http://localhost:9017/aduser/";
