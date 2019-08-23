78f06bcaa590b2b65f6e8f00cf74d4555fca5c92
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2019-08-23 11:44:57 +0200

    [Analytics] Måle expand/collapse av facet-panelene

--- src/analytics.js
+++ src/analytics.js
@@ -1,8 +1,10 @@
 import { takeEvery } from 'redux-saga/es/effects';
 import { FETCH_IS_AUTHENTICATED_FAILURE, FETCH_IS_AUTHENTICATED_SUCCESS } from './authentication/authenticationReducer';
 import { FETCH_FAVOURITES_SUCCESS } from './favourites/favouritesReducer';
+import { COLLAPSE_FACET_PANEL, EXPAND_FACET_PANEL } from './search/facets/facetPanelsReducer';
 
 function analytics(...props) {
+    console.log(props)
     if (window.ga) {
         window.ga(...props);
     }
@@ -20,7 +22,7 @@ function trackAuthenticationFailure() {
  * Viser om innlogget bruker har valgt "arbeidsgiver" eller "personbruker" når de logget inn.
  * Hvis bruker allerede er innlogget, f.eks på nav.no, vet vi ikke rollen enda ("ukjent").
  */
-export function trackAuthenticationRole(action) {
+function trackAuthenticationRole(action) {
     if(action.isAuthenticated) {
         analytics('send', 'event', 'Authentication', 'role', localStorage.getItem('innloggetBrukerKontekst') || 'ukjent');
     }
@@ -32,7 +34,7 @@ export function trackAuthenticationRole(action) {
  * opprydding av gamle favoritter.
  * @param total
  */
-export function trackTotalFavourites(action) {
+function trackTotalFavourites(action) {
     analytics('send', 'event', 'Favoritter', 'fetchTotalElements', action.response.totalElements);
 }
 
@@ -43,9 +45,19 @@ export function trackStillingPrint() {
     analytics('send', 'event', 'Stilling', 'print');
 }
 
+function trackExpandFacetPanel(action) {
+    analytics('send', 'event', 'Facet', 'expand', action.panelId);
+}
+
+function trackCollapseFacetPanel(action) {
+    analytics('send', 'event', 'Facet', 'collapse', action.panelId);
+}
+
 export const analyticsSaga = function* saga() {
     yield takeEvery(FETCH_IS_AUTHENTICATED_SUCCESS, trackAuthenticationStatus);
     yield takeEvery(FETCH_IS_AUTHENTICATED_SUCCESS, trackAuthenticationRole);
     yield takeEvery(FETCH_IS_AUTHENTICATED_FAILURE, trackAuthenticationFailure);
     yield takeEvery(FETCH_FAVOURITES_SUCCESS, trackTotalFavourites);
+    yield takeEvery(EXPAND_FACET_PANEL, trackExpandFacetPanel);
+    yield takeEvery(COLLAPSE_FACET_PANEL, trackCollapseFacetPanel);
 };
