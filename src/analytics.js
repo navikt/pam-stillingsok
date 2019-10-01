14ecaa8908a26687c3cfcc6241374cfdf2576242
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2019-10-01 11:16:38 +0200

    [Analytics] Legger til måling for favoritter og lagrede søk

--- src/analytics.js
+++ src/analytics.js
@@ -1,7 +1,6 @@
 import { takeEvery } from 'redux-saga/es/effects';
-import { FETCH_IS_AUTHENTICATED_FAILURE, FETCH_IS_AUTHENTICATED_SUCCESS } from './authentication/authenticationReducer';
-import { FETCH_FAVOURITES_SUCCESS, SET_FAVOURITES_SORTING } from './favourites/favouritesReducer';
-import { COLLAPSE_FACET_PANEL, EXPAND_FACET_PANEL } from './search/facets/facetPanelsReducer';
+import { ADD_TO_FAVOURITES_SUCCESS } from './favourites/favouritesReducer';
+import { ADD_SAVED_SEARCH_SUCCESS } from './savedSearches/savedSearchesReducer';
 
 function analytics(...props) {
     if (window.ga) {
@@ -9,59 +8,17 @@ function analytics(...props) {
     }
 }
 
-function trackAuthenticationStatus(action) {
-    analytics('send', 'event', 'Authentication', 'status', action.isAuthenticated);
+function trackAddToFavouritesSuccess() {
+    analytics('send', 'event', 'Favoritter', 'Legg til favoritt');
 }
 
-function trackAuthenticationFailure() {
-    analytics('send', 'event', 'Authentication', 'status', 'failure');
+function trackAddSavedSearchSuccess(action) {
+    analytics('send', 'event', 'Lagrede søk', 'Legg til lagret søk');
 }
 
-/**
- * Viser om innlogget bruker har valgt "arbeidsgiver" eller "personbruker" når de logget inn.
- * Hvis bruker allerede er innlogget, f.eks på nav.no, vet vi ikke rollen enda ("ukjent").
- */
-function trackAuthenticationRole(action) {
-    if(action.isAuthenticated) {
-        analytics('send', 'event', 'Authentication', 'role', localStorage.getItem('innloggetBrukerKontekst') || 'ukjent');
-    }
-}
 
-/**
- * Vi ønsker å vite hvor mange favoritter det er vanlig å ha. Og så vil vi gjerne
- * vite om det er noen brukere som har veldig mange favoritter, og om vi bør innføre paging eller
- * opprydding av gamle favoritter.
- * @param total
- */
-function trackTotalFavourites(action) {
-    analytics('send', 'event', 'Favoritter', 'fetchTotalElements', action.response.totalElements);
-}
-
-function trackFavouritesSorting(action) {
-    analytics('send', 'event', 'Favoritter', 'sort', action.sortField);
-}
-
-/**
- * Viser hvor mange bruker print-knappen på stillingsiden.
- */
-export function trackStillingPrint() {
-    analytics('send', 'event', 'Stilling', 'print');
-}
-
-function trackExpandFacetPanel(action) {
-    analytics('send', 'event', 'Facet', 'expand', action.panelId);
-}
-
-function trackCollapseFacetPanel(action) {
-    analytics('send', 'event', 'Facet', 'collapse', action.panelId);
-}
 
 export const analyticsSaga = function* saga() {
-    yield takeEvery(FETCH_IS_AUTHENTICATED_SUCCESS, trackAuthenticationStatus);
-    yield takeEvery(FETCH_IS_AUTHENTICATED_SUCCESS, trackAuthenticationRole);
-    yield takeEvery(FETCH_IS_AUTHENTICATED_FAILURE, trackAuthenticationFailure);
-    yield takeEvery(FETCH_FAVOURITES_SUCCESS, trackTotalFavourites);
-    yield takeEvery(EXPAND_FACET_PANEL, trackExpandFacetPanel);
-    yield takeEvery(COLLAPSE_FACET_PANEL, trackCollapseFacetPanel);
-    yield takeEvery(SET_FAVOURITES_SORTING, trackFavouritesSorting);
+    yield takeEvery(ADD_TO_FAVOURITES_SUCCESS, trackAddToFavouritesSuccess);
+    yield takeEvery(ADD_SAVED_SEARCH_SUCCESS, trackAddSavedSearchSuccess);
 };
