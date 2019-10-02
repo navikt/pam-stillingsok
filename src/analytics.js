a79713a615f2f849ea7c3a653d629b1891a9ccf9
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2019-10-02 15:26:52 +0200

    [Analytics] Legger til måling for måle om bruker gjør minst et søk

--- src/analytics.js
+++ src/analytics.js
@@ -1,8 +1,12 @@
-import { takeEvery } from 'redux-saga/es/effects';
+import { takeLatest, takeEvery } from 'redux-saga/es/effects';
 import { ADD_TO_FAVOURITES_SUCCESS, REMOVE_FROM_FAVOURITES_SUCCESS } from './favourites/favouritesReducer';
 import { ADD_SAVED_SEARCH_SUCCESS } from './savedSearches/savedSearchesReducer';
+import { SEARCH } from './search/searchReducer';
+
+let igonreFutherEvents = false;
 
 function analytics(...props) {
+    console.log(JSON.stringify(props))
     if (window.ga) {
         window.ga(...props);
     }
@@ -20,10 +24,16 @@ function trackAddSavedSearchSuccess(action) {
     analytics('send', 'event', 'Lagrede søk', 'Legg til lagret søk');
 }
 
-
+function trackFirstSearch() {
+    if(!igonreFutherEvents) {
+        analytics('send', 'event', 'Ledige stillinger', 'Utført minst et søk');
+        igonreFutherEvents = true;
+    }
+}
 
 export const analyticsSaga = function* saga() {
     yield takeEvery(ADD_TO_FAVOURITES_SUCCESS, trackAddToFavouritesSuccess);
     yield takeEvery(REMOVE_FROM_FAVOURITES_SUCCESS, trackRemoveFromFavouritesSuccess);
     yield takeEvery(ADD_SAVED_SEARCH_SUCCESS, trackAddSavedSearchSuccess);
+    yield takeEvery(SEARCH, trackFirstSearch);
 };
