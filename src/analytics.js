5c71dbba4d8dcd7c740264f4881dd3ad54065ef4
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2019-10-03 19:38:51 +0200

    [Analytics] Sender event når man tar i bruk søkeboksen første gang

--- src/analytics.js
+++ src/analytics.js
@@ -1,13 +1,22 @@
 import { takeEvery } from 'redux-saga/es/effects';
 import { ADD_TO_FAVOURITES_SUCCESS, REMOVE_FROM_FAVOURITES_SUCCESS } from './favourites/favouritesReducer';
 import { ADD_SAVED_SEARCH_SUCCESS } from './savedSearches/savedSearchesReducer';
+import { SET_SEARCH_STRING } from './search/searchQueryReducer';
 import { SEARCH } from './search/searchReducer';
 
-let ignoreFurtherEvents = false;
+const ignoreFurther = [];
 
 function analytics(...props) {
     if (window.ga) {
         window.ga(...props);
+    }
+}
+
+function once(...props) {
+    const key = props.join();
+    if(!ignoreFurther.includes(key)) {
+        ignoreFurther.push(key);
+        analytics(props);
     }
 }
 
@@ -24,10 +33,11 @@ function trackAddSavedSearchSuccess(action) {
 }
 
 function trackFirstSearch() {
-    if(!ignoreFurtherEvents) {
-        analytics('send', 'event', 'Ledige stillinger', 'Utført minst ett søk');
-        ignoreFurtherEvents = true;
-    }
+    once('send', 'event', 'Ledige stillinger', 'Utført minst ett søk');
+}
+
+function trackFirstInteractionWithSearchBox() {
+    once('send', 'event', 'Ledige stillinger', 'Tar i bruk søkeboks');
 }
 
 export const analyticsSaga = function* saga() {
@@ -35,4 +45,5 @@ export const analyticsSaga = function* saga() {
     yield takeEvery(REMOVE_FROM_FAVOURITES_SUCCESS, trackRemoveFromFavouritesSuccess);
     yield takeEvery(ADD_SAVED_SEARCH_SUCCESS, trackAddSavedSearchSuccess);
     yield takeEvery(SEARCH, trackFirstSearch);
+    yield takeEvery(SET_SEARCH_STRING, trackFirstInteractionWithSearchBox);
 };
