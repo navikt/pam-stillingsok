654642f7f0c0270c28681fcdcb0be57de800bd00
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2019-12-04 14:38:02 +0100

    [Fix] Legger til lenke for eksterne annonser i annonsevisningen

--- src/stilling/externalAd/ExternalAd.js
+++ src/stilling/externalAd/ExternalAd.js
@@ -1,29 +1,29 @@
+import { Normaltekst } from 'nav-frontend-typografi';
 import React from 'react';
 import PropTypes from 'prop-types';
 import './ExternalAd.less';
 import { isValidUrl } from '../../utils';
 
 export default function ExternalAd({ url }) {
-        return (
-            <section className="ExternalAd">
-                <h2>Denne annonsen er hentet fra FINN</h2>
-                <p>
-                    Du kan lese hele annonseteksten og søke på stillingen på finn.no
+    return (
+        <section className="ExternalAd">
+            <h2 className="ExternalAd__title">Denne annonsen er hentet fra FINN</h2>
+            <p className="ExternalAd__text">
+                Du kan lese hele annonseteksten og søke på stillingen på finn.no
+            </p>
+            {url && isValidUrl(url) && (
+                <p className="ExternalAd__link">
+                    <a
+                        className="link"
+                        href={url}
+                    >
+                        Åpne annonsen på FINN
+                    </a>
+                    <span className="ExternalAd__link__icon" aria-label="Ekstern lenke"/>
                 </p>
-                <p>
-                    {url && isValidUrl(url) && (
-                        <a
-                            className="ExternalAd__button Knapp Knapp--hoved"
-                            href={url}
-                        >
-                            <div className="ExternalAd__button-content">
-                                <span className="ExternalAd__button-icon" />Åpne annonsen på FINN
-                            </div>
-                        </a>
-                    )}
-                </p>
-            </section>
-        );
+            )}
+        </section>
+    );
 }
 
 ExternalAd.defaultProps = {
