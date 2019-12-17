e22406530f2066c261ce049dfeb6d752c1779172
otenav <ola.toft-eriksen@nav.no>
2019-12-17 14:25:51 +0100

    [Fix] Legger til flere CSP headere

--- server/server.js
+++ server/server.js
@@ -34,6 +34,7 @@ server.use(helmet.contentSecurityPolicy({
     directives: {
         defaultSrc: ["'none'"],
         baseUri: ["'none'"],
+        mediaSrc: ["'none'"],
         scriptSrc: ["'self'", 'https://www.google-analytics.com'],
         objectSrc: ["'none'"],
         frameAncestors: ["'none'"],
@@ -41,7 +42,8 @@ server.use(helmet.contentSecurityPolicy({
         styleSrc: ["'self'"],
         fontSrc: ["'self'", 'data:', 'https://fonts.gstatic.com'],
         imgSrc: ["'self'", 'data:', 'https://www.google-analytics.com'],
-        connectSrc: ["'self'", process.env.PAMADUSER_URL, 'https://www.google-analytics.com']
+        connectSrc: ["'self'", process.env.PAMADUSER_URL, 'https://www.google-analytics.com'],
+        frameSrc: ["'none'"]
     }
 }));
 
