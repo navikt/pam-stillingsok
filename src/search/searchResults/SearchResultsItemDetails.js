369ca86532b0885605a36910542205c0550c5508
otenav <ola.toft-eriksen@nav.no>
2020-01-21 09:31:41 +0100

    [Fix] Fjerner lenke-test for åpning av finn-annonser. Åpner alle finn-annonser i samme fane.

--- src/search/searchResults/SearchResultsItemDetails.js
+++ src/search/searchResults/SearchResultsItemDetails.js
@@ -9,69 +9,16 @@ import { CONTEXT_PATH } from '../../fasitProperties';
 import { formatISOString, isValidISOString } from '../../utils';
 import './SearchResultsItemDetails.less';
 
-const trackTest = (testVersion, source) => {
-    let action;
-    if (testVersion === 'a') {
-        action = "Åpnet annonse (Test A - Intern visning)"
-    } else if (testVersion === 'b') {
-        action = "Åpnet annonse (Test B - Ekstern visning, samme fane)"
-    } else if (testVersion === 'c') {
-        action = "Åpnet annonse (Test C - Ekstern visning, ny fane)"
-    } else {
-        action = "Åpnet annonse"
-    }
-
-    ga('send', 'event', {
-        eventCategory: 'Ledige stillinger > Lenketest',
-        eventAction: action,
-        eventLabel: source,
-        transport: 'beacon'
-    });
-};
-
-
-export function getTestVersion() {
-    if (window.testVersion === undefined) {
-        try {
-            let stored = localStorage.getItem("linkTestVersion");
-            if (stored && (stored === 'a' || stored === 'b' || stored === 'c')) {
-                window.testVersion = stored;
-            } else {
-                stored = ['a', 'b', 'c'][Math.floor(Math.random() * 3)];
-                localStorage.setItem("linkTestVersion", stored);
-                window.testVersion = stored;
-            }
-        } catch (e) {
-            window.testVersion = 'a';
-        }
-    }
-    return window.testVersion;
-}
-
-function LinkTestVersion({ children, stilling, isFinn, testVersion }) {
-    if (isFinn && testVersion === 'c') {
+function LinkToAd({ children, stilling, isFinn }) {
+    if (isFinn) {
         return (
-            <a href={`https://www.finn.no/${stilling.reference}`} className="SearchResultItem__link" target="_blank"
-               onClick={() => {
-                   trackTest(testVersion, "finn")
-               }}>
-                {children}
-            </a>
-        )
-    }
-    if (isFinn && testVersion === 'b') {
-        return (
-            <a href={`https://www.finn.no/${stilling.reference}`} className="SearchResultItem__link" onClick={() => {
-                trackTest(testVersion, "finn")
-            }}>
+            <a href={`https://www.finn.no/${stilling.reference}`} className="SearchResultItem__link">
                 {children}
             </a>
         )
     }
     return (
-        <Link to={`${CONTEXT_PATH}/stilling/${stilling.uuid}`} className="SearchResultItem__link" onClick={() => {
-            trackTest(testVersion, isFinn ? "finn" : "annen-kilde")
-        }}>
+        <Link to={`${CONTEXT_PATH}/stilling/${stilling.uuid}`} className="SearchResultItem__link">
             {children}
         </Link>
     )
@@ -87,11 +34,10 @@ export default function SearchResultsItemDetails({ stilling }) {
     }
     const location = getWorkLocation(stilling.properties.location, stilling.locationList);
     const employer = getEmployer(stilling);
-    const testVersion = getTestVersion();
     const isFinn = stilling.source && stilling.source.toLowerCase() === "finn";
 
     return (
-        <LinkTestVersion testVersion={testVersion} stilling={stilling} isFinn={isFinn}>
+        <LinkToAd stilling={stilling} isFinn={isFinn}>
             <Row className="SearchResultsItemDetails">
                 <Column xs="12" md="4">
                     {employer && (
@@ -127,7 +73,7 @@ export default function SearchResultsItemDetails({ stilling }) {
                         <Normaltekst className="SearchResultsItemDetails__applicationdue">
                             Søknadsfrist: {frist}
                         </Normaltekst>
-                        {isFinn && (testVersion === 'b' || testVersion === 'c') && (
+                        {isFinn && (
                             <Normaltekst className="SearchResultsItemDetails__external-link">
                                 Annonsen åpnes på FINN <span className="SearchResultsItemDetails__external-link-icon" aria-label="Ekstern lenke" />
                             </Normaltekst>
@@ -135,7 +81,7 @@ export default function SearchResultsItemDetails({ stilling }) {
                     </div>
                 </Column>
             </Row>
-        </LinkTestVersion>
+        </LinkToAd>
     );
 }
 
