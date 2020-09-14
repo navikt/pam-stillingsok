f83818e61b00efe08094579e45a9216dcd415207
Håkon Fredrik Melby <hakon.fredrik.melby@nav.no>
2020-09-14 14:30:36 +0200

    Regelbrudd checkboxer

--- src/rapporterAnnonse/RapporterAnnonse.js
+++ src/rapporterAnnonse/RapporterAnnonse.js
@@ -1,9 +1,59 @@
 import React from "react";
+import Container from "nav-frontend-grid/lib/container";
+import Sidetittel from "nav-frontend-typografi/lib/sidetittel";
+import {Link} from "react-router-dom";
+import Undertittel from "nav-frontend-typografi/lib/undertittel";
+import "./RapporterAnnonse.less";
+import Checkbox from "nav-frontend-skjema/lib/checkbox";
+import {useState} from 'react';
+
+const violationCategories = [
+    {label: "Diskriminerende innhold", key: "discrimination"},
+    {label: "Stillingen er ikke reell", key: "fake"},
+    {label: "Det er markedsføring", key: "marketing"},
+    {label: "Stillingen krever en form for betaling eller avgift fra søker", key: "scam"},
+]
 
 const RapporterAnnonse = () => {
-    return (
-        <div> Hei </div>
-    )
-}
+    const [violation, setViolation] = useState(false);
+    const [violationCategory, setViolationCategory] = useState(null);
 
-export default RapporterAnnonse
+    const onViolationCheck = () => {
+        setViolation(!violation);
+    }
+
+    const onViolationCategoryCheck = (e) => {
+        setViolationCategory(e.target.value);
+    }
+
+
+    return (
+        <Container className="RapporterAnnonse">
+            <Sidetittel>Rapporter annonse</Sidetittel>
+
+            <div className="report-form">
+                <Undertittel>Henveldensen gjelder</Undertittel>
+
+                <Checkbox name="regelbrudd" label="Regelbrudd" onChange={onViolationCheck} checked={violation === true}/>
+                <Link to="/">Les om gjeldende regler</Link>
+
+                {violation &&
+                violationCategories.map(c => {
+                    return (
+                        <Checkbox
+                            className="sub-checkbox"
+                            key={c.key}
+                            label={c.label}
+                            value={c.key}
+                            onChange={onViolationCategoryCheck}
+                            checked={violationCategory === c.key}
+                        />
+                    )
+                })
+                }
+            </div>
+        </Container>
+    )
+};
+
+export default RapporterAnnonse;
--- src/stilling/Stilling.js
+++ src/stilling/Stilling.js
@@ -196,7 +196,7 @@ const Stilling = ({ cachedStilling, error, getStilling, isFetchingStilling, matc
                                     </React.Fragment>
                                 )}
                                 <div className="Rapport__link">
-                                    <Link to="/stillinger/rapporter-annonse" aria-label="Rapporter annonse">
+                                    <Link to={CONTEXT_PATH + "/rapporter-annonse"} aria-label="Rapporter annonse">
                                     Rapporter annonse
                                     </Link>
                                 </div>
