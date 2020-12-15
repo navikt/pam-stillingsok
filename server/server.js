91aa72287c6fcaeade4e0ccc8e0c37e17c526e6c
Julian Torp <julian.torp@nav.no>
2020-12-15 20:11:05 +0100

    correct CSP

--- server/server.js
+++ server/server.js
@@ -75,7 +75,7 @@ server.use(helmet.contentSecurityPolicy({
         fontSrc: ["'self'", 'data:', 'https://fonts.gstatic.com', ...hotJarSources.font],
         imgSrc: ["'self'", 'data:', 'https://www.google-analytics.com', ...hotJarSources.img],
         connectSrc: ["'self'", process.env.PAMADUSER_URL, 'https://www.google-analytics.com', 'https://amplitude.nav.no', ...hotJarSources.connect],
-        frameSrc: ["'none'", ...hotJarSources.frame]
+        frameSrc: ["'self'", ...hotJarSources.frame]
     }
 }));
 
