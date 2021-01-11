748f9228b9b88beb706ea5c0fa877ada9ba2a91c
Julian Torp <julian.torp@nav.no>
2021-01-11 16:32:37 +0100

    add toggle to ad statistics link

--- src/common/components/AdStatisticsLink.js
+++ src/common/components/AdStatisticsLink.js
@@ -1,11 +1,24 @@
 import * as React from "react";
 import './AdStatisticsLink.less';
 import logAmplitudeEvent from "../../amplitudeTracker";
+import {useEffect, useState} from "react";
+import Cookies from "universal-cookie/index";
 
 const AD_STATISTICS_URL = 'https://data.nav.no/datapakke/703bc2e74c271ee895609ebd93c52460';
 
-const AdStatisticsLink = () => (
-    <a
+const AdStatisticsLink = () => {
+    const [showAdStatistics, setShowAdStatistics] = useState(false);
+
+    useEffect(() => {
+        const cookies = new Cookies();
+        setShowAdStatistics(cookies.get('showAdStatisticsLink') === 'true')
+    }, [])
+
+    if (!showAdStatistics) {
+        return null;
+    }
+
+    return (<a
         href={AD_STATISTICS_URL}
         className="AdStatisticsLink"
         target="_blank"
@@ -15,7 +28,7 @@ const AdStatisticsLink = () => (
             <span className="AdStatisticsLink__text">Se konkurransen på arbeidsplassen.no</span>
             <span className="Header__Lenkeikon"/>
         </div>
-    </a>
-);
+    </a>);
+}
 
 export default AdStatisticsLink;
