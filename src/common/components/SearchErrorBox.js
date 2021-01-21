08ef722110912d81f6f4b66f9b4fdf0f5c1ca157
Julian Torp <julian.torp@nav.no>
2021-01-21 14:45:03 +0100

    update text

--- src/common/components/SearchErrorBox.js
+++ src/common/components/SearchErrorBox.js
@@ -10,6 +10,7 @@ const SearchErrorBox = () => {
             <Undertittel>
                 Du kan dessverre ikke søke etter stillinger nå
             </Undertittel>
+            <br />
             <Normaltekst>
                 På grunn av en feil i våre systemer er ikke stillingssøket tilgjengelig.
                 Vi jobber raskt med å løse problemet og beklager ulempen dette vil medføre.
