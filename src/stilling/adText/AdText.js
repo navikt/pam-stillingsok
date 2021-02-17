960bc0e055d1db6ac8b7e7c6a280ae3a742aa6bf
Julian Torp <julian.torp@nav.no>
2021-02-17 10:02:39 +0100

    revert style change on søk på stillinger link

--- src/stilling/adText/AdText.js
+++ src/stilling/adText/AdText.js
@@ -20,19 +20,6 @@ const preprocessAd = (adText) => {
 
 export default function AdText({adText}) {
 
-    useEffect(() => {
-        const elements = document.querySelectorAll('a');
-        let el;
-        elements.forEach((it) => {
-            if (it.innerHTML === 'Søk på stillingen') el = it;
-        });
-        if (el) {
-            el.classList.add('Knapp');
-            el.classList.add('Knapp--hoved');
-            el.classList.add('SokPaaStilling--knapp');
-        }
-    }, []);
-
     if (adText) {
         const preprocessedAd = preprocessAd(adText);
         return (
