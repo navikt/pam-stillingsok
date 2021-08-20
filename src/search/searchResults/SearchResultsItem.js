30f19af2ca9ed2fc7bf58b149c1b097e397636ec
otenav <ola.toft-eriksen@nav.no>
2021-08-20 13:22:35 +0200

    Fjern favoritt stjernen i trefflisten, beholder kun lagre som favoritt inne i annonsevisning.

--- src/search/searchResults/SearchResultsItem.js
+++ src/search/searchResults/SearchResultsItem.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import ToggleFavouriteStar from '../../favourites/toggleFavoriteButton/ToggleFavouriteStar';
 import './SearchResultsItem.less';
 import SearchResultsItemDetails from './SearchResultsItemDetails';
 
@@ -8,7 +7,6 @@ export default function SearchResultItem({ stilling }) {
     return (
         <div className="SearchResultItem">
             <SearchResultsItemDetails stilling={stilling} />
-            <ToggleFavouriteStar uuid={stilling.uuid} className="SearchResultItem__favourite" />
         </div>
     );
 }
