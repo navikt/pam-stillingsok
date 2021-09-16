8449e9bbc968a356b407b2be860f0a0b9d690bc2
otenav <ola.toft-eriksen@nav.no>
2021-09-16 15:50:21 +0200

    Rydder litt i css

--- src/savedSearches/totalSavedSearch/TotalSavedSearch.js
+++ src/savedSearches/totalSavedSearch/TotalSavedSearch.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Element, Normaltekst } from 'nav-frontend-typografi';
 import './TotalSavedSearch.less';
 
 export default function TotalSavedSearch({ total }) {
     return (
-        <div className="TotalSavedSearch">
-            <h2 className="TotalSavedSearch__label">{`Antall lagrede søk: ${total}`}</h2>
-        </div>
+        <h2 className="TotalSavedSearch">
+            {total !== 1 ? `${total} lagrede søk` : '1 lagret søk'}
+        </h2>
     );
 }
 
