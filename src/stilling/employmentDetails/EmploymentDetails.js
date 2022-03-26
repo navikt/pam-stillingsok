cb16c74623bf48ff4a58bc02133c6784a6908047
otenav <ola.toft-eriksen@nav.no>
2022-03-26 09:24:59 +0100

    [CSS] Legger til ikoner for å lettere skille mellom annonseteksten og dataene som plassert i høyre kolonne

--- src/stilling/employmentDetails/EmploymentDetails.js
+++ src/stilling/employmentDetails/EmploymentDetails.js
@@ -11,7 +11,10 @@ export default function EmploymentDetails({ stilling }) {
 
     return (
         <div className="EmploymentDetails detail-section">
-            <Undertittel className="EmploymentDetails__head detail-section__head">Om stillingen</Undertittel>
+            <Undertittel className="EmploymentDetails__head detail-section__head">
+                <svg aria-hidden="true" width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" focusable="false" role="img"><path fillRule="evenodd" clipRule="evenodd" d="M14 3h-4a1 1 0 00-1 1v1h6V4a1 1 0 00-1-1zM7 4v1H1a1 1 0 00-1 1v7a2 2 0 001 1.732V21a2 2 0 002 2h18a2 2 0 002-2v-6.268A2 2 0 0024 13V6a1 1 0 00-1-1h-6V4a3 3 0 00-3-3h-4a3 3 0 00-3 3zm0 3h15v6h-9v-1a1 1 0 10-2 0v1H2V7h5zm4 8H3v6h18v-6h-8v1a1 1 0 11-2 0v-1z" fill="currentColor"></path></svg>
+                Om stillingen
+            </Undertittel>
             <div className="detail-section__body">
                 <dl className="dl-flex typo-normal">
                     {location && [
