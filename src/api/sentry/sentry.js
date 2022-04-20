8ad3f4dc3cbe85f0d7f9f4c4be5596926d6546cd
otenav <ola.toft-eriksen@nav.no>
2022-04-20 20:37:58 +0200

    cache bust

--- src/api/sentry/sentry.js
+++ src/api/sentry/sentry.js
@@ -7,7 +7,6 @@ export default function initSentry() {
         dsn: "https://76170ea4b79246638c1d9eb1c0e4fca9@sentry.gc.nav.no/37",
         beforeSend(event, hint) {
             const error = hint.originalException;
-            debugger;
             if (error && blockedStatusCodes.includes(error.statusCode)) {
                 return null; // event will be discarded
             } else {
