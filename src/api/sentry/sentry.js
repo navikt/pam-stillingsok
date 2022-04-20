ad88d60306091a119107a7086fb20625b61a5761
otenav <ola.toft-eriksen@nav.no>
2022-04-20 22:48:28 +0200

    Ignore some known TypeErrors when logging to Sentry

--- src/api/sentry/sentry.js
+++ src/api/sentry/sentry.js
@@ -1,14 +1,29 @@
 import * as Sentry from "@sentry/browser";
 
-const blockedStatusCodes = [0, 401, 404]
+const ignoreStatusCodes = [0, 401, 404];
+
+// TypeErrors can for example occur when user
+// navigates away from app while a fetch call is still running
+const ignoreTypeErrors = [
+    "TypeError: Failed to fetch",
+    "TypeError: Load failed",
+    "TypeError: NetworkError when attempting to fetch resource.",
+    "TypeError: cancelled",
+    "TypeError: avbrutt",
+    "TypeError: cancelado",
+    "TypeError: anulowane",
+    "TypeError: avbruten",
+    "TypeError: anulat"
+];
 
 export default function initSentry() {
     Sentry.init({
         dsn: "https://76170ea4b79246638c1d9eb1c0e4fca9@sentry.gc.nav.no/37",
-        blacklistUrls: [new RegExp("localhost"), new RegExp("arbeidsplassen-q.nav.no")],
+        denyUrls: [new RegExp("localhost"), new RegExp("arbeidsplassen-q.nav.no")],
+        ignoreErrors: [...ignoreTypeErrors],
         beforeSend(event, hint) {
             const error = hint.originalException;
-            if (error && error.statusCode !== undefined && blockedStatusCodes.includes(error.statusCode)) {
+            if (error && error.statusCode !== undefined && ignoreStatusCodes.includes(error.statusCode)) {
                 return null; // event will be discarded
             } else {
                 return event;
