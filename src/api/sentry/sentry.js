7a590951c0871159efa8cabcd8cf88b212ee1310
otenav <ola.toft-eriksen@nav.no>
2022-04-20 15:54:40 +0200

    Fornekler blokkering av 401 og 404 errors

--- src/api/sentry/sentry.js
+++ src/api/sentry/sentry.js
@@ -7,7 +7,7 @@ export default function initSentry() {
         dsn: "https://76170ea4b79246638c1d9eb1c0e4fca9@sentry.gc.nav.no/37",
         blacklistUrls: [new RegExp("localhost"), new RegExp("arbeidsplassen-q.nav.no")],
         beforeSend(event, hint) {
-            const error = hint.syntheticException;
+            const error = hint.originalException;
             if (error && error.statusCode && blockedAprErrorStatusCodes.includes(error.statusCode)) {
                 return null;
             } else {
