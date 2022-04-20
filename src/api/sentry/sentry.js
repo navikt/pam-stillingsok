f84a20de60919139bc53a35934388a2a3c42d500
otenav <ola.toft-eriksen@nav.no>
2022-04-20 20:37:27 +0200

    cache bust

--- src/api/sentry/sentry.js
+++ src/api/sentry/sentry.js
@@ -5,11 +5,10 @@ const blockedStatusCodes = [0, 401, 404]
 export default function initSentry() {
     Sentry.init({
         dsn: "https://76170ea4b79246638c1d9eb1c0e4fca9@sentry.gc.nav.no/37",
-        blacklistUrls: [new RegExp("localhost"), new RegExp("arbeidsplassen-q.nav.no")],
         beforeSend(event, hint) {
             const error = hint.originalException;
-            console.log(error)
-            if (error && error.statusCode && blockedStatusCodes.includes(error.statusCode)) {
+            debugger;
+            if (error && blockedStatusCodes.includes(error.statusCode)) {
                 return null; // event will be discarded
             } else {
                 return event;
