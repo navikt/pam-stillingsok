eb7a00301c682276e0e01196ce7a7270e0dec9d7
otenav <ola.toft-eriksen@nav.no>
2022-04-22 00:42:54 +0200

    cache bust

--- src/hooks/useRobotsNoIndexMetaTag.js
+++ src/hooks/useRobotsNoIndexMetaTag.js
@@ -1,21 +1,21 @@
 import { useEffect } from "react";
 
 function removeRobotsMetaTag() {
-    let metaRobots = document.querySelector("meta[name=robots]");
+    let metaRobots = document.head.querySelector("meta[name=robots]");
     if (metaRobots) {
-        document.querySelector("head").removeChild(metaRobots);
+        document.head.removeChild(metaRobots);
     }
 }
 
 function addRobotsNoIndexMetaTag() {
     const content = "noindex";
-    let metaRobots = document.querySelector("meta[name=robots]");
+    let metaRobots = document.head.querySelector("meta[name=robots]");
 
     if (!metaRobots) {
         metaRobots = document.createElement("meta");
         metaRobots.setAttribute("name", "robots");
         metaRobots.setAttribute("content", content);
-        document.querySelector("head").appendChild(metaRobots);
+        document.head.appendChild(metaRobots);
     } else {
         metaRobots.setAttribute("content", content);
     }
