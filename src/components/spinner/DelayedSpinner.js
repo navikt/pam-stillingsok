0b5ef6f7237c7dfb58cfb3c4bd6cc6ceae6a0efa
otenav <ola.toft-eriksen@nav.no>
2022-04-22 15:28:55 +0200

    DelayedSpinner did get delay parameter

--- src/components/spinner/DelayedSpinner.js
+++ src/components/spinner/DelayedSpinner.js
@@ -6,7 +6,6 @@ import "./DelayedSpinner.less";
  * While doing fetch calls on a fast network, a spinner will normally be
  * visible just for a few milliseconds. To reduce visual noise, it can
  * be delayed, and appear only for slower fetch calls.
- * @param delay - default 1000ms
  */
 export default function DelayedSpinner() {
     const [showSpinner, setShowSpinner] = useState(false);
