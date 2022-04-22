053112e391cf15b000570f1c1effa44dd155d8ac
otenav <ola.toft-eriksen@nav.no>
2022-04-22 08:43:56 +0200

    Legger til try/catch, og logger error til sentry.

--- src/hooks/useRobotsNoIndexMetaTag.js
+++ src/hooks/useRobotsNoIndexMetaTag.js
@@ -1,23 +1,33 @@
 import { useEffect } from "react";
+import { captureMessage } from "@sentry/browser";
 
 function removeRobotsMetaTag() {
-    let metaRobots = document.head.querySelector("meta[name=robots]");
-    if (metaRobots) {
-        document.head.removeChild(metaRobots);
+    let metaRobots;
+    try {
+        metaRobots = document.head.querySelector("meta[name=robots]");
+        if (metaRobots) {
+            document.head.removeChild(metaRobots);
+        }
+    } catch (e) {
+        captureMessage("Error in removeRobotsMetaTag. metaRobots is " + metaRobots);
     }
 }
 
 function addRobotsNoIndexMetaTag() {
-    const content = "noindex";
-    let metaRobots = document.head.querySelector("meta[name=robots]");
+    try {
+        const content = "noindex";
+        let metaRobots = document.head.querySelector("meta[name=robots]");
 
-    if (!metaRobots) {
-        metaRobots = document.createElement("meta");
-        metaRobots.setAttribute("name", "robots");
-        metaRobots.setAttribute("content", content);
-        document.head.appendChild(metaRobots);
-    } else {
-        metaRobots.setAttribute("content", content);
+        if (!metaRobots) {
+            metaRobots = document.createElement("meta");
+            metaRobots.setAttribute("name", "robots");
+            metaRobots.setAttribute("content", content);
+            document.head.appendChild(metaRobots);
+        } else {
+            metaRobots.setAttribute("content", content);
+        }
+    } catch (e) {
+        captureMessage("Error in addRobotsNoIndexMetaTag");
     }
 }
 
