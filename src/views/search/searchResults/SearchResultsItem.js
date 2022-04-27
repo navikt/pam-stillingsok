a7332e162a4b99fc1057fff74e7bf9dddf4aaae1
otenav <ola.toft-eriksen@nav.no>
2022-04-27 20:55:39 +0200

    Improve aria-label for ad articles

--- src/views/search/searchResults/SearchResultsItem.js
+++ src/views/search/searchResults/SearchResultsItem.js
@@ -4,10 +4,31 @@ import SearchResultsItemDetails from "./SearchResultsItemDetails";
 import FavouritesButton from "../../favourites/FavouritesButton";
 import "./SearchResultsItem.less";
 import getEmployer from "../../../../server/common/getEmployer";
+import getWorkLocation from "../../../../server/common/getWorkLocation";
+
+function toLabel(stilling) {
+    const fragments = [];
+    const location = getWorkLocation(stilling.properties.location, stilling.locationList);
+    const employer = getEmployer(stilling);
+    if(stilling.title) {
+        fragments.push(stilling.title);
+    }
+    if (stilling.properties.jobtitle && stilling.title !== stilling.properties.jobtitle) {
+        fragments.push(stilling.properties.jobtitle);
+    }
+    if(employer) {
+        fragments.push(employer);
+    }
+    if(location) {
+        fragments.push(location);
+    }
+    return fragments.join(". ");
+}
 
 export default function SearchResultItem({ stilling }) {
+    const label = toLabel(stilling);
     return (
-        <article aria-label={`${stilling.title}, ${getEmployer(stilling)}`} className="SearchResultItem">
+        <article aria-label={label} className="SearchResultItem">
             <SearchResultsItemDetails stilling={stilling} />
             <FavouritesButton
                 showText={false}
