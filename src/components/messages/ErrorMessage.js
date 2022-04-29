b5212caa97f4c7ce3fef29faa51b2273cdbc76a3
otenav <ola.toft-eriksen@nav.no>
2022-04-29 10:15:26 +0200

    Add role alert to ErrorMessage.js

--- src/components/messages/ErrorMessage.js
+++ src/components/messages/ErrorMessage.js
@@ -3,7 +3,7 @@ import "./ErrorMessage.less";
 
 function ErrorMessage() {
     return (
-        <section className="ErrorMessage">
+        <section className="ErrorMessage" role="alert">
             <h2 className="ErrorMessage__h2">Det oppsto en feil</h2>
             <p className="ErrorMessage__text">Forsøk å laste inn siden på nytt</p>
         </section>
