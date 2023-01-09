f13d7f7fb9e9e955a9c5c64056326fb57239ba86
otenav <ola.toft-eriksen@nav.no>
2023-01-09 13:23:37 +0100

    Gjør klar banner for promotere Superrask søknad.

---
+++ src/pages/interestForm/SuperraskSoknadBanner.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./SuperraskSoknadBanner.less";
+
+function SuperraskSoknadBanner() {
+    return (
+        <section className="SuperraskSoknadBanner">
+            <h3 className="Search__h2">Nyhet! Superrask søknad</h3>
+            <p>
+                Vis frem deg selv og din erfaring enkelt og raskt når du søker på stillinger med superrask søknad.
+            </p>
+            <a href="/slik-fungerer-superrask-soknad" className="link">Hvordan fungerer superrask søknad?</a>
+        </section>
+    );
+}
+
+
+export default SuperraskSoknadBanner;
--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -26,6 +26,7 @@ import { useHistory } from "react-router";
 import SearchResult from "./searchResult/SearchResult";
 import H1WithAutoFocus from "../../components/h1WithAutoFocus/H1WithAutoFocus";
 import EventBanner from "./event/EventBanner";
+import SuperraskSoknadBanner from "../interestForm/SuperraskSoknadBanner";
 
 const Search = () => {
     const { authenticationStatus } = useContext(AuthenticationContext);
