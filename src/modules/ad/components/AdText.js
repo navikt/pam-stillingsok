beea19d780aa3a30b1a4734fa24c94e01dca6c60
otenav <ola.toft-eriksen@nav.no>
2023-01-25 19:10:15 +0100

    Catch error extracting emails

--- src/modules/ad/components/AdText.js
+++ src/modules/ad/components/AdText.js
@@ -6,14 +6,18 @@ import { containsEmail, extractEmail, isValidEmail, mailtoInString } from "../..
 
 const preprocessAd = (adText) => {
     if (containsEmail(adText)) {
-        const extractedEmails = [...extractEmail(adText)];
-        let preprocessedAd = adText.replace(/&#64;/g, "@");
-        extractedEmails.forEach((it) => {
-            if (isValidEmail(it) && !mailtoInString(preprocessedAd, it)) {
-                preprocessedAd = preprocessedAd.replace(it, `<a rel="nofollow" href="mailto:${it}">${it}</a>`);
-            }
-        });
-        return preprocessedAd;
+        try {
+            const extractedEmails = [...extractEmail(adText)];
+            let preprocessedAd = adText.replace(/&#64;/g, "@");
+            extractedEmails.forEach((it) => {
+                if (isValidEmail(it) && !mailtoInString(preprocessedAd, it)) {
+                    preprocessedAd = preprocessedAd.replace(it, `<a rel="nofollow" href="mailto:${it}">${it}</a>`);
+                }
+            });
+            return preprocessedAd;
+        } catch (err) {
+            return adText;
+        }
     }
     return adText;
 };
