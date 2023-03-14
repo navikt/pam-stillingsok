6bc5186933d5af497efe421b09c57525e6f7bc29
haugse <sara.haugse@gmail.com>
2023-03-14 15:16:19 +0100

    sjekk header

--- server/api/userApiProxyConfig.js
+++ server/api/userApiProxyConfig.js
@@ -73,7 +73,7 @@ const setCallId = async (req, res, next) => {
     //sjekker om headeren finnes
     if (callId === undefined || callId === null) {
         callId = uuidv4();
-        console.log(`Lager en callId ${callId} for ${req.url} med ${req.method}, cookies: ${req.cookies}`)
+        console.log(`Lager en callId ${callId} for ${req.url} med ${req.method}, cookies: ${req.headers}`)
     }
     req.headers['nav-callid'] = callId;
     next();
