a8ae06271953de1c8b638be33325e13a93b59b41
haugse <sara.haugse@gmail.com>
2023-03-14 15:28:48 +0100

    sjekk xsrf-token

--- server/api/userApiProxyConfig.js
+++ server/api/userApiProxyConfig.js
@@ -73,7 +73,7 @@ const setCallId = async (req, res, next) => {
     //sjekker om headeren finnes
     if (callId === undefined || callId === null) {
         callId = uuidv4();
-        console.log(`Lager en callId ${callId} for ${req.url} med ${req.method}, cookies: ${req.headers}`)
+        console.log(`Lager en callId ${callId} for ${req.url} med ${req.method}, cookies: ${req.headers['xsrf-token-arbeidsplassen']}`)
     }
     req.headers['nav-callid'] = callId;
     next();
