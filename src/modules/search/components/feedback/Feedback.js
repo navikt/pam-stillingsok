ee4ad2cd84da4f8a6df400920ac54f997f3c9176
otenav <ola.toft-eriksen@nav.no>
2023-04-27 14:54:20 +0200

    Rating as grid

--- src/modules/search/components/feedback/Feedback.js
+++ src/modules/search/components/feedback/Feedback.js
@@ -33,7 +33,7 @@ function Feedback() {
             {!hasGivenRating ? (
                 <div className="Feedback__options">
                     <Option rating={1} text="Dårlig" emoji={<FaceFrownIcon aria-hidden="true" />} />
-                    <Option rating={2} text="Mangler en del" emoji={<FaceIcon aria-hidden="true" />} />
+                    <Option rating={2} text="Hverken eller" emoji={<FaceIcon aria-hidden="true" />} />
                     <Option rating={3} text="Bra!" emoji={<FaceSmileIcon aria-hidden="true" />} />
                 </div>
             ) : (
