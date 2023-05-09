28b2de649db900aee937043027346a3222f04658
Audun Skjelvan <audun.skjelvan@nav.no>
2023-05-09 10:38:16 +0200

    styling

--- src/modules/auth/components/RequiresAuthentication.js
+++ src/modules/auth/components/RequiresAuthentication.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { BodyLong, Button, Heading } from "@navikt/ds-react";
+import { EnterIcon } from "@navikt/aksel-icons";
 import { AuthenticationContext, AuthenticationStatus } from "../contexts/AuthenticationProvider";
 import DelayedSpinner from "../../../common/components/spinner/DelayedSpinner";
 import ErrorMessage from "../../../common/components/messages/ErrorMessage";
@@ -24,12 +25,15 @@ function RequiresAuthentication({ children, onCancel }) {
                 <Heading level="2" size="large" spacing>
                     Du må først logge inn
                 </Heading>
-                <BodyLong className="LoginRequiredMessage__text">
+                <BodyLong className="LoginRequiredMessage__text mb-2">
                     Du bruker BankID for å logge inn på arbeidsplassen.no
                 </BodyLong>
-                <LoginBubble />
+                <div className="mb-2">
+                    <LoginBubble />
+                </div>
+                
                 <div className="login-buttons-wrapper">
-                    <Button variant="primary" onClick={login}>
+                    <Button variant="primary" icon={<EnterIcon aria-hidden="true" />} onClick={login}>
                         Logg inn
                     </Button>
                     
