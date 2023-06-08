d4d711208e8c6b256602dacad68a69cb0c215256
Audun Skjelvan <audun.skjelvan@nav.no>
2023-06-08 18:08:36 +0200

    mobile and desktop view

--- src/modules/search/components/searchForm/filters/Filters.jsx
+++ src/modules/search/components/searchForm/filters/Filters.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Tabs, Heading } from "@navikt/ds-react";
 import Published from "./Published";
+import CriteriaPanel from "./CriteriaPanel";
 import Counties from "./Locations";
 import Occupations from "./Occupations";
 import Extent from "./Extent";
@@ -10,68 +11,120 @@ import EngagementType from "./Engagement";
 
 function Filters({ query, dispatchQuery, initialSearchResult, searchResult }) {
     return (
-        <div className="Filters">
-            <Tabs defaultValue="sted">
-                <Tabs.List>
-                    <Tabs.Tab value="sted" label="Sted" />
-                    <Tabs.Tab value="yrke" label="Yrke" />
-                    <Tabs.Tab value="andre" label="Andre filtre" />
-                </Tabs.List>
-                <Tabs.Panel value="sted" className="h-24 w-full bg-gray-50 p-4">
+        <>
+            <div className="Filters Filters__desktop">
+                <div className="Filter__section">
+                    <Published
+                        query={query}
+                        dispatch={dispatchQuery}
+                        initialValues={initialSearchResult.aggregations.published}
+                        updatedValues={searchResult && searchResult.aggregations.published}
+                    />
+                </div>
+                <CriteriaPanel title="Sted" panelId="sted">
                     <Counties
                         query={query}
                         dispatch={dispatchQuery}
                         initialValues={initialSearchResult}
                         updatedValues={searchResult}
                     />
-                </Tabs.Panel>
-                <Tabs.Panel value="yrke" className="h-24 w-full bg-gray-50 p-4">
+                </CriteriaPanel>
+                <CriteriaPanel title="Yrke" panelId="yrke">
                     <Occupations
                         query={query}
                         dispatch={dispatchQuery}
                         initialValues={initialSearchResult.aggregations.occupationFirstLevels}
                         updatedValues={searchResult && searchResult.aggregations.occupationFirstLevels}
                     />
-                </Tabs.Panel>
-                <Tabs.Panel value="andre" className="h-24  w-full bg-gray-50 p-4">
-                    <div className="Filter__section">
-                        <Published
-                            query={query}
-                            dispatch={dispatchQuery}
-                            initialValues={initialSearchResult.aggregations.published}
-                            updatedValues={searchResult && searchResult.aggregations.published}
-                        />
-                    </div>
-                    <Heading level="2" size="small" spacing>
-                        Heltid/deltid
-                    </Heading>
+                </CriteriaPanel>
+                <CriteriaPanel title="Heltid/deltid" panelId="extent">
                     <Extent
                         query={query}
                         dispatch={dispatchQuery}
                         initialValues={initialSearchResult.aggregations.extent}
                         updatedValues={searchResult && searchResult.aggregations.extent}
                     />
-                    <Heading level="2" size="small" spacing>
-                        Sektor
-                    </Heading>
+                </CriteriaPanel>
+                <CriteriaPanel title="Sektor" panelId="sector">
                     <Sector
                         query={query}
                         dispatch={dispatchQuery}
                         initialValues={initialSearchResult.aggregations.sector}
                         updatedValues={searchResult && searchResult.aggregations.sector}
                     />
-                    <Heading level="2" size="small" spacing>
-                        Ansettelsesform
-                    </Heading>
+                </CriteriaPanel>
+                <CriteriaPanel title="Ansettelsesform" panelId="engagementType">
                     <EngagementType
                         query={query}
                         dispatch={dispatchQuery}
                         initialValues={initialSearchResult.aggregations.engagementTypes}
                         updatedValues={searchResult && searchResult.aggregations.engagementTypes}
                     />
-                </Tabs.Panel>
-            </Tabs>
-        </div>
+                </CriteriaPanel>
+            </div>
+            <div className="Filters Filters__mobile">
+                <Tabs defaultValue="sted">
+                    <Tabs.List>
+                        <Tabs.Tab value="sted" label="Sted" />
+                        <Tabs.Tab value="yrke" label="Yrke" />
+                        <Tabs.Tab value="andre" label="Andre filtre" />
+                    </Tabs.List>
+                    <Tabs.Panel value="sted" className="mt-2 h-24 w-full bg-gray-50 p-4">
+                        <Counties
+                            query={query}
+                            dispatch={dispatchQuery}
+                            initialValues={initialSearchResult}
+                            updatedValues={searchResult}
+                        />
+                    </Tabs.Panel>
+                    <Tabs.Panel value="yrke" className="mt-2 h-24 w-full bg-gray-50 p-4">
+                        <Occupations
+                            query={query}
+                            dispatch={dispatchQuery}
+                            initialValues={initialSearchResult.aggregations.occupationFirstLevels}
+                            updatedValues={searchResult && searchResult.aggregations.occupationFirstLevels}
+                        />
+                    </Tabs.Panel>
+                    <Tabs.Panel value="andre" className="mt-2 h-24  w-full bg-gray-50 p-4">
+                        <div className="Filter__section">
+                            <Published
+                                query={query}
+                                dispatch={dispatchQuery}
+                                initialValues={initialSearchResult.aggregations.published}
+                                updatedValues={searchResult && searchResult.aggregations.published}
+                            />
+                        </div>
+                        <Heading level="2" size="small" spacing>
+                            Heltid/deltid
+                        </Heading>
+                        <Extent
+                            query={query}
+                            dispatch={dispatchQuery}
+                            initialValues={initialSearchResult.aggregations.extent}
+                            updatedValues={searchResult && searchResult.aggregations.extent}
+                        />
+                        <Heading level="2" size="small" spacing>
+                            Sektor
+                        </Heading>
+                        <Sector
+                            query={query}
+                            dispatch={dispatchQuery}
+                            initialValues={initialSearchResult.aggregations.sector}
+                            updatedValues={searchResult && searchResult.aggregations.sector}
+                        />
+                        <Heading level="2" size="small" spacing>
+                            Ansettelsesform
+                        </Heading>
+                        <EngagementType
+                            query={query}
+                            dispatch={dispatchQuery}
+                            initialValues={initialSearchResult.aggregations.engagementTypes}
+                            updatedValues={searchResult && searchResult.aggregations.engagementTypes}
+                        />
+                    </Tabs.Panel>
+                </Tabs>
+            </div>
+        </>
     );
 }
 
