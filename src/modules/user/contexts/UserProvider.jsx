510f36c76bd30db04996002d1b7659bfcef71eea
Khiem Diep <khiem.harald.tu.diep@nav.no>
2023-06-15 14:44:25 +0200

    fix bug

--- src/modules/user/contexts/UserProvider.jsx
+++ src/modules/user/contexts/UserProvider.jsx
@@ -63,12 +63,11 @@ function UserProvider({ children }) {
     }, [userResponse]);
 
     useEffect(() => {
-        if (
-            authenticationStatus === AuthenticationStatus.NOT_FETCHED ||
-            authenticationStatus === AuthenticationStatus.IS_FETCHING
-        )
-            return;
-        setAuthenticatedStatus(authenticationStatus === AuthenticationStatus.IS_AUTHENTICATED);
+        if (authenticationStatus === AuthenticationStatus.IS_AUTHENTICATED) {
+            setAuthenticatedStatus(true);
+        } else if (authenticationStatus === AuthenticationStatus.NOT_AUTHENTICATED) {
+            setAuthenticatedStatus(false);
+        }
     }, [authenticationStatus]);
 
     return (
