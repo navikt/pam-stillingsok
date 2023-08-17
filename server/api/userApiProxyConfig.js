a9fc2d4467275e86db95ca3371d5f05001d6b654
haugse <sara.haugse@gmail.com>
2023-08-17 08:55:26 +0200

    Fikset logger og lint-feil

--- server/api/userApiProxyConfig.js
+++ server/api/userApiProxyConfig.js
@@ -1,84 +1,72 @@
-const {createProxyMiddleware} = require('http-proxy-middleware');
-const {v4: uuidv4} = require("uuid");
-const {getTokenX} = require("../tokenX/tokenXUtils");
-const isLocal = process.env.ARBEIDSPLASSEN_URL && process.env.ARBEIDSPLASSEN_URL.includes('localhost') || process.env.IS_LOCAL === 'true';
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const { v4: uuidv4 } = require("uuid");
+const { getTokenX } = require("../tokenX/tokenXUtils");
+const { logger } = require("../common/logger");
 
-let audience = process.env.ADUSER_AUDIENCE;
+const isLocal =
+    (process.env.ARBEIDSPLASSEN_URL && process.env.ARBEIDSPLASSEN_URL.includes("localhost")) ||
+    process.env.IS_LOCAL === "true";
 
-const origins = ['/stillinger/api/v1/user', '/stillinger/api/v1/userfavouriteads/:uuid',
-                '/stillinger/api/v1/userfavouriteads', '/stillinger/api/v1/reportposting',
-                '/stillinger/api/v1/savedsearches/:uuid', '/stillinger/api/v1/savedsearches']
+const audience = process.env.ADUSER_AUDIENCE;
 
-const setUpAduserApiProxy = (server) => {
-    origins.forEach(origin => {
-        server.get(origin,
-            setCallId,
-            setTokenX,
-            setupProxy
-        );
-        server.delete(origin,
-            setCallId,
-            setTokenX,
-            setupProxy
-        );
-        server.post(origin,
-            setCallId,
-            setTokenX,
-            setupProxy
-        );
-        server.put(origin,
-            setCallId,
-            setTokenX,
-            setupProxy
-        );
-    });
-}
-const getToken = async (accessToken) => {
-    return await getTokenX(accessToken, audience);
-}
+const origins = [
+    "/stillinger/api/v1/user",
+    "/stillinger/api/v1/userfavouriteads/:uuid",
+    "/stillinger/api/v1/userfavouriteads",
+    "/stillinger/api/v1/reportposting",
+    "/stillinger/api/v1/savedsearches/:uuid",
+    "/stillinger/api/v1/savedsearches",
+];
 
-const restream = (proxyReq, req, res, options) => {
-    if (req.body && (req.method === 'PUT' || req.method === 'POST')){
-        let bodyData = JSON.stringify(req.body);
+const setCallId = async (req, res, next) => {
+    let callId = req.headers["nav-callid"];
+
+    // sjekker om headeren finnes
+    if (callId === undefined || callId === null) {
+        callId = uuidv4();
+        logger.info(`Lager en callId ${callId} for ${req.url} med ${req.method}`);
+    }
+    req.headers["nav-callid"] = callId;
+    next();
+};
+const getToken = async (accessToken) => getTokenX(accessToken, audience);
+
+const setTokenX = async (req, res, next) => {
+    if (req.headers.authorization) {
+        const accessToken = req.headers.authorization.split(" ")[1];
+        const tokenX = await getToken(accessToken);
+        req.headers.authorization = `Bearer ${tokenX.access_token}`;
+    } else {
+        res.redirect("/oauth2/login");
+    }
+    next();
+};
+const restream = (proxyReq, req) => {
+    if (req.body && (req.method === "PUT" || req.method === "POST")) {
+        const bodyData = JSON.stringify(req.body);
         // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
-        proxyReq.setHeader('Content-Type','application/json');
-        proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+        proxyReq.setHeader("Content-Type", "application/json");
+        proxyReq.setHeader("Content-Length", Buffer.byteLength(bodyData));
         // stream the content
         proxyReq.write(bodyData);
     }
 };
 
-const setTokenX = async (req, res, next) => {
-    if(req.headers.authorization) {
-        const accessToken = req.headers.authorization.split(' ')[1];
-        const tokenX = await getToken(accessToken);
-        req.headers['authorization'] = `Bearer ${tokenX.access_token}`;
-    }
-    else {
-        res.redirect('/oauth2/login')
-    }
-    next();
-}
+const setupProxy = createProxyMiddleware({
+    target: process.env.PAMADUSER_URL,
+    pathRewrite: { "^/stillinger/": "/" },
+    changeOrigin: true,
+    secure: !isLocal,
+    onProxyReq: restream,
+});
 
-const setupProxy =
-    createProxyMiddleware({
-        target: process.env.PAMADUSER_URL,
-        pathRewrite: {'^/stillinger/': '/'},
-        changeOrigin: true,
-        secure: !isLocal,
-        onProxyReq: restream
+const setUpAduserApiProxy = (server) => {
+    origins.forEach((origin) => {
+        server.get(origin, setCallId, setTokenX, setupProxy);
+        server.delete(origin, setCallId, setTokenX, setupProxy);
+        server.post(origin, setCallId, setTokenX, setupProxy);
+        server.put(origin, setCallId, setTokenX, setupProxy);
     });
+};
 
-const setCallId = async (req, res, next) => {
-    let callId = req.headers['nav-callid'];
-
-    //sjekker om headeren finnes
-    if (callId === undefined || callId === null) {
-        callId = uuidv4();
-        console.log(`Lager en callId ${callId} for ${req.url} med ${req.method}`)
-    }
-    req.headers['nav-callid'] = callId;
-    next();
-}
-
-module.exports = setUpAduserApiProxy
+module.exports = setUpAduserApiProxy;
