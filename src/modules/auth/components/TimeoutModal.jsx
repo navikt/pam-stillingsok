c5e1106449be3f1e790982fb0e6b219c2e3013aa
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2023-09-22 16:20:26 +0200

    Refeshtoken wip 2

--- src/modules/auth/components/TimeoutModal.jsx
+++ src/modules/auth/components/TimeoutModal.jsx
@@ -1,34 +1,63 @@
 import React, {useContext} from "react";
 import PropTypes from "prop-types";
 import {BodyLong, Button, Heading, Modal} from "@navikt/ds-react";
-import {EnterIcon} from "@navikt/aksel-icons";
 import {AuthenticationContext} from "../contexts/AuthenticationProvider";
 import LoginBubble from "../../../common/components/icons/LoginBubble";
+import "./TimeoutModal.css";
 
-function TimeoutModal({ isSessionExpiring, isSessionTimingOut, onCloseClick }) {
-    const { login } = useContext(AuthenticationContext);
-    const message = isSessionExpiring && !isSessionTimingOut ? "Du blir snart logget ut pga inaktivitet" : "Sesjonen din er snart ferdig din pøbel!"
+export const Reason = {
+    TIMEOUT: "TIMEOUT",
+    EXPIRY: "EXPIRY",
+    LOGGED_OUT: "LOGGED_OUT",
+    NO_REASON: "NO_REASON"
+};
 
-    console.log("HALLO")
+const TimeoutModal = ({ modalReason, refresh, onCloseClick }) => {
+    const { login, logout } = useContext(AuthenticationContext);
+
+    let title, message, actionText, closeText = ""
+    let action, closeAction = () => {}
+
+    if (modalReason === Reason.TIMEOUT) {
+        title = "Forbli innlogget?"
+        message = "Av sikkerhetsgrunner lurer vi på om du vil fortsette å være innlogget. Hvis du ikke velger å fortsette, vil du automatisk bli logget ut innen kort tid."
+        actionText = "Fortsett å være innlogget"
+        closeText = "Logg ut"
+        action = refresh
+        closeAction = logout
+    } else if (modalReason === Reason.EXPIRY) {
+        title = "Din pålogging utløper snart"
+        message = "Logg inn på nytt for å fortsette, eller avslutt og gå til forsiden."
+        actionText = "Logg inn på nytt"
+        closeText = "Avslutt"
+        action = login
+        closeAction = logout
+    } else if (modalReason === Reason.LOGGED_OUT) {
+        title = "Din pålogging har utløpt"
+        message = "Du har blitt automatisk logget ut av sikkerhetsgrunner."
+        actionText = "Lukk"
+        action = onCloseClick
+    } else {
+        return null
+    }
 
     return (
-        <Modal role="alertdialog" open aria-label="Du må logge inn først" onClose={onCloseClick}>
+        <Modal role="alertdialog" open aria-label={title} closeButton={false} onClose={() => {}}>
             <section className="LoginRequiredMessage">
                 <Heading level="1" size="large" className="mb-2">
-                    Ting går åt skogen
+                    {title}
                 </Heading>
-                <BodyLong className="LoginRequiredMessage__text mb-8">{message}</BodyLong>
+                <BodyLong className="LoginRequiredMessage__text mb-8 TimeoutModalBody">{message}</BodyLong>
                 <div className="mb-8"><LoginBubble /></div>
 
                 <div className="login-buttons-wrapper">
-                    <Button variant="primary" icon={<EnterIcon aria-hidden="true"/>} onClick={login}>
-                        Logg inn
+                    <Button variant="primary" onClick={action}>
+                        {actionText}
                     </Button>
 
-                    <Button variant="secondary" onClick={onCloseClick}>
-                        Lukk
-                    </Button>
-
+                    {modalReason !== Reason.LOGGED_OUT && <Button variant="secondary" onClick={closeAction}>
+                        {closeText}
+                    </Button>}
                 </div>
             </section>
         </Modal>
@@ -36,8 +65,7 @@ function TimeoutModal({ isSessionExpiring, isSessionTimingOut, onCloseClick }) {
 }
 
 TimeoutModal.propTypes = {
-    isSessionExpiring: PropTypes.bool.isRequired,
-    isSessionTimingOut: PropTypes.bool.isRequired,
+    modalReason: PropTypes.string.isRequired,
     onCloseClick: PropTypes.func.isRequired,
 };
 
