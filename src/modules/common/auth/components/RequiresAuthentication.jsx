765ac905a1392003bf1b7610add2e3543e635f2a
Audun Skjelvan <audun.skjelvan@nav.no>
2023-10-15 17:54:48 +0200

    merge

--- src/modules/common/auth/components/RequiresAuthentication.jsx
+++ src/modules/common/auth/components/RequiresAuthentication.jsx
@@ -2,10 +2,6 @@ import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import { BodyLong, Button, Heading, Modal } from "@navikt/ds-react";
 import { EnterIcon } from "@navikt/aksel-icons";
-<<<<<<< HEAD
-=======
-import { FigureWithKey } from "@navikt/arbeidsplassen-react";
->>>>>>> master
 import { AuthenticationContext, AuthenticationStatus } from "../contexts/AuthenticationProvider";
 import DelayedSpinner from "../../components/spinner/DelayedSpinner";
 import ErrorMessage from "../../components/messages/ErrorMessage";
