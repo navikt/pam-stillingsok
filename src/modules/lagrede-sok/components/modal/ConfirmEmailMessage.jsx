1e324711a80594a6734c7790b1fcfd568f05f19d
Audun Skjelvan <audun.skjelvan@nav.no>
2023-10-15 18:53:16 +0200

    confirm email

--- src/modules/lagrede-sok/components/modal/ConfirmEmailMessage.jsx
+++ src/modules/lagrede-sok/components/modal/ConfirmEmailMessage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
-import { BodyLong, Button } from "@navikt/ds-react";
+import { BodyLong, Button, Modal } from "@navikt/ds-react";
 
 function ConfirmEmailMessage({ onClose }) {
     const buttonRef = useRef(null);
@@ -12,15 +12,16 @@ function ConfirmEmailMessage({ onClose }) {
     }, []);
 
     return (
-        <section role="status">
-            <BodyLong>Du må bekrefte e-postadressen din. Klikk på lenken i e-posten du har mottatt.</BodyLong>
-
-            <div className="SaveSearchForm__buttons">
+        <>
+            <Modal.Body>
+                <BodyLong>Du må bekrefte e-postadressen din. Klikk på lenken i e-posten du har mottatt.</BodyLong>
+            </Modal.Body>
+            <Modal.Footer>
                 <Button variant="primary" ref={buttonRef} onClick={onClose}>
                     Lukk
                 </Button>
-            </div>
-        </section>
+            </Modal.Footer>
+        </>
     );
 }
 
