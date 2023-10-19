5474ee6743b529cdd6b54ae94721cc03a78da3a2
otenav <ola.toft-eriksen@nav.no>
2023-10-19 12:33:14 +0200

    Legger til illustrasjon på siden som sier at du må gi samtykke til innloggede tjenester

--- src/modules/lagrede-sok/components/UserConsentIsRequired.jsx
+++ src/modules/lagrede-sok/components/UserConsentIsRequired.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { BodyLong, Button, Heading, VStack } from "@navikt/ds-react";
+import { FigureJugglingShieldWithCheckmark } from "@navikt/arbeidsplassen-react";
 import UserConsentModal from "../../common/user/UserConsentModal";
 
 function UserConsentIsRequired() {
@@ -8,6 +9,7 @@ function UserConsentIsRequired() {
     return (
         <section className="container-small mt-16 mb-16">
             <VStack align="center">
+                <FigureJugglingShieldWithCheckmark className="mb-8" />
                 <Heading level="1" size="large" className="text-center" spacing>
                     Du må samtykke for å kunne ta i bruk lagrede søk
                 </Heading>
