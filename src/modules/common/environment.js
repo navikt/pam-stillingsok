108a570c255ee3850746608e9c26fe9260d9e1f5
otenav <ola.toft-eriksen@nav.no>
2023-11-10 15:02:18 +0100

    Fix env file

--- src/modules/common/environment.js
+++ src/modules/common/environment.js
@@ -1,6 +1,6 @@
-export const CONTEXT_PATH = "/stillinger";
-export const INTEREST_API_URL = "http://localhost:3000/interesse-api";
-export const STILLINGSOK_URL = "http://localhost:3000/stillinger";
-export const JOBBTREFF_API_URL = "http://localhost:8088/jobbtreff-promo";
-export const LOGIN_URL = "/oauth2/login";
-export const LOGOUT_URL = "/oauth2/logout?redirect=/utlogget";
+export const CONTEXT_PATH = window.__PAM_CONTEXT_PATH__;
+export const INTEREST_API_URL = window.__INTEREST_API_URL__;
+export const STILLINGSOK_URL = window.__PAM_STILLINGSOK_URL__;
+export const JOBBTREFF_API_URL = window.__PAM_JOBBTREFF_API_URL__;
+export const LOGIN_URL = window.__LOGIN_URL__;
+export const LOGOUT_URL = window.__LOGOUT_URL__;
