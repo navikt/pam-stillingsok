77b8bf5a82034161aea041c5fa8aead191973276
Audun Skjelvan <audun.skjelvan@nav.no>
2023-12-01 15:03:19 +0100

    feilmelding

--- src/modules/stilling/rapporter-annonse/components/validate.js
+++ src/modules/stilling/rapporter-annonse/components/validate.js
@@ -2,7 +2,7 @@ export default function validateForm(categories, description) {
     const errors = {};
 
     if (!categories || categories.length === 0) {
-        errors.categoryFieldset = "Du må velge minst én grunn til at annonsen bryter rettningslinjene";
+        errors.categoryFieldset = "Du må velge minst én kategori hvor annonsen bryter retningslinjene";
     }
 
     if (description && description.length > 300) {
