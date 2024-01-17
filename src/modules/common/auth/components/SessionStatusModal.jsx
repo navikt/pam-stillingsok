e51619130b9187825a195e3c5a2594572f0c0c4a
Audun Skjelvan <audun.skjelvan@nav.no>
2024-01-17 11:06:52 +0100

    adjust session logout modal

--- src/modules/common/auth/components/SessionStatusModal.jsx
+++ src/modules/common/auth/components/SessionStatusModal.jsx
@@ -8,6 +8,8 @@ function SessionStatusModal({ markAsLoggedOut, setHasBeenLoggedIn, login, logout
     const [isSessionExpiring, setIsSessionExpiring] = useState(null);
     const [isSessionTimingOut, setIsSessionTimingOut] = useState(null);
     const [isTimeoutModalOpen, setIsTimeoutModalOpen] = useState(false);
+    const [sessionExpiringInMinutes, setSessionExpiringInMinutes] = useState(10);
+    const [sessionTimingOutInMinutes, setSessionTimingOutInMinutes] = useState(10);
 
     const handleSessionInfoResponse = async (response, isCurrentlyLoggedIn, errorMessage) => {
         if (response.status === 401) {
@@ -31,11 +33,19 @@ function SessionStatusModal({ markAsLoggedOut, setHasBeenLoggedIn, login, logout
 
             setHasBeenLoggedIn(true);
 
-            const sessionIsExpiring = session.ends_in_seconds < 60 * 5;
+            const sessionIsExpiring = session.ends_in_seconds < 60 * 10;
+            setSessionExpiringInMinutes(Math.floor(session.ends_in_seconds / 60));
+
             const sessionIsTimingOut =
                 session.timeout_in_seconds > -1
-                    ? session.timeout_in_seconds < 60 * 5
-                    : tokens.expire_in_seconds < 60 * 5;
+                    ? session.timeout_in_seconds < 60 * 10
+                    : tokens.expire_in_seconds < 60 * 10;
+
+            setSessionTimingOutInMinutes(
+                Math.floor(
+                    session.timeout_in_seconds > -1 ? session.timeout_in_seconds / 60 : tokens.expire_in_seconds / 60,
+                ),
+            );
 
             setIsSessionExpiring(sessionIsExpiring);
             setIsSessionTimingOut(sessionIsTimingOut);
@@ -68,14 +78,13 @@ function SessionStatusModal({ markAsLoggedOut, setHasBeenLoggedIn, login, logout
 
     if (isSessionExpiring) {
         title = "Din pålogging utløper snart";
-        message = "Logg inn på nytt for å fortsette, eller avslutt og logg ut.";
+        message = `Din pålogging utløper om ${sessionExpiringInMinutes} minutter. Logg inn på nytt for å fortsette, eller avslutt og logg ut.`;
         actionText = "Logg inn på nytt";
         closeText = "Avslutt";
         action = login;
     } else if (isSessionTimingOut) {
         title = "Forbli innlogget?";
-        message =
-            "Av sikkerhetsgrunner lurer vi på om du vil fortsette å være innlogget. Hvis du ikke velger å fortsette, vil du automatisk bli logget ut innen kort tid.";
+        message = `Av sikkerhetsgrunner lurer vi på om du vil fortsette å være innlogget. Hvis du ikke velger å fortsette, vil du automatisk bli logget ut om ${sessionTimingOutInMinutes} minutter.`;
         actionText = "Fortsett å være innlogget";
         closeText = "Logg ut";
         action = () => refreshToken(hasBeenLoggedIn);
@@ -111,7 +120,13 @@ function SessionStatusModal({ markAsLoggedOut, setHasBeenLoggedIn, login, logout
             </Modal.Header>
             <Modal.Body>
                 <VStack gap="6">
-                    <BodyLong>{message}</BodyLong>
+                    <BodyLong
+                        className="login-required-message__text mb-8 session-modal-body"
+                        role="timer"
+                        suppressHydrationWarning
+                    >
+                        {message}
+                    </BodyLong>
                     <HStack justify="center">
                         <FigureWithKey />
                     </HStack>
