be4bdfdf553e6f9eb9d9d5617320d4853b7e73f7
khtdiep <112953341+khtdiep@users.noreply.github.com>
2024-02-08 12:38:36 +0100

    move env vars to .env (#571)

--- src/app/_common/environment.js
+++ src/app/_common/environment.js
@@ -1,6 +1,5 @@
 export const CONTEXT_PATH = "/stillinger";
 export const INTEREST_API_URL = "/interesse-api";
 export const STILLINGSOK_URL = "window.__PAM_STILLINGSOK_URL__";
-export const JOBBTREFF_API_URL = "window.__PAM_JOBBTREFF_API_URL__";
 export const LOGIN_URL = "window.__LOGIN_URL__";
 export const LOGOUT_URL = "window.__LOGOUT_URL__";
