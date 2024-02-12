b265df0489f33c05b996c16a2e1fa9ae5aad3415
Christoffer Karlsson <christoffer.karlsson@nav.no>
2024-02-12 13:08:11 +0100

    Remove manual oauth2 endpoints, as they are handled by Wonderwall

--- src/app/oauth2/login/route.js
+++
@@ -1,8 +0,0 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-
-export default async function GET() {
-    // Todo: Replace this hack
-    cookies().set("is-logged-in-hack", "true");
-    redirect("/stillinger");
-}
--- src/app/oauth2/logout/route.js
+++
@@ -1,8 +0,0 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-
-export default async function GET() {
-    // Todo: Replace this hack
-    cookies().delete("is-logged-in-hack");
-    redirect("/stillinger");
-}
