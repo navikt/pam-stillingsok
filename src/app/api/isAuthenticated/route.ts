64ff5f315e0259e2613d186b1ce95c768a34151c
Christoffer Karlsson <christoffer.karlsson@nav.no>
2024-02-15 14:01:37 +0100

    Remove logging

--- src/app/api/isAuthenticated/route.ts
+++ src/app/api/isAuthenticated/route.ts
@@ -3,14 +3,11 @@ import { getSession } from "@navikt/oasis";
 export const dynamic = "force-dynamic";
 
 export async function GET(req: Request) {
-    console.log("HEJ!");
     const session = await getSession(req);
 
     if (!session) {
-        console.error("Not logged in!");
         return new Response(null, { status: 401 });
     }
 
-    console.error("Logged in!");
     return new Response(null, { status: 200 });
 }
