d506ecdbc3755bf48fd32c6ec0db0c35ba138c97
Christoffer Karlsson <christoffer.karlsson@nav.no>
2024-02-23 11:28:17 +0100

    Send /api/user request to aduser

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,3 +1,5 @@
+import { validateToken, requestTokenxOboToken, getToken } from "@navikt/oasis";
+
 const mockData = {
     id: 1212,
     uuid: "12121212",
@@ -9,8 +11,41 @@ const mockData = {
     verifiedEmail: false,
 };
 
-export async function GET() {
-    return Response.json(mockData);
+export const dynamic = "force-dynamic";
+
+export async function GET(req) {
+    const token = getToken(req);
+    const validationResult = await validateToken(token);
+
+    if (!validationResult.ok) {
+        return new Response(null, { status: 401 });
+    }
+
+    const oboRes = await requestTokenxOboToken(token, process.env.ADUSER_AUDIENCE);
+    if (!oboRes.ok) {
+        console.error("Failed to exchange token");
+        console.error(oboRes.error.message);
+        console.error(oboRes.error);
+        return new Response(null, { status: 500 });
+    }
+
+    let url = `${process.env.PAMADUSER_URL}/api/v1/user`;
+    const res = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Bearer ${oboRes.token}`,
+        },
+    });
+
+    if (!res.ok) {
+        console.error("Failed to fetch from aduser");
+        console.error(res.status);
+        return new Response(null, { status: res.status });
+    }
+
+    return res.json();
 }
 
 export async function POST() {
