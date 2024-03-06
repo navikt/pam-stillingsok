2492778aa39fa8119c74f5cc67874d45fc0913bf
Khiem Diep <khiem.harald.tu.diep@nav.no>
2024-03-06 09:53:28 +0100

    sentry: set environment

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -4,4 +4,5 @@ Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
     tracesSampleRate: 0.1,
     debug: false,
+    environment: process.env.NAIS_CLUSTER_NAME === "prod-gcp" ? "production" : "development",
 });
