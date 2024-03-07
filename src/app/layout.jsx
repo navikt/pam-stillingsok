9e45f3d2ba39d6cfe6d0a9d2e787ac1c894e3a9e
otenav <ola.toft-eriksen@nav.no>
2024-03-07 16:19:32 +0100

    tester response tid

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,26 +1,4 @@
-import "@navikt/ds-css/dist/global/tokens.css";
-import "@navikt/ds-css/dist/global/reset.css";
-import "@navikt/ds-css/dist/global/baseline.css";
-import "@navikt/ds-css/dist/global/print.css";
-import "@navikt/ds-css/dist/components.css";
-import "@navikt/arbeidsplassen-css";
-import "@navikt/arbeidsplassen-theme";
-import "./(sok)/_components/search.css";
-import "./stilling/ad.css";
-import "./_common/components/typeahead/Typeahead.css";
-import "./styles.css";
 import PropTypes from "prop-types";
-import interLocalFont from "next/font/local";
-import App from "./App";
-import Providers from "./Providers";
-import * as actions from "@/app/_common/actions";
-
-const myFont = interLocalFont({
-    variable: "--font-inter",
-    src: "../../public/font/InterVariable.ttf",
-    weight: "100 900",
-    display: "swap",
-});
 
 export const getMetadataTitle = (title = "Ledige stillinger") => `${title} - arbeidsplassen.no`;
 export const defaultMetadataDescription =
@@ -31,27 +9,10 @@ export const defaultOpenGraphImage = {
     height: 630,
 };
 
-export const metadata = {
-    title: getMetadataTitle("Ledige stillinger"),
-    description: defaultMetadataDescription,
-    openGraph: {
-        title: getMetadataTitle("Ledige stillinger"),
-        description: defaultMetadataDescription,
-        images: [defaultOpenGraphImage],
-    },
-    icons: {
-        icon: `/favicon.png`,
-    },
-};
-
 export default async function RootLayout({ children }) {
     return (
         <html lang="no">
-            <body data-theme="arbeidsplassen" className={myFont.className}>
-                <Providers userPreferences={await actions.getUserPreferences()}>
-                    <App amplitudeToken={process.env.AMPLITUDE_TOKEN}>{children}</App>
-                </Providers>
-            </body>
+            <body>tester responstid</body>
         </html>
     );
 }
