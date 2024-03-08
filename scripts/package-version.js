70cc2f180a68bf06b1aca7c0ba9b2c82353f600c
otenav <ola.toft-eriksen@nav.no>
2024-03-08 12:14:14 +0100

    Delete unused code

--- scripts/package-version.js
+++
@@ -1,9 +0,0 @@
-// Todo: Hva er dette og trenger vi koden?
-const npmPackage = require("../package.json");
-
-const { version } = npmPackage;
-const versionNumberList = version.split(".");
-versionNumberList.pop();
-
-// eslint-disable-next-line no-console
-console.log(versionNumberList.join("."));
