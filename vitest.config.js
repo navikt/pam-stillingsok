8003d65fddf533bc5c9a2bf28c93d5cba771f0a1
Christoffer Karlsson <christoffer.karlsson@nav.no>
2024-04-03 09:24:10 +0200

    Prettier fix vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -1,15 +1,15 @@
-import path from 'path'
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
     test: {
-        environment: 'jsdom',
+        environment: "jsdom",
     },
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, './src')
-        }
-    }
-})
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+});
