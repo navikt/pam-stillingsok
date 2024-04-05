c68c68de040c86386b6fbcc05b092088601c0cf3
Khiem Diep <khiem.harald.tu.diep@nav.no>
2024-04-05 14:54:38 +0200

    add navCallId to server side errors for better debugging

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -1,7 +1,13 @@
 import * as Sentry from "@sentry/nextjs";
+import { getCallId } from "@/app/_common/monitoring/callId";
 
 Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
     tracesSampleRate: 0.1,
     debug: false,
+
+    beforeSend(event) {
+        event.tags = { ...event.tags, navCallId: getCallId() };
+        return event;
+    },
 });
