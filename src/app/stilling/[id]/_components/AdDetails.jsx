7a344f45ceda388ae300d91566a6173d4eb8328a
Minh Ha Do <minh.ha.do@nav.no>
2024-04-10 11:58:50 +0200

    Refactor ad structure

--- src/app/stilling/[id]/_components/AdDetails.jsx
+++ src/app/stilling/[id]/_components/AdDetails.jsx
@@ -1,38 +1,59 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { BodyShort, Label, Link as AkselLink } from "@navikt/ds-react";
+import { BodyShort, Button, Heading, HGrid, Label, Stack } from "@navikt/ds-react";
 import Link from "next/link";
 import { formatDate } from "@/app/_common/utils/utils";
 import ShareAd from "@/app/stilling/[id]/_components/ShareAd";
+import { ExclamationmarkTriangleIcon } from "@navikt/aksel-icons";
 
 export default function AdDetails({ adData }) {
     return (
         <section className="full-width">
-            {adData.updated && (
-                <BodyShort spacing>
-                    <Label as="span">Sist endret:</Label> {formatDate(adData.updated)}
-                </BodyShort>
-            )}
-            {adData.medium && (
-                <BodyShort spacing>
-                    <Label as="span">Hentet fra:</Label> {adData.medium}
-                </BodyShort>
-            )}
-            {adData.reference && (
-                <BodyShort spacing>
-                    <Label as="span">Referanse:</Label> {adData.reference}
-                </BodyShort>
-            )}
-            {adData.adNumber && (
-                <BodyShort spacing>
-                    <Label as="span">Stillingsnummer:</Label> {adData.adNumber}
-                </BodyShort>
-            )}
-            <BodyShort>
-                <AkselLink as={Link} href={`/rapporter-annonse/${adData.id}`}>
+            <Stack
+                gap="5"
+                direction={{ xs: "column", sm: "row" }}
+                align={{ xs: "start", sm: "center" }}
+                justify="space-between"
+            >
+                <Heading level="2" size="large">
+                    Annonsedata
+                </Heading>
+                <Button
+                    className="report-ad-button"
+                    as={Link}
+                    href={`/rapporter-annonse/${adData.id}`}
+                    variant="tertiary"
+                    icon={<ExclamationmarkTriangleIcon aria-hidden />}
+                >
                     Rapporter annonse
-                </AkselLink>
-            </BodyShort>
+                </Button>
+                <HGrid className="ad-details" gap="6" columns={{ xs: 1, sm: 2 }}>
+                    {adData.adNumber && (
+                        <div>
+                            <Label as="span">Stillingsnummer</Label>
+                            <BodyShort spacing>{adData.adNumber}</BodyShort>
+                        </div>
+                    )}
+                    {adData.updated && (
+                        <div>
+                            <Label as="span">Sist endret</Label>
+                            <BodyShort spacing>{formatDate(adData.updated)}</BodyShort>
+                        </div>
+                    )}
+                    {adData.medium && (
+                        <div>
+                            <Label as="span">Hentet fra</Label>
+                            <BodyShort spacing>{adData.medium}</BodyShort>
+                        </div>
+                    )}
+                    {adData.reference && (
+                        <div>
+                            <Label as="span">Referanse</Label>
+                            <BodyShort spacing>{adData.reference}</BodyShort>
+                        </div>
+                    )}
+                </HGrid>
+            </Stack>
         </section>
     );
 }
--- src/app/stilling/[id]/_components/EmploymentDetails.jsx
+++ src/app/stilling/[id]/_components/EmploymentDetails.jsx
@@ -1,16 +1,42 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { BodyLong, Heading, Label } from "@navikt/ds-react";
+import { BodyLong, Heading, HStack, Label } from "@navikt/ds-react";
 import { formatDate } from "@/app/_common/utils/utils";
 import "./EmploymentDetails.css";
 import joinStringWithSeperator from "@/app/_common/utils/joinStringWithSeperator";
+import FavouritesButton from "@/app/favoritter/_components/FavouritesButton";
 
 export default function EmploymentDetails({ adData }) {
+    /**
+     *  TODO: refactor denne
+     *  Blir brukt for FavouritesButton som forventer gammeldags data.
+     *  Venter med å refaktorere FavouritesButton for den blir brukt
+     *  flere steder
+     */
+    const stilling = {
+        source: adData.source,
+        reference: adData.reference,
+        title: adData.title,
+        status: adData.status,
+        locationList: adData.locationList,
+        published: adData.published,
+        expires: adData.expires,
+        properties: {
+            jobtitle: adData.jobTitle,
+            applicationdue: adData.applicationDue,
+            location: adData.location,
+            employer: adData.employer.name,
+        },
+    };
+
     return (
         <section className="full-width">
-            <Heading level="2" size="large" spacing>
-                Om jobben
-            </Heading>
+            <HStack gap="4" justify="space-between" align="center">
+                <Heading level="2" size="large">
+                    Om jobben
+                </Heading>
+                <FavouritesButton variant="tertiary" id={adData.id} stilling={stilling} />
+            </HStack>
 
             <dl className="dl" id="employment-details">
                 {adData.jobTitle && (
