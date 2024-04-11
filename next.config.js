41c011efec2f34ee96ccc3b3218c174fbd27896a
Audun Skjelvan <audun.skjelvan@nav.no>
2024-04-11 11:50:59 +0200

    try purge css

--- next.config.js
+++ next.config.js
@@ -10,6 +10,33 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    plugins: [
+        // restore the Next.js default behavior
+        "postcss-flexbugs-fixes",
+        [
+            "postcss-preset-env",
+            {
+                autoprefixer: {
+                    flexbox: "no-2009",
+                },
+                stage: 3,
+                features: {
+                    "custom-properties": false,
+                },
+            },
+        ],
+        [
+            // configure PurgeCSS
+            "@fullhuman/postcss-purgecss",
+            {
+                content: ["./src/app/**/*.{js,jsx,ts,tsx}"],
+                defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+                safelist: {
+                    standard: ["html", "body"],
+                },
+            },
+        ],
+    ],
 };
 
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
---
+++ src/postcss.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    plugins: [
+        // restore the Next.js default behavior
+        "postcss-flexbugs-fixes",
+        [
+            "postcss-preset-env",
+            {
+                autoprefixer: {
+                    flexbox: "no-2009",
+                },
+                stage: 3,
+                features: {
+                    "custom-properties": false,
+                },
+            },
+        ],
+        [
+            // configure PurgeCSS
+            "@fullhuman/postcss-purgecss",
+            {
+                content: ["./src/app/**/*.{js,jsx,ts,tsx}"],
+                defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+                safelist: {
+                    standard: ["html", "body"],
+                },
+            },
+        ],
+    ],
+};
