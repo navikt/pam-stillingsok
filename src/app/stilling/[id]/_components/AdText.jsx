62f645726b4bae88d51a3baf36f7018179c87219
Minh Ha Do <minh.ha.do@nav.no>
2024-04-12 09:29:48 +0200

    Refactor text structure

--- src/app/stilling/[id]/_components/AdText.jsx
+++ src/app/stilling/[id]/_components/AdText.jsx
@@ -4,7 +4,18 @@ import parse from "html-react-parser";
 import { RichText } from "@navikt/arbeidsplassen-react";
 
 export default function AdText({ adText }) {
+    const options = {
+        replace: ({ attribs }) => {
+            if (attribs && attribs.id === "arb-aapningstekst") {
+                return <></>;
+            }
+        },
+    };
+
     if (adText) {
+        if (adText.includes("arb-aapningstekst")) {
+            return <RichText className="job-posting-text">{parse(adText, options)}</RichText>;
+        }
         return <RichText className="job-posting-text">{parse(adText)}</RichText>;
     }
     return null;
