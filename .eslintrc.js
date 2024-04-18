e3ef6cd58dc5065fa1555aa86691e9216b00fcf7
Audun Skjelvan <audun.skjelvan@nav.no>
2024-04-18 11:37:06 +0200

    vercel style lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,29 @@ const project = resolve(__dirname, "jsconfig.json");
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
     root: true,
-    extends: ["plugin:@next/next/recommended", "airbnb", "next/core-web-vitals", "prettier"],
+    extends: [
+        require.resolve("@vercel/style-guide/eslint/browser"),
+        require.resolve("@vercel/style-guide/eslint/react"),
+        require.resolve("@vercel/style-guide/eslint/next"),
+        require.resolve("@vercel/style-guide/eslint/node"),
+        require.resolve("@vercel/style-guide/eslint/typescript"),
+        "plugin:@next/next/recommended",
+        "airbnb",
+        "next/core-web-vitals",
+        "prettier",
+    ],
     plugins: ["react", "unused-imports", "prettier"],
     parserOptions: { project },
     settings: {
+        "import/resolver": {
+            "eslint-import-resolver-custom-alias": {
+                alias: {
+                    "@/app": "./src/app",
+                },
+                extensions: [".js", ".jsx"],
+                packages: ["packages/*"],
+            },
+        },
         /**
          * enable MUI Joy components to be checked
          * @see {@link https://github.com/jsx-eslint/eslint-plugin-jsx-a11y?tab=readme-ov-file#configurations}
@@ -33,32 +52,17 @@ module.exports = {
         },
     },
     rules: {
-        "no-underscore-dangle": "off",
-        "import/prefer-default-export": "off",
-        "react/jsx-no-bind": "off",
-        "react/no-unescaped-entities": "off",
-        "prettier/prettier": "error",
-        "react/react-in-jsx-scope": "off",
-        "react/jsx-props-no-spreading": "off",
-        "react/forbid-prop-types": "off",
-        // defaultProps rule to be deprecated on function components
-        // https://github.com/reactjs/rfcs/pull/107
-        "react/require-default-props": [
-            "error",
-            {
-                ignoreFunctionalComponents: true,
-            },
-        ],
-        "unused-imports/no-unused-imports": "error",
-        "unused-imports/no-unused-vars": [
+        "unicorn/filename-case": "off",
+        // sort import statements
+        "import/order": [
             "warn",
             {
-                vars: "all",
-                varsIgnorePattern: "^_",
-                args: "after-used",
-                argsIgnorePattern: "^_",
+                groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+                "newlines-between": "always",
+                alphabetize: { order: "asc" },
             },
         ],
-        singleQuote: "off",
+        // sort named imports within an import statement
+        "sort-imports": ["warn", { ignoreDeclarationSort: true }],
     },
 };
