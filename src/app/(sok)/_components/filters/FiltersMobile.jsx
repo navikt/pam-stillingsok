0c7de5e8e620120e35b930f00cdbdab913eed530
Audun Skjelvan <audun.skjelvan@nav.no>
2024-05-13 07:34:17 +0200

    initial commit mobile filters

--- src/app/(sok)/_components/filters/FiltersMobile.jsx
+++ src/app/(sok)/_components/filters/FiltersMobile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Button, Heading, Modal, Tabs } from "@navikt/ds-react";
 import { formatNumber } from "@/app/_common/utils/utils";
@@ -12,97 +12,143 @@ import EngagementType from "./Engagement";
 import WorkLanguage from "./WorkLanguage";
 
 function FiltersMobile({ onCloseClick, searchResult, query, dispatchQuery, aggregations, locations }) {
-    return (
-        <Modal className="filter-modal" header={{ heading: "Filtre" }} open onClose={onCloseClick} width="100%">
-            <Modal.Body>
-                <Tabs defaultValue="sted">
-                    <Tabs.List>
-                        <Tabs.Tab value="sted" label="Sted" />
-                        <Tabs.Tab value="yrke" label="Yrke" />
-                        <Tabs.Tab value="andre" label="Andre filtre" />
-                    </Tabs.List>
-                    <Tabs.Panel value="sted" className="mt-8">
-                        <Counties
-                            query={query}
-                            dispatch={dispatchQuery}
-                            locations={locations}
-                            updatedValues={searchResult}
-                        />
-                    </Tabs.Panel>
-                    <Tabs.Panel value="yrke" className="mt-8">
-                        <Occupations
-                            query={query}
-                            dispatch={dispatchQuery}
-                            initialValues={aggregations.occupationFirstLevels}
-                            updatedValues={searchResult && searchResult.aggregations.occupationFirstLevels}
-                        />
-                    </Tabs.Panel>
-                    <Tabs.Panel value="andre" className="mt-8">
-                        <div className="mb-6">
-                            <Published
-                                query={query}
-                                dispatch={dispatchQuery}
-                                initialValues={aggregations.published}
-                                updatedValues={searchResult && searchResult.aggregations.published}
-                            />
-                        </div>
-                        <div className="mb-6">
-                            <Heading level="2" size="small">
-                                Hjemmekontor
-                            </Heading>
-                            <Remote
-                                query={query}
-                                dispatch={dispatchQuery}
-                                initialValues={aggregations.remote}
-                                updatedValues={searchResult.aggregations.remote}
-                            />
-                        </div>
-                        <div className="mb-6">
-                            <Heading level="2" size="small">
-                                Heltid/deltid
-                            </Heading>
-                            <Extent
-                                query={query}
-                                dispatch={dispatchQuery}
-                                initialValues={aggregations.extent}
-                                updatedValues={searchResult && searchResult.aggregations.extent}
-                            />
-                        </div>
-                        <div className="mb-6">
-                            <Heading level="2" size="small">
-                                Sektor
-                            </Heading>
-                            <Sector
-                                query={query}
-                                dispatch={dispatchQuery}
-                                initialValues={aggregations.sector}
-                                updatedValues={searchResult && searchResult.aggregations.sector}
-                            />
-                        </div>
-                        <div className="mb-6">
-                            <Heading level="2" size="small">
-                                Ansettelsesform
-                            </Heading>
-                            <EngagementType
-                                query={query}
-                                dispatch={dispatchQuery}
-                                initialValues={aggregations.engagementTypes}
-                                updatedValues={searchResult && searchResult.aggregations.engagementTypes}
-                            />
-                        </div>
-                        <div className="mb-6">
-                            <Heading level="2" size="small">
-                                Arbeidsspråk
-                            </Heading>
-                            <WorkLanguage
-                                query={query}
-                                dispatch={dispatchQuery}
-                                initialValues={aggregations.workLanguage}
-                                updatedValues={searchResult && searchResult.aggregations.workLanguage}
-                            />
-                        </div>
+    const [selectedTab, setSelectedTab] = useState("");
+    const [modalHeading, setModalHeading] = useState({ heading: "Filtre" });
 
-                        {/* TODO: COMMENT IN WHEN FILTER IS READY BACKEND
+    const changeView = () => {
+        if (selectedTab !== "") {
+            setSelectedTab("");
+            setModalHeading({ heading: "Filtre" });
+            return false;
+        }
+        return true;
+    };
+
+    const setSelectedFilter = (value) => {
+        setSelectedTab(value);
+        setModalHeading({
+            label: "Filtre",
+            heading: value,
+        });
+    };
+
+    return (
+        <Modal
+            className="filter-modal"
+            header={modalHeading}
+            open
+            onBeforeClose={changeView}
+            onClose={onCloseClick}
+            width="100%"
+        >
+            <Modal.Body>
+                <Tabs defaultValue={selectedTab} value={selectedTab} onChange={setSelectedFilter}>
+                    <Tabs.List className={`flex-direction-column ${selectedTab !== "" ? "hidden" : ""}`}>
+                        <Tabs.Tab value="Publisert" label="Publisert" />
+                        <Tabs.Tab value="Sted og hjemmekontor" label="Sted og hjemmekontor" />
+                        <Tabs.Tab value="Yrke og sektor" label="Yrke og sektor" />
+                        <Tabs.Tab value="Arbeidsspråk" label="Arbeidsspråk" />
+                        <Tabs.Tab value="Omfang og ansettelsesform" label="Omfang og ansettelsesform" />
+                    </Tabs.List>
+
+                    {selectedTab === "Publisert" && (
+                        <Tabs.Panel value="Publisert" className="mt-8">
+                            <div className="mb-6">
+                                <Published
+                                    query={query}
+                                    dispatch={dispatchQuery}
+                                    initialValues={aggregations.published}
+                                    updatedValues={searchResult && searchResult.aggregations.published}
+                                />
+                            </div>
+                        </Tabs.Panel>
+                    )}
+                    {selectedTab === "Sted og hjemmekontor" && (
+                        <Tabs.Panel value="Sted og hjemmekontor" className="mt-8">
+                            <Counties
+                                query={query}
+                                dispatch={dispatchQuery}
+                                locations={locations}
+                                updatedValues={searchResult}
+                            />
+                            <div className="mb-6">
+                                <Heading level="2" size="small">
+                                    Hjemmekontor
+                                </Heading>
+                                <Remote
+                                    query={query}
+                                    dispatch={dispatchQuery}
+                                    initialValues={aggregations.remote}
+                                    updatedValues={searchResult.aggregations.remote}
+                                />
+                            </div>
+                        </Tabs.Panel>
+                    )}
+
+                    {selectedTab === "Yrke og sektor" && (
+                        <Tabs.Panel value="Yrke og sektor" className="mt-8">
+                            <Occupations
+                                query={query}
+                                dispatch={dispatchQuery}
+                                initialValues={aggregations.occupationFirstLevels}
+                                updatedValues={searchResult && searchResult.aggregations.occupationFirstLevels}
+                            />
+                        </Tabs.Panel>
+                    )}
+
+                    {selectedTab === "Arbeidsspråk" && (
+                        <Tabs.Panel value="Arbeidsspråk">
+                            <div className="mb-6">
+                                <Heading level="2" size="small">
+                                    Arbeidsspråk
+                                </Heading>
+                                <WorkLanguage
+                                    query={query}
+                                    dispatch={dispatchQuery}
+                                    initialValues={aggregations.workLanguage}
+                                    updatedValues={searchResult && searchResult.aggregations.workLanguage}
+                                />
+                            </div>
+                        </Tabs.Panel>
+                    )}
+
+                    {selectedTab === "Omfang og ansettelsesform" && (
+                        <Tabs.Panel value="Omfang og ansettelsesform" className="mt-8">
+                            <div className="mb-6">
+                                <Heading level="2" size="small">
+                                    Heltid/deltid
+                                </Heading>
+                                <Extent
+                                    query={query}
+                                    dispatch={dispatchQuery}
+                                    initialValues={aggregations.extent}
+                                    updatedValues={searchResult && searchResult.aggregations.extent}
+                                />
+                            </div>
+                            <div className="mb-6">
+                                <Heading level="2" size="small">
+                                    Sektor
+                                </Heading>
+                                <Sector
+                                    query={query}
+                                    dispatch={dispatchQuery}
+                                    initialValues={aggregations.sector}
+                                    updatedValues={searchResult && searchResult.aggregations.sector}
+                                />
+                            </div>
+                            <div className="mb-6">
+                                <Heading level="2" size="small">
+                                    Ansettelsesform
+                                </Heading>
+                                <EngagementType
+                                    query={query}
+                                    dispatch={dispatchQuery}
+                                    initialValues={aggregations.engagementTypes}
+                                    updatedValues={searchResult && searchResult.aggregations.engagementTypes}
+                                />
+                            </div>
+
+                            {/* TODO: COMMENT IN WHEN FILTER IS READY BACKEND
                         <div className="mb-6">
                             <Heading level="2" size="small">
                                 Utdanning
@@ -114,7 +160,8 @@ function FiltersMobile({ onCloseClick, searchResult, query, dispatchQuery, aggre
                                 updatedValues={searchResult && searchResult.aggregations.education}
                             />
                         </div> */}
-                    </Tabs.Panel>
+                        </Tabs.Panel>
+                    )}
                 </Tabs>
             </Modal.Body>
             <Modal.Footer>
@@ -123,6 +170,7 @@ function FiltersMobile({ onCloseClick, searchResult, query, dispatchQuery, aggre
                         ? `Vis ${formatNumber(searchResult.totalAds)} treff`
                         : "Vis treff"}
                 </Button>
+                {selectedTab !== "" && <Button onClick={changeView}>Tilbake</Button>}
             </Modal.Footer>
         </Modal>
     );
