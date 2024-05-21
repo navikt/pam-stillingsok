17621384e528f3856cbd8f1aed99504161d881c0
Audun <67591449+audisaudisaudis@users.noreply.github.com>
2024-05-21 12:23:38 +0200

    pull latest docker shared to ensure it is newest version (#663)
    * pull docker shared first
    
    * name
    
    * dont need this tip in README anymore

--- start-docker-compose.sh
+++ start-docker-compose.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Pull latest version
+git -C ../pam-docker-compose-shared pull
+
 # Start shared docker compose services (must be sourced, or DOCKER_COMPOSE_COMMAND won't be available)
 . ../pam-docker-compose-shared/start-docker-compose.sh postgres mock-oauth2-server redis
 
