17d6aec1364d1ed48cb0c0acfbc6e468b021eb99
Audun Skjelvan <audun.skjelvan@nav.no>
2024-05-28 08:05:25 +0200

    change filter position

--- src/app/(sok)/_components/utils/sortRemoteValues.js
+++ src/app/(sok)/_components/utils/sortRemoteValues.js
@@ -2,7 +2,7 @@
  * Sorter publisert basert på egen array
  */
 export default function sortRemoteValues(facets) {
-    const sortedPublishedValues = ["Hjemmekontor ikke mulig", "Hybridkontor", "Hjemmekontor"];
+    const sortedPublishedValues = ["Hybridkontor", "Hjemmekontor", "Hjemmekontor ikke mulig"];
     const clone = facets;
 
     clone.sort((a, b) => sortedPublishedValues.indexOf(a.key) - sortedPublishedValues.indexOf(b.key));
