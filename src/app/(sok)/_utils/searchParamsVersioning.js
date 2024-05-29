ce6e2a7cbe2945e74e5ef9934ad217f758e6acb3
Christoffer Karlsson <christoffer.karlsson@nav.no>
2024-05-29 15:39:43 +0200

    Add search param versioning support and move occupation changes to version 1 migration

---
+++ src/app/(sok)/_utils/searchParamsVersioning.js
@@ -0,0 +1,50 @@
+import { containsOldOccupations, rewriteOccupationSearchParams } from "@/app/(sok)/_utils/occupationChanges";
+
+export const VERSION_QUERY_PARAM = "v";
+
+const FIRST_VERSION = 0;
+const CURRENT_VERSION = 1;
+
+// Returns new search params if the searchParams have been migrated.
+export function migrateSearchParams(searchParams) {
+    let newSearchParams = searchParams;
+    const version = getCurrentVersion(searchParams);
+
+    if (version === CURRENT_VERSION) {
+        return undefined;
+    }
+
+    if (version < 1) {
+        newSearchParams = migrateToV1(newSearchParams);
+    }
+
+    return newSearchParams;
+}
+
+function getCurrentVersion(searchParams) {
+    if (hasNoSearchParams(searchParams)) {
+        return CURRENT_VERSION;
+    }
+
+    const version = searchParams[VERSION_QUERY_PARAM];
+
+    return version ? parseInt(version, 10) : FIRST_VERSION;
+}
+
+function hasNoSearchParams(searchParams) {
+    return Object.keys(searchParams).length === 0;
+}
+
+function migrateToV1(searchParams) {
+    let newSearchParams = searchParams;
+
+    // V1 changes some occupations to new ones.
+    // After changing, users might still access a search with their old occupations due to saved searches and bookmarks.
+    if (containsOldOccupations(searchParams)) {
+        newSearchParams = rewriteOccupationSearchParams(newSearchParams);
+    }
+
+    newSearchParams[VERSION_QUERY_PARAM] = 1;
+
+    return newSearchParams;
+}
