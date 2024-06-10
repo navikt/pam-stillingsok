bc1c51cb8dadab111815c38afa1865ec3b0e8774
otenav <ola.toft-eriksen@nav.no>
2024-06-10 10:08:14 +0200

    Clean up

--- src/app/(sok)/_utils/versioning/version02.test.js
+++ src/app/(sok)/_utils/versioning/version02.test.js
@@ -1,39 +1,5 @@
 import { describe, expect, test } from "vitest";
-import {
-    containsOldOccupations,
-    getOccupationsOnlyInFirstLevel,
-    rewriteOccupationSearchParams,
-} from "@/app/(sok)/_utils/occupationChanges";
 import { migrateToV2 } from "@/app/(sok)/_utils/versioning/version02";
-
-const oldSearchParams = {
-    shouldNotChangeOrBeDeleted: ["a", "b"],
-    "occupationFirstLevels[]": ["a", "b"],
-    "occupationSecondLevels[]": ["a", "b"],
-    "municipals[]": ["a", "b"],
-    "counties[]": ["a", "b"],
-    "countries[]": ["a", "b"],
-    "extent[]": ["a", "b"],
-    "engagementType[]": ["a", "b"],
-    "sector[]": ["a", "b"],
-    "education[]": ["a", "b"],
-    "workLanguage[]": ["a", "b"],
-    "remote[]": ["a", "b"],
-};
-
-const expectedResult = {
-    occupationLevel1: ["a", "b"],
-    occupationLevel2: ["a", "b"],
-    municipal: ["a", "b"],
-    county: ["a", "b"],
-    country: ["a", "b"],
-    extent: ["a", "b"],
-    engagementType: ["a", "b"],
-    sector: ["a", "b"],
-    education: ["a", "b"],
-    workLanguage: ["a", "b"],
-    remote: ["a", "b"],
-};
 
 describe("version02", () => {
     test("Should migrate all outdated search params", () => {
@@ -69,7 +35,7 @@ describe("version02", () => {
         expect(migratedSearchParams).toEqual(expectedResult);
     });
 
-    test("Does not delete or change valid search params", () => {
+    test("Should not delete or change valid search params", () => {
         const validSearchParams = {
             validSearchParam: ["a", "b"],
         };
