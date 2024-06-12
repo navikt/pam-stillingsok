f27a53da0dcf3f2c2556a1564e1154040fac12da
Audun Skjelvan <audun.skjelvan@nav.no>
2024-06-12 14:52:03 +0200

    wrap selected filters in a list with label

--- src/app/(sok)/_components/selectedFilters/SelectedFilters.jsx
+++ src/app/(sok)/_components/selectedFilters/SelectedFilters.jsx
@@ -286,7 +286,11 @@ function SelectedFilters({ query, queryDispatch }) {
         <HStack gap="2" align="center">
             {chips.length > MAX_CHIPS ? (
                 <>
-                    {showAll ? chips : chips.slice(0, MAX_CHIPS)}
+                    {showAll ? (
+                        <Chips aria-label="Liste med valgte søkefiltre">{chips}</Chips>
+                    ) : (
+                        <Chips aria-label="Liste med valgte søkefiltre">{chips.slice(0, MAX_CHIPS)}</Chips>
+                    )}
                     {!showAll && (
                         <Button
                             size="small"
@@ -300,7 +304,7 @@ function SelectedFilters({ query, queryDispatch }) {
                     )}
                 </>
             ) : (
-                chips
+                <Chips aria-label="Liste med valgte søkefiltre">{chips}</Chips>
             )}
             <Button
                 type="button"
