861b5c5ce0fc4ed1b4a027b4d5dba45555b0ed08
Christoffer Karlsson <christoffer.karlsson@nav.no>
2024-06-17 15:04:50 +0200

    Add comment on why removing the id of ads

--- src/app/api/internal/metrics/route.js
+++ src/app/api/internal/metrics/route.js
@@ -6,6 +6,8 @@ const UUID_PATTERN = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{1
 
 export async function POST(request) {
     const data = await request.json();
+    // There's no good way to get the dynamic path in Next with app router, so we're removing the ID's manually from the path,
+    // to avoid having 20k+ different paths in the metrics.
     const path = data.path.replaceAll(UUID_PATTERN, "[id]");
     httpRequests.inc({ method: data.method, path });
     return new Response();
