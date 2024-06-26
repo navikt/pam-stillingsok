cfb104c68fde7054a6d44fd165e5b1752b094150
Christoffer Karlsson <christoffer.karlsson@nav.no>
2024-06-26 19:26:59 +0200

    Convert API routes to TS

--- src/app/api/internal/metrics/route.ts
+++ src/app/api/internal/metrics/route.ts
@@ -4,7 +4,7 @@ export const dynamic = "force-dynamic";
 
 const UUID_PATTERN = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/g;
 
-export async function POST(request) {
+export async function POST(request: Request) {
     const data = await request.json();
     // There's no good way to get the dynamic path in Next with app router, so we're removing the ID's manually from the path,
     // to avoid having 20k+ different paths in the metrics.
