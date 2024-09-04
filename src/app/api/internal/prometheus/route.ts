bb30677de6aa2fe41905fa988cc290ea22e65583
otenav <ola.toft-eriksen@nav.no>
2024-09-04 12:31:27 +0200

    Forsøker å fikse ts return value

--- src/app/api/internal/prometheus/route.ts
+++ src/app/api/internal/prometheus/route.ts
@@ -2,7 +2,7 @@ import { register } from "prom-client";
 
 export const dynamic = "force-dynamic";
 
-export async function GET(): Response {
+export async function GET(): Promise<Response> {
     return new Response(await register.metrics(), {
         headers: { "Content-Type": register.contentType },
     });
