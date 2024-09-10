53de08b2681b521178382a1bc1e3e729e3ea2ec2
otenav <ola.toft-eriksen@nav.no>
2024-09-10 22:47:23 +0200

    Icons

--- src/app/(sok)/_components/filters/DistanceOrLocation.tsx
+++ src/app/(sok)/_components/filters/DistanceOrLocation.tsx
@@ -1,5 +1,6 @@
 import DrivingDistance from "@/app/(sok)/_components/filters/DrivingDistance";
 import { ToggleGroup } from "@navikt/ds-react";
+import { CarIcon, LocationPinIcon } from "@navikt/aksel-icons";
 import React, { ReactElement, useState } from "react";
 import { Postcode } from "@/app/(sok)/_utils/fetchPostcodes";
 import Counties from "./Locations";
@@ -19,8 +20,8 @@ function DistanceOrLocation({ postcodes, locations, searchResult }: DistanceOrLo
     return (
         <>
             <ToggleGroup defaultValue={selectedOption} onChange={setSelectedOption} fill>
-                <ToggleGroup.Item value="distance" label="Reisevei" />
-                <ToggleGroup.Item value="location" label="Sted" />
+                <ToggleGroup.Item value="distance" icon={<CarIcon aria-hidden />} label="Reisevei" />
+                <ToggleGroup.Item value="location" icon={<LocationPinIcon aria-hidden />} label="Sted" />
             </ToggleGroup>
             {selectedOption === "distance" && <DrivingDistance postcodes={postcodes} />}
             {selectedOption === "location" && <Counties locations={locations} updatedValues={searchResult} />}
