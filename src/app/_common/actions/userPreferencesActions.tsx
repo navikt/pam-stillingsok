e02950ae28d58cef51337b17fa392337a98cab98
Audun Skjelvan <audun.skjelvan@nav.no>
2024-09-16 10:45:56 +0200

    filter allowed values

--- src/app/_common/actions/userPreferencesActions.tsx
+++ src/app/_common/actions/userPreferencesActions.tsx
@@ -17,6 +17,7 @@ const ALLOWED_PANELID_VALUES = [
     "workLanguage",
     "hjemmekontor",
 ];
+const ALLOWED_DISTANCE_OR_LOCATION_VALUES = ["location", "distance"];
 const ALLOWED_DISMISSED_PANELS: string[] = ["new-filters-survey"];
 const ALLOWED_RESULTS_PER_PAGE: number[] = [25, 100];
 const COOKIE_OPTIONS: { secure: boolean; httpOnly: boolean } = { secure: true, httpOnly: true };
@@ -55,7 +56,11 @@ export async function getUserPreferences(): Promise<UserPreferences> {
 
         const publishedJobFilterOpen = parsedCookie.publishedJobFilterOpen === true;
 
-        const { locationOrDistance } = parsedCookie;
+        const locationOrDistance =
+            parsedCookie.locationOrDistance &&
+            ALLOWED_DISTANCE_OR_LOCATION_VALUES.includes(parsedCookie.locationOrDistance)
+                ? parsedCookie.locationOrDistance
+                : undefined;
 
         return {
             openFilters,
@@ -123,6 +128,11 @@ export async function saveResultsPerPage(resultsPerPage: number): Promise<void> 
 }
 
 export async function saveLocationOrDistance(locationOrDistance: string): Promise<void> {
+    if (!ALLOWED_DISTANCE_OR_LOCATION_VALUES.includes(locationOrDistance)) {
+        // Trying to set an invalid value
+        return;
+    }
+
     const existingCookie = await getUserPreferences();
     const newCookieValue = { ...existingCookie, locationOrDistance };
     cookies().set(USER_PREFERENCES_COOKIE_NAME, JSON.stringify(newCookieValue), COOKIE_OPTIONS);
