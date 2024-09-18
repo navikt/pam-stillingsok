05a55a0595a58658dfd9ef92ba9c84e14c152aab
haugse <sara.haugse@gmail.com>
2024-09-18 11:42:00 +0200

    Henter kommuner og fylker fra pam-geografi

--- src/app/(sok)/page.jsx
+++ src/app/(sok)/page.jsx
@@ -17,6 +17,7 @@ import Link from "next/link";
 import React from "react";
 import MaxQuerySizeExceeded from "@/app/_common/components/MaxQuerySizeExceeded";
 import { fetchCachedPostcodes } from "@/app/(sok)/_utils/fetchPostcodes";
+import { getDefaultHeaders } from "@/app/_common/utils/fetch";
 
 const MAX_QUERY_SIZE = 10000;
 
@@ -42,8 +43,14 @@ export async function generateMetadata({ searchParams }) {
 
 async function fetchLocations() {
     const [response1, response2] = await Promise.all([
-        fetch(`${process.env.PAMADUSER_URL}/api/v1/geography/municipals`, { next: { revalidate: 3600 } }),
-        fetch(`${process.env.PAMADUSER_URL}/api/v1/geography/counties`, { next: { revalidate: 3600 } }),
+        fetch(`${process.env.PAM_GEOGRAFI_API_URL}/kommuner`, {
+            next: { revalidate: 3600 },
+            headers: getDefaultHeaders(),
+        }),
+        fetch(`${process.env.PAM_GEOGRAFI_API_URL}/fylker`, {
+            next: { revalidate: 3600 },
+            headers: getDefaultHeaders(),
+        }),
     ]);
 
     if (!response1.ok || !response2.ok) {
@@ -55,13 +62,13 @@ async function fetchLocations() {
 
     return [
         ...counties.map((c) => ({
-            key: c.name,
-            code: c.code,
+            key: c.navn,
+            code: c.fylkesnummer,
             municipals: municipals
-                .filter((m) => m.countyCode === c.code)
+                .filter((m) => m.fylkesnummer === c.fylkesnummer)
                 .map((m) => ({
-                    key: `${c.name}.${m.name}`,
-                    code: m.code,
+                    key: `${c.navn}.${m.navn}`,
+                    code: m.fylkesnummer,
                 })),
         })),
         {
