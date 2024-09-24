798da233278c155f62094d96a51320b284103c8f
Minh Ha Do <minh.ha.do@nav.no>
2024-09-24 13:30:50 +0200

    Refactor icons to ts

--- src/app/stilling/[id]/_components/icons/LinkedinIcon.tsx
+++ src/app/stilling/[id]/_components/icons/LinkedinIcon.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
+import { ReactElement } from "react";
 
-function LinkedinIcon() {
+function LinkedinIcon(): ReactElement {
     return (
         <svg
             role="img"
