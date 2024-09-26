1af2285b6fdc9382c9c066905f73cfe4123748d9
otenav <ola.toft-eriksen@nav.no>
2024-09-26 13:37:53 +0200

    Remove unused files

--- src/app/(sok)/_components/utils/findZeroCountLocationFacets.js
+++
@@ -1,9 +0,0 @@
-export default function findZeroCountLocationFacets(usersSearchCriteria, nationalCountMap, internationalCountMap) {
-    if (Array.isArray(usersSearchCriteria) && usersSearchCriteria.length > 0) {
-        return usersSearchCriteria.filter(
-            (c) => !(Object.keys(nationalCountMap).includes(c) || Object.keys(internationalCountMap).includes(c)),
-        );
-    }
-
-    return [];
-}
