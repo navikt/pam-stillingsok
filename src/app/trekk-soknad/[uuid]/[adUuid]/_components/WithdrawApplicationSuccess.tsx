d94a1a118551b122b1f93bd4f6036a8305ea9e20
Steffen Aas <steffen.aas@outlook.com>
2024-10-08 11:30:49 +0200

    Reverser fjerning av title ref Legg til ref paa tittel  i WithdrawApplicationSuccess oppdater TS til siste versjon

--- src/app/trekk-soknad/[uuid]/[adUuid]/_components/WithdrawApplicationSuccess.tsx
+++ src/app/trekk-soknad/[uuid]/[adUuid]/_components/WithdrawApplicationSuccess.tsx
@@ -1,12 +1,19 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect, useRef } from "react";
 import { BodyLong, Button, Heading } from "@navikt/ds-react";
 import Link from "next/link";
 import GiveFeedback from "@/app/stilling/[id]/superrask-soknad/_components/GiveFeedback";
 
 function WithdrawApplicationSuccess(): ReactElement {
+    const titleRef = useRef<HTMLHeadingElement>(null);
+
+    useEffect(() => {
+        if (titleRef?.current) {
+            titleRef.current.focus();
+        }
+    }, []);
     return (
         <>
-            <Heading level="1" size="large" spacing tabIndex={-1} aria-live="polite" role="alert">
+            <Heading level="1" size="large" spacing ref={titleRef} tabIndex={-1} aria-live="polite" role="alert">
                 Din søknad er nå trukket
             </Heading>
 
