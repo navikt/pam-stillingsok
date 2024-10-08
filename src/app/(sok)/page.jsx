3a97c9e134d1f500270b8cf0d577bef8fd23406a
otenav <ola.toft-eriksen@nav.no>
2024-10-08 09:29:47 +0200

    Fix redirect loop when query url is empty

--- src/app/(sok)/page.jsx
+++ src/app/(sok)/page.jsx
@@ -91,6 +91,7 @@ export default async function Page({ searchParams }) {
         }
     }
     const newSearchParams = migrateSearchParams(searchParams);
+
     if (newSearchParams !== undefined) {
         const newQuery = {
             ...createQuery(newSearchParams),
