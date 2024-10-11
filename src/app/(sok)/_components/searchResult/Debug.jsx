290a03219784387a49c557c1c21d5363e5242dda
otenav <ola.toft-eriksen@nav.no>
2024-10-11 08:25:16 +0200

    Debug

--- src/app/(sok)/_components/searchResult/Debug.jsx
+++ src/app/(sok)/_components/searchResult/Debug.jsx
@@ -1,98 +1,93 @@
 import React from "react";
 import { BodyShort, Box, HStack, VStack } from "@navikt/ds-react";
-import PropTypes from "prop-types";
 import { useSearchParams } from "next/navigation";
 import { QueryNames } from "@/app/(sok)/_utils/QueryNames";
 import { mediumDisplayName } from "@/app/_common/utils/utils";
 
-function GroupItem({ children, color = "surface-neutral-subtle", semibold }) {
+function GroupItem({ children, color = "surface-neutral-subtle", tag }) {
     return (
-        <Box background={color} padding="05 2" borderRadius="small">
-            <BodyShort size="small" weight={semibold && "semibold"}>
-                {children}
-            </BodyShort>
+        <Box background={color} paddingInline={tag ? "2 1" : "2"} paddingBlock="1" borderRadius="small">
+            <HStack align="center" gap="2">
+                <BodyShort size="small" weight="semibold">
+                    {children}
+                </BodyShort>
+                {tag && (
+                    <Box paddingBlock="05" borderRadius="small" paddingInline="1" background="surface-neutral-subtle">
+                        <BodyShort size="small" textColor="subtle">
+                            {tag}
+                        </BodyShort>
+                    </Box>
+                )}
+            </HStack>
         </Box>
     );
 }
 
 function Debug({ ad }) {
     const searchParams = useSearchParams();
-    const janzzCategories = ad.categoryList?.filter((it) => it.categoryType === "JANZZ") || [];
-    const otherCategories = ad.categoryList?.filter((it) => it.categoryType !== "JANZZ") || [];
-    const synonyms =
+    const janzzOccupations = ad.categoryList?.filter((it) => it.categoryType === "JANZZ") || [];
+    const otherOccupationCategories = ad.categoryList?.filter((it) => it.categoryType !== "JANZZ") || [];
+    const janzzSynonyms =
         ad.properties?.searchtags
             ?.filter((synonym) => {
-                const isDuplicate = janzzCategories.some((janzz) => synonym.label === janzz.name);
+                const isDuplicate = janzzOccupations.some((janzz) => synonym.label === janzz.name);
                 return !isDuplicate;
             })
             .sort((a, b) => a.label.localeCompare(b.label, "no")) || [];
 
     return (
         <VStack gap="4">
-            {searchParams.has(QueryNames.SEARCH_STRING) && (
-                <HStack gap="2">
-                    <BodyShort size="small">score {ad.score?.toFixed(2)}</BodyShort>
-                </HStack>
-            )}
             <HStack gap="2">
-                {janzzCategories.map((category) => (
-                    <GroupItem color="surface-success-subtle" semibold key={category.id}>
+                {searchParams.has(QueryNames.SEARCH_STRING) && (
+                    <GroupItem color="surface-info-moderate">{ad.score?.toFixed(1)}</GroupItem>
+                )}
+
+                {ad.medium && <GroupItem color="surface-info-subtle">{mediumDisplayName(ad.medium)}</GroupItem>}
+
+                {janzzOccupations.map((category) => (
+                    <GroupItem color="surface-success-subtle" key={category.id}>
                         {category.name}
                     </GroupItem>
                 ))}
-
-                {synonyms.map((tag) => (
-                    <GroupItem key={tag.label}>{tag.label}</GroupItem>
+            </HStack>
+            <HStack gap="2" align="center">
+                {janzzSynonyms.map((tag) => (
+                    <GroupItem key={tag.label} tag="janzz">
+                        {tag.label}
+                    </GroupItem>
                 ))}
 
-                {otherCategories.map((category) => (
-                    <GroupItem key={category.id}>
-                        {category.name} ({category.categoryType?.toUpperCase()})
+                {otherOccupationCategories.map((category) => (
+                    <GroupItem key={category.id} tag={category.categoryType?.toLowerCase()}>
+                        {category.name}
                     </GroupItem>
                 ))}
             </HStack>
 
-            {ad.properties?.keywords && (
-                <HStack gap="2" align="center">
-                    <BodyShort size="small">Keywords:</BodyShort>
-                    {ad.properties.keywords.split(",").map((keywords) => (
-                        <GroupItem key={keywords}>{keywords}</GroupItem>
-                    ))}
-                </HStack>
-            )}
-
             {ad.properties?.searchtagsai && Array.isArray(ad.properties.searchtagsai) && (
                 <HStack gap="2" align="center">
-                    <BodyShort size="small">SearchtagsAI:</BodyShort>
                     {ad.properties.searchtagsai.map((searchTagAi) => (
-                        <GroupItem key={searchTagAi}>{searchTagAi}</GroupItem>
+                        <GroupItem key={searchTagAi} tag="ai">
+                            {searchTagAi}
+                        </GroupItem>
                     ))}
                 </HStack>
             )}
 
-            {ad.medium && (
+            {ad.properties?.keywords && (
                 <HStack gap="2" align="center">
-                    <BodyShort size="small">Kilde:</BodyShort>
-                    <GroupItem>{mediumDisplayName(ad.medium)}</GroupItem>
+                    {ad.properties.keywords.split(/[,;]/).map((keyword) => {
+                        if (keyword === "null") return null;
+                        return (
+                            <GroupItem key={keyword} tag="kwrd">
+                                {keyword}
+                            </GroupItem>
+                        );
+                    })}
                 </HStack>
             )}
         </VStack>
     );
 }
-
-Debug.propTypes = {
-    ad: PropTypes.shape({
-        medium: PropTypes.string,
-        categoryList: PropTypes.arrayOf(PropTypes.shape({ name: PropTypes.string, categoryType: PropTypes.string })),
-        occupationList: PropTypes.arrayOf(PropTypes.shape({ level1: PropTypes.string, level2: PropTypes.string })),
-        properties: PropTypes.shape({
-            keywords: PropTypes.string,
-            searchtags: PropTypes.arrayOf(PropTypes.shape({ label: PropTypes.string, score: PropTypes.number })),
-            searchtagsai: PropTypes.arrayOf(PropTypes.string),
-        }),
-        score: PropTypes.number,
-    }),
-    score: PropTypes.string,
-};
 
 export default Debug;
