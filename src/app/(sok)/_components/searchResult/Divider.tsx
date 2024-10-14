ba1a97f5c6da8c62bce17787122e1f20d2b9c13b
otenav <ola.toft-eriksen@nav.no>
2024-10-14 13:22:44 +0200

    Fix divider

--- src/app/(sok)/_components/searchResult/Divider.tsx
+++ src/app/(sok)/_components/searchResult/Divider.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from "react";
-import { BodyShort, Box, Heading, Hide, HStack } from "@navikt/ds-react";
+import { BodyShort, Box, Button, Heading, Hide, HStack } from "@navikt/ds-react";
 import useQuery from "@/app/(sok)/_components/QueryProvider";
 import joinStringWithSeperator from "@/app/_common/utils/joinStringWithSeperator";
 import DetectiveIcon from "@/app/(sok)/_components/icons/DetectiveIcon";
@@ -13,17 +13,20 @@ export default function Divider({ index, indexOfLastWithScoreAboveThreshold }: D
 
     if (indexOfLastWithScoreAboveThreshold !== 0 && indexOfLastWithScoreAboveThreshold === index) {
         return (
-            <Box padding="2 4" borderRadius="small" background="surface-alt-1-subtle" className="mt-12">
+            <Box paddingInline="4" paddingBlock="2" borderRadius="small" background="surface-subtle" className="mt-12">
                 <HStack wrap={false} justify="space-between" align="center" gap="4">
                     <Box>
                         <Heading level="3" size="small" className="mb-05">
-                            Mindre relevante treff
+                            Viser bare mest relevante treff
                         </Heading>
 
-                        <BodyShort>
-                            Treffene under nevner likevel <b>{joinStringWithSeperator(query.getAll("q"), "eller")}</b> i
+                        <BodyShort spacing>
+                            13 flere annonser nevner <b>{joinStringWithSeperator(query.getAll("q"), "eller")}</b> i
                             annonseteksten
                         </BodyShort>
+                        <Button variant="secondary-neutral" size="small">
+                            Søk på nytt med alle treff
+                        </Button>
                     </Box>
                     <Hide below="md">
                         <Box paddingInline="4">
