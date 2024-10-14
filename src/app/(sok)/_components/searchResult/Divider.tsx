99d3b59042cd7fb6415bee56b03046e2a890e4d8
otenav <ola.toft-eriksen@nav.no>
2024-10-14 12:58:43 +0200

    Fix divider

--- src/app/(sok)/_components/searchResult/Divider.tsx
+++ src/app/(sok)/_components/searchResult/Divider.tsx
@@ -1,37 +1,39 @@
 import React, { ReactElement } from "react";
-import { BodyShort, Box, Heading } from "@navikt/ds-react";
+import { BodyShort, Box, Heading, Hide, HStack } from "@navikt/ds-react";
 import useQuery from "@/app/(sok)/_components/QueryProvider";
 import joinStringWithSeperator from "@/app/_common/utils/joinStringWithSeperator";
+import DetectiveIcon from "@/app/(sok)/_components/icons/DetectiveIcon";
 
 interface DividerProps {
     index: number;
-    score: number;
     indexOfLastWithScoreAboveThreshold: number;
 }
-export default function Divider({ index, score, indexOfLastWithScoreAboveThreshold }: DividerProps): ReactElement {
+export default function Divider({ index, indexOfLastWithScoreAboveThreshold }: DividerProps): ReactElement {
     const query = useQuery();
-    const scoreThreshold = 2;
 
-    return (
-        <>
-            {index === 0 && score >= scoreThreshold && (
-                <Box padding="2" borderRadius="small" background="surface-subtle">
-                    <Heading level="2" size="small">
-                        Beste treff
-                    </Heading>
-                </Box>
-            )}
-            {indexOfLastWithScoreAboveThreshold !== 0 && indexOfLastWithScoreAboveThreshold === index && (
-                <Box padding="2" borderRadius="medium" background="surface-subtle" className="mt-16">
-                    <Heading level="2" size="small" className="mb-05">
-                        Mindre relevante treff
-                    </Heading>
-                    <BodyShort>
-                        Treffene under nevner likevel <b>{joinStringWithSeperator(query.getAll("q"), "eller")}</b> i
-                        annonseteksten
-                    </BodyShort>
-                </Box>
-            )}
-        </>
-    );
+    if (indexOfLastWithScoreAboveThreshold !== 0 && indexOfLastWithScoreAboveThreshold === index) {
+        return (
+            <Box padding="3 4" borderRadius="small" background="surface-alt-1-subtle" className="mt-12">
+                <HStack wrap={false} justify="space-between" align="center" gap="4">
+                    <Box>
+                        <Heading level="3" size="small" className="mb-05">
+                            Mindre relevante treff
+                        </Heading>
+
+                        <BodyShort>
+                            Treffene under nevner likevel <b>{joinStringWithSeperator(query.getAll("q"), "eller")}</b> i
+                            annonseteksten
+                        </BodyShort>
+                    </Box>
+                    <Hide below="md">
+                        <Box paddingInline="4">
+                            <DetectiveIcon />
+                        </Box>
+                    </Hide>
+                </HStack>
+            </Box>
+        );
+    }
+
+    return null;
 }
