6ec1057cb40c4ab8bd87564eedd3de352768b170
otenav <ola.toft-eriksen@nav.no>
2024-10-14 13:40:35 +0200

    Fix divider

--- src/app/(sok)/_components/searchResult/Divider.tsx
+++ src/app/(sok)/_components/searchResult/Divider.tsx
@@ -1,8 +1,7 @@
 import React, { ReactElement } from "react";
-import { BodyShort, Box, Button, Heading, Hide, HStack } from "@navikt/ds-react";
+import { BodyShort, Box, Heading } from "@navikt/ds-react";
 import useQuery from "@/app/(sok)/_components/QueryProvider";
 import joinStringWithSeperator from "@/app/_common/utils/joinStringWithSeperator";
-import DetectiveIcon from "@/app/(sok)/_components/icons/DetectiveIcon";
 
 interface DividerProps {
     index: number;
@@ -13,27 +12,15 @@ export default function Divider({ index, indexOfLastWithScoreAboveThreshold }: D
 
     if (indexOfLastWithScoreAboveThreshold !== 0 && indexOfLastWithScoreAboveThreshold === index) {
         return (
-            <Box paddingInline="4" paddingBlock="2" borderRadius="small" background="surface-subtle" className="mt-12">
-                <HStack wrap={false} justify="space-between" align="center" gap="4">
-                    <Box>
-                        <Heading level="3" size="small" className="mb-05">
-                            Viser bare mest relevante treff
-                        </Heading>
+            <Box background="surface-alt-1-subtle" className="mt-16" paddingBlock="4" paddingInline="2">
+                <Heading level="3" size="medium" className="mb-05">
+                    Mindre relevante treff
+                </Heading>
 
-                        <BodyShort spacing>
-                            13 flere annonser nevner <b>{joinStringWithSeperator(query.getAll("q"), "eller")}</b> i
-                            annonseteksten
-                        </BodyShort>
-                        <Button variant="secondary-neutral" size="small">
-                            Søk på nytt med alle treff
-                        </Button>
-                    </Box>
-                    <Hide below="md">
-                        <Box paddingInline="4">
-                            <DetectiveIcon />
-                        </Box>
-                    </Hide>
-                </HStack>
+                <BodyShort>
+                    Treffene er ikke like relevante, men nevner likevel{" "}
+                    <b>{joinStringWithSeperator(query.getAll("q"), "eller")}</b> i annonseteksten
+                </BodyShort>
             </Box>
         );
     }
