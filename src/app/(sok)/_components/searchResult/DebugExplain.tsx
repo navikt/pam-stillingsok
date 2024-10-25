8fd4b98204b794278d55ab0a505905f241209f95
otenav <ola.toft-eriksen@nav.no>
2024-10-25 13:23:32 +0200

    Add score explanation to debug panel

---
+++ src/app/(sok)/_components/searchResult/DebugExplain.tsx
@@ -0,0 +1,49 @@
+import React, { ReactElement } from "react";
+import { BodyShort, Box, HStack, ReadMore } from "@navikt/ds-react";
+
+function ExplainItem({ explanation }): ReactElement {
+    const { description, value } = explanation;
+    let color = "surface-subtle";
+
+    if (value < 0) {
+        color = "surface-danger-subtle";
+    } else if (value === 0) {
+        color = "surface-subtle";
+    } else if (value < 1) {
+        color = "surface-warning-subtle";
+    } else if (value >= 1) {
+        color = "surface-success-subtle";
+    }
+
+    return (
+        <HStack align="center" gap="1" wrap={false}>
+            <Box background={color} padding="05 2" borderRadius="small">
+                <BodyShort textColor="subtle" size="small" className="monospace">
+                    {value.toFixed(2)}
+                </BodyShort>
+            </Box>
+            {description}
+        </HStack>
+    );
+}
+
+export default function DebugExplain({ explanation }): ReactElement {
+    return (
+        <Box paddingBlock="05">
+            {explanation.details.length > 0 ? (
+                <ReadMore
+                    size="small"
+                    header={<ExplainItem explanation={explanation} />}
+                    defaultOpen={["sum of", "max"].some((string) => explanation.description.startsWith(string))}
+                >
+                    {explanation.details.length > 0 &&
+                        explanation.details.map((it) => <DebugExplain explanation={it} key={it.description} />)}
+                </ReadMore>
+            ) : (
+                <Box paddingInline="6 0">
+                    <ExplainItem explanation={explanation} />
+                </Box>
+            )}
+        </Box>
+    );
+}
