86c2d6237c3bc69230925b27acaabfa364786d2d
otenav <ola.toft-eriksen@nav.no>
2024-10-30 13:57:17 +0100

    Migrate url parameter from Førskolelærer to Barnehagelærer

---
+++ src/app/(sok)/_utils/versioning/version05.ts
@@ -0,0 +1,13 @@
+export function migrateToV5(searchParams): URLSearchParams {
+    const migratedSearchParams = new URLSearchParams(searchParams.toString());
+    const occupationOldValue = "Utdanning.Førskolelærer";
+    const occupationNewValue = "Utdanning.Barnehagelærer";
+    const OCCUPATION_SECOND_LEVEL = "occupationLevel2";
+
+    if (migratedSearchParams.has(OCCUPATION_SECOND_LEVEL, occupationOldValue)) {
+        migratedSearchParams.delete(OCCUPATION_SECOND_LEVEL, occupationOldValue);
+        migratedSearchParams.append(OCCUPATION_SECOND_LEVEL, occupationNewValue);
+    }
+
+    return migratedSearchParams;
+}
