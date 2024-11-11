84e5cfaf16bc5e28d5ca356dc0415705d8fe2017
Steffen Aas <steffen.aas@outlook.com>
2024-11-11 14:08:44 +0100

    Endre ZodError log til info

--- src/app/_common/actions/LogZodError.ts
+++ src/app/_common/actions/LogZodError.ts
@@ -3,5 +3,5 @@ import logger from "@/app/_common/utils/logger";
 import { ZodError } from "zod";
 
 export function logZodError(id: string, error: ZodError): void {
-    logger.error("ZodError", { id: id, error: error });
+    logger.info("ZodError", { id: id, error: error });
 }
