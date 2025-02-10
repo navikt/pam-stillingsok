f9c5ce8667108c1e69c83ce4c6ff6290c2d6146f
Khiem Diep <khiem.harald.tu.diep@nav.no>
2025-02-10 14:29:42 +0100

    let a session id live only for 24 hours

--- src/app/_common/monitoring/session.ts
+++ src/app/_common/monitoring/session.ts
@@ -1,13 +1,22 @@
 import { v4 as uuidv4 } from "uuid";
 
 export const SESSION_ID_TAG = "sessionId";
+export const SESSION_ID_TIMESTAMP_TAG = "sessionIdTimestamp";
+const SESSION_MAX_DURATION_MS = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+
 export function getSessionId() {
-    let sessionId = null;
+    let sessionId: string | null = null;
+    let sessionIdTimestamp: string | null = null;
     try {
         sessionId = sessionStorage.getItem(SESSION_ID_TAG);
-        if (sessionId === null) {
+        sessionIdTimestamp = sessionStorage.getItem(SESSION_ID_TIMESTAMP_TAG);
+
+        const now = Date.now();
+        if (!sessionId || !sessionIdTimestamp || now - parseInt(sessionIdTimestamp, 10) > SESSION_MAX_DURATION_MS) {
             sessionId = uuidv4();
+            sessionIdTimestamp = now.toString();
             sessionStorage.setItem(SESSION_ID_TAG, sessionId);
+            sessionStorage.setItem(SESSION_ID_TIMESTAMP_TAG, sessionIdTimestamp);
         }
     } catch (e) {
         // It's possible user has disabled persistent data
