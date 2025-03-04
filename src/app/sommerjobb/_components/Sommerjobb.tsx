7f73ab5826a3f2fabe11a532bd7793f432a18bab
Audun Skjelvan <audun.skjelvan@nav.no>
2025-03-04 10:27:11 +0100

    add some chips

--- src/app/sommerjobb/_components/Sommerjobb.tsx
+++ src/app/sommerjobb/_components/Sommerjobb.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useId } from "react";
-import { Heading } from "@navikt/ds-react";
+import { Chips, Heading } from "@navikt/ds-react";
 
 function Sommerjobb(): JSX.Element {
     const jobbMedId = useId();
@@ -16,9 +16,19 @@ function Sommerjobb(): JSX.Element {
                 <Heading id={jobbMedId} level="2" size="small">
                     Jeg vil jobbe med...
                 </Heading>
-                {/* <Chips aria-labelledby={jobbMedId}>
-                    <ChipsToggle>Butikk</ChipsToggle>
-                </Chips> */}
+                <Chips aria-labelledby={jobbMedId}>
+                    <Chips.Toggle>Butikk</Chips.Toggle>
+                    <Chips.Toggle>Helse</Chips.Toggle>
+                    <Chips.Toggle>Kontor</Chips.Toggle>
+                    <Chips.Toggle>Kultur</Chips.Toggle>
+                    <Chips.Toggle>Kundeservice</Chips.Toggle>
+                    <Chips.Toggle>Lager og industri</Chips.Toggle>
+                    <Chips.Toggle>Renhold</Chips.Toggle>
+                    <Chips.Toggle>Restaurant og cafe</Chips.Toggle>
+                    <Chips.Toggle>Transport</Chips.Toggle>
+                    <Chips.Toggle>Turisme</Chips.Toggle>
+                    <Chips.Toggle>Utendørs</Chips.Toggle>
+                </Chips>
             </section>
             <section></section>
         </div>
