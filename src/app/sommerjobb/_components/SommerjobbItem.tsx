0546b4e791aac8029fdb5b80050f475f6d97aae5
otenav <ola.toft-eriksen@nav.no>
2025-03-04 10:24:13 +0100

    item

---
+++ src/app/sommerjobb/_components/SommerjobbItem.tsx
@@ -0,0 +1,14 @@
+import React, { ReactElement } from "react";
+import { Heading } from "@navikt/ds-react";
+
+function SommerjobbItem(): ReactElement {
+    return (
+        <section>
+            <Heading size="small" level="3">
+                tittel
+            </Heading>
+        </section>
+    );
+}
+
+export default SommerjobbItem;
