6845a13cf5a5bd96b472de4ac587b2c2267ee451
BAH <bjorn.arne.hagen@nav.no>
2025-03-05 13:17:07 +0100

    Legger til deadlineText

--- src/app/sommerjobb/_components/SommerjobbItem.tsx
+++ src/app/sommerjobb/_components/SommerjobbItem.tsx
@@ -5,12 +5,16 @@ import { ChevronRightIcon } from "@navikt/aksel-icons";
 import Employer from "@/app/_common/icons/Employer";
 import Location from "@/app/_common/icons/Location";
 import Calendar from "@/app/_common/icons/Calendar";
+import { formatDate } from "@/app/_common/utils/utils";
+import deadlineText from "@/app/_common/utils/deadlineText";
 
 interface SommerjobbItemProps {
     sommerjobbAd: SommerjobbAd;
 }
 
 function SommerjobbItem({ sommerjobbAd }: SommerjobbItemProps): ReactElement {
+    const deadline = sommerjobbAd.applicationDue ? formatDate(sommerjobbAd.applicationDue) : undefined;
+
     return (
         <div>
             <Box as="article" shadow="small" background="surface-default" borderRadius="small">
@@ -23,20 +27,24 @@ function SommerjobbItem({ sommerjobbAd }: SommerjobbItemProps): ReactElement {
                         <BodyShort spacing>{sommerjobbAd.description}</BodyShort>
 
                         <HStack>
-                            <HStack className={`margin-right`} gap="2">
+                            <HStack className={`margin-right mb-2`} gap="2">
                                 <Employer />
                                 <BodyShort size="small">{sommerjobbAd.employerName}</BodyShort>
                             </HStack>
-                            <HStack gap="2">
+                            <HStack gap="2" className={`mb-2`}>
                                 <Location />
                                 <BodyShort size="small">{sommerjobbAd.location}</BodyShort>
                             </HStack>
                         </HStack>
 
-                        <HStack gap="2">
-                            <Calendar />
-                            <BodyShort size="small">{sommerjobbAd.applicationDue}</BodyShort>
-                        </HStack>
+                        {deadline && sommerjobbAd.applicationDue && (
+                            <HStack gap="2">
+                                <Calendar />
+                                <BodyShort size="small">
+                                    {deadlineText(deadline, new Date(), sommerjobbAd.applicationDue)}
+                                </BodyShort>
+                            </HStack>
+                        )}
                     </div>
                     <ChevronRightIcon aria-hidden="true" className="chevron" />
                 </a>
