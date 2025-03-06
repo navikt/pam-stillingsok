836788081b268aeda785248c361741e9ce414917
Audun Skjelvan <audun.skjelvan@nav.no>
2025-03-06 14:20:30 +0100

    delete sommerjobb2025

--- src/app/sommerjobb2025/_components/constants.ts
+++
@@ -1,9 +0,0 @@
-export const PAGE_PARAM_NAME = "page";
-export const JOB_CATEGORY_PARAM_NAME = "jobbeMed";
-export const DISTANCE_PARAM_NAME = "avstand";
-export const POSTCODE_PARAM_NAME = "postnr";
-
-export const SEARCH_RESULT_SIZE = 10;
-
-export const DEFAULT_DISTANCE = 5;
-export const DISTANCE_VALUES = [1, 3, 5, 7, 10, 20, 30, 50, 75, 100, 150];
