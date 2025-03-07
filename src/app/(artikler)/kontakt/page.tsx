068eeb1c30b2417b72d9894dd5b46927d4fff186
otenav <ola.toft-eriksen@nav.no>
2025-03-07 12:12:21 +0100

    mer struktur

--- src/app/(artikler)/kontakt/page.tsx
+++ src/app/(artikler)/kontakt/page.tsx
@@ -1,5 +1,5 @@
 import { getMetadataTitle } from "@/constants/layout";
-import Kontakt from "@/app/(artikler)/kontakt/Kontakt";
+import Kontakt from "@/app/(artikler)/kontakt/_components/Kontakt";
 
 export const metadata = {
     title: getMetadataTitle("Kontakt oss"),
---
+++ src/app/(forside)/bedrift/page.tsx
@@ -0,0 +1,9 @@
+import { getMetadataTitle } from "@/constants/layout";
+
+export const metadata = {
+    title: getMetadataTitle("todo"),
+};
+
+export default function Page() {
+    return <h1>bedrift</h1>;
+}
