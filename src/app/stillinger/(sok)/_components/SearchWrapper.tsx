fe92b863b2f2d97a071676ad9fa6460eca7055b4
Audun <67591449+audisaudisaudis@users.noreply.github.com>
2025-03-17 10:48:19 +0100

    add debug info to help (#925)

--- src/app/stillinger/(sok)/_components/SearchWrapper.tsx
+++ src/app/stillinger/(sok)/_components/SearchWrapper.tsx
@@ -8,6 +8,7 @@ import { Postcode } from "@/app/stillinger/(sok)/_utils/fetchPostcodes";
 import { SearchResult } from "@/app/stillinger/_common/types/SearchResult";
 import { FetchError } from "@/app/stillinger/(sok)/_utils/fetchTypes";
 import { SearchLocation } from "@/app/stillinger/(sok)/page";
+import useIsDebug from "@/app/stillinger/(sok)/_components/IsDebugProvider";
 
 type SearchWrapperProps = {
     searchResult: SearchResult;
@@ -25,8 +26,11 @@ const SearchWrapper = ({
     resultsPerPage,
     errors,
 }: SearchWrapperProps) => {
+    const { isDebug } = useIsDebug();
+
     return (
         <QueryProvider>
+            {isDebug && <div>Results per page - {resultsPerPage}</div>}
             <Search
                 searchResult={searchResult}
                 locations={locations}
