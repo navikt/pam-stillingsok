6ea0ed2511ea2301e4c655e2008f38f0d99efb8e
otenav <ola.toft-eriksen@nav.no>
2025-03-18 21:19:27 +0100

    Debug

--- src/app/sommerjobb/_components/DebugItem.tsx
+++ src/app/sommerjobb/_components/DebugItem.tsx
@@ -14,23 +14,28 @@ interface SommerjobbItemProps {
 }
 
 function DebugExplain({ explanation }: DebugExplainProps): ReactElement {
+    const result: string[] = [];
+    explain(explanation, result);
     return (
         <>
-            {explanation.details.length > 0 && (
-                <>
-                    {explanation.description.startsWith("weight(") && (
-                        <Box background="surface-subtle" padding="1">
-                            <BodyShort size="small">
-                                {explanation.description.split("weight(")[1].split(" ")[0].split(":").join("=")}
-                            </BodyShort>
-                        </Box>
-                    )}
-                    {explanation.details.length > 0 &&
-                        explanation.details.map((it) => <DebugExplain explanation={it} key={it.description} />)}
-                </>
-            )}
+            {result.map((it) => (
+                <Box background="surface-subtle" padding="1" key={it}>
+                    <BodyShort size="small">{it}</BodyShort>
+                </Box>
+            ))}
         </>
     );
+}
+
+function explain(explanation: StillingSoekResponseExplanation, result: string[]) {
+    if (explanation.details.length > 0) {
+        if (explanation.description.startsWith("weight(")) {
+            result.push(explanation.description.split("weight(")[1].split(" ")[0].split(":").join("="));
+        }
+        explanation.details.forEach((it) => {
+            explain(it, result);
+        });
+    }
 }
 
 function DebugItem({ sommerjobbAd }: SommerjobbItemProps): ReactElement {
