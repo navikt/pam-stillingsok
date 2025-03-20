61915fa29f08e4cea63d041e3cfbcea374739e43
otenav <ola.toft-eriksen@nav.no>
2025-03-20 08:33:51 +0100

    Fjerner debug log

--- src/app/stillinger/(sok)/_components/SearchWrapper.tsx
+++ src/app/stillinger/(sok)/_components/SearchWrapper.tsx
@@ -8,7 +8,6 @@ import { Postcode } from "@/app/stillinger/(sok)/_utils/fetchPostcodes";
 import { SearchResult } from "@/app/stillinger/_common/types/SearchResult";
 import { FetchError } from "@/app/stillinger/(sok)/_utils/fetchTypes";
 import { SearchLocation } from "@/app/stillinger/(sok)/page";
-import useIsDebug from "@/app/stillinger/(sok)/_components/IsDebugProvider";
 
 type SearchWrapperProps = {
     searchResult: SearchResult;
@@ -26,11 +25,8 @@ const SearchWrapper = ({
     resultsPerPage,
     errors,
 }: SearchWrapperProps) => {
-    const { isDebug } = useIsDebug();
-
     return (
         <QueryProvider>
-            {isDebug && <div>Results per page - {resultsPerPage}</div>}
             <Search
                 searchResult={searchResult}
                 locations={locations}
