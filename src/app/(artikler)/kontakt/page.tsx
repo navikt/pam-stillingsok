d0226bb7e7583ede23b5f40a0a64791e0f2e3d5e
Audun Skjelvan <audun.skjelvan@nav.no>
2025-04-01 10:04:14 +0200

    revert

--- src/app/(artikler)/kontakt/page.tsx
+++ src/app/(artikler)/kontakt/page.tsx
@@ -2,7 +2,7 @@ import { getMetadataTitle } from "@/app/metadata";
 import Kontakt from "@/app/(artikler)/kontakt/_components/Kontakt";
 
 export const metadata = {
-    title: getMetadataTitle("Kontakt oss."),
+    title: getMetadataTitle("Kontakt oss"),
 };
 
 export default function Page() {
