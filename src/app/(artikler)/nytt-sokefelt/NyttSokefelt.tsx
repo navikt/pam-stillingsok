19e38502c06400fba14f2772cc59e498769ae407
otenav <ola.toft-eriksen@nav.no>
2025-04-02 12:40:46 +0200

    Artikel NyttSokefelt

---
+++ src/app/(artikler)/nytt-sokefelt/NyttSokefelt.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { BodyLong, Heading, Link as AkselLink, LinkPanel, List } from "@navikt/ds-react";
+import NextLink from "next/link";
+import React from "react";
+import ImageLinkPanelMedium from "@/app/_common/components/ImageLinkPanelMedium";
+
+export default function NyttSokefelt() {
+    return (
+        <article>
+            <div className="container-small mt-5 mb-12">
+                <Heading size="xlarge" level="1" spacing>
+                    Nytt søkefelt - enklere, raskere og mer fleksibelt!
+                </Heading>
+
+                <BodyLong size="large" spacing>
+                    Nå kan du kombinere fritekst og filtre for å finne akkurat jobben du er ute etter.
+                </BodyLong>
+            </div>
+
+            <div className="container-medium mb-12">
+                <img className="article-image" src="/images/ansatt.png" alt="En glad person som står i en butikk." />
+            </div>
+
+            <div className="container-small mb-16">
+                <BodyLong spacing>
+                    Ved å legge til flere ord i søket ditt, vil du oppdage flere relevante stillinger og øke sjansen for
+                    å lande drømmejobben.
+                </BodyLong>
+                <Heading size="small" level="2" spacing>
+                    Måter å bruke det nye søket på:
+                </Heading>
+                <List aria-label="Måter å bruke det nye søket på:" className="mb-6">
+                    <List.Item>
+                        <strong>Utforskende søk</strong> – start bredt for å oppdage ulike muligheter
+                    </List.Item>
+                    <List.Item>
+                        <strong>Spisset søk</strong> – når du har en klar idé om hva slags jobb du vil ha
+                    </List.Item>
+                    <List.Item>
+                        <strong>Se på tvers av bransjer</strong> – søk på ferdigheter og interesser fremfor
+                        tradisjonelle stillingstitler
+                    </List.Item>
+                    <List.Item>
+                        <strong>Finn sesongarbeid</strong> – kombiner nøkkelord for sesongbaserte jobber
+                    </List.Item>
+                </List>
+
+                <BodyLong>
+                    <AkselLink as={NextLink} href="/slik-bruker-du-det-nye-soket" className="mb-12">
+                        Les mer om hvordan du kan bruke søket for best resultat
+                    </AkselLink>
+                </BodyLong>
+                <LinkPanel className="arb-link-panel-primary" href="/stillinger">
+                    <LinkPanel.Title className="navds-link-panel__title navds-heading--small">
+                        Prøv det nye søket
+                    </LinkPanel.Title>
+                </LinkPanel>
+            </div>
+
+            <div className="container-medium mb-24">
+                <Heading size="large" level="2" spacing>
+                    Videre lesning
+                </Heading>
+                <div className="image-link-panel-grid-medium">
+                    <ImageLinkPanelMedium
+                        image="/images/students.jpg"
+                        alt="3 blide studenter som sitter med mobil og pc og snakker sammen utenfor skolen"
+                        title="Tips til jobbsøknaden"
+                        description="Les våre tips om hvordan skrive søknaden slik at en arbeidsgiver får lyst til å møte akkurat deg."
+                        href="/tips-til-jobbsoknaden"
+                        color="secondary"
+                    />
+                    <ImageLinkPanelMedium
+                        image="/images/jobbtreff.jpg"
+                        title="Enklere å finne jobber som kan passe"
+                        alt="Bilde av person med laptop"
+                        description="Vi bruker kunstig intelligens til å plassere annonsen i den kategorien som den (mest sannsynlig) hører hjemme i."
+                        href="/nye-filtre"
+                        color="tertiary"
+                    />
+                </div>
+            </div>
+        </article>
+    );
+}
