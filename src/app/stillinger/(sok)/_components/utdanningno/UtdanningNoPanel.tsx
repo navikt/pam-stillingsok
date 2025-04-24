012dd6553ebc7483ffe19f3754f335881893ff5d
Audun Skjelvan <audun.skjelvan@nav.no>
2025-04-24 13:05:32 +0200

    justify image center on small screens

--- src/app/stillinger/(sok)/_components/utdanningno/UtdanningNoPanel.tsx
+++ src/app/stillinger/(sok)/_components/utdanningno/UtdanningNoPanel.tsx
@@ -1,47 +1,34 @@
 import React from "react";
-import { BodyShort, Box, Heading, HGrid, Link as AkselLink } from "@navikt/ds-react";
-import { useSearchParams } from "next/navigation";
-import { QueryNames } from "@/app/stillinger/(sok)/_utils/QueryNames";
+import { BodyShort, Box, Heading, HGrid, HStack, Link as AkselLink } from "@navikt/ds-react";
 import FigureConfused from "@/app/_common/components/FigureConfused";
 
-type UtdanningNoPanelProps = {
-    resultsPerPage: number;
-    totalAds: number;
-};
-function UtdanningNoPanel({ resultsPerPage, totalAds }: UtdanningNoPanelProps) {
-    const searchParams = useSearchParams();
-    const fromParam = searchParams.get(QueryNames.FROM);
-    const from = fromParam ? parseInt(fromParam, 10) : 0;
+function UtdanningNoPanel() {
+    return (
+        <Box padding={{ xs: "4", md: "6" }} borderRadius="small" background="surface-alt-3-subtle">
+            <HGrid gap="4" columns={{ xs: 1, sm: "repeat(2, minmax(0, auto))" }} align="center">
+                <div>
+                    <Heading level="3" size="small" spacing>
+                        Fant du ikke noe som fristet?
+                    </Heading>
 
-    if (from + resultsPerPage >= totalAds) {
-        return (
-            <Box padding={{ xs: "4", md: "6" }} borderRadius="small" background="surface-alt-3-subtle">
-                <HGrid gap="4" columns="repeat(2, minmax(0, auto))" align="center">
-                    <div>
-                        <Heading level="3" size="small" spacing>
-                            Fant du ikke noe som fristet?
-                        </Heading>
+                    <BodyShort spacing>
+                        Utforsk hvilke{" "}
+                        <AkselLink
+                            className="default-text-color-link"
+                            data-umami-event="Søkeresultat klikk utdanning.no"
+                            href="https://utdanning.no/interessesiden/yrker/"
+                        >
+                            jobber som passer dine interesser på utdanning.no
+                        </AkselLink>
+                    </BodyShort>
+                </div>
 
-                        <BodyShort spacing>
-                            Utforsk hvilke{" "}
-                            <AkselLink
-                                className="default-text-color-link"
-                                data-umami-event="Søkeresultat klikk utdanning.no"
-                                href="https://utdanning.no/interessesiden/yrker/"
-                            >
-                                jobber som passer dine interesser på utdanning.no
-                            </AkselLink>
-                        </BodyShort>
-                    </div>
-
-                    <div>
-                        <FigureConfused />
-                    </div>
-                </HGrid>
-            </Box>
-        );
-    }
-    return null;
+                <HStack justify="center">
+                    <FigureConfused />
+                </HStack>
+            </HGrid>
+        </Box>
+    );
 }
 
 export default UtdanningNoPanel;
