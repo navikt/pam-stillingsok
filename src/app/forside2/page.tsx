43c20951bb398f7d8756e90537bf1a27cd18bdd0
Audun Skjelvan <audun.skjelvan@nav.no>
2025-04-25 06:19:21 +0200

    Use next image

---
+++ src/app/forside2/page.tsx
@@ -0,0 +1,13 @@
+import Home from "@/app/forside2/_components/Home";
+
+export const metadata = {
+    title: "Arbeidsplassen.no - Alle ledige jobber, samlet på én plass",
+    description: "Lete etter jobb skal være enkelt. Fra deltid til direktør, finn jobben som passer for deg.",
+    verification: {
+        google: "I1DCqGuh-OEl_WXTcLI7NaNdS4-MjGo9nS_g2OQoajo",
+    },
+};
+
+export default function Page() {
+    return <Home />;
+}
