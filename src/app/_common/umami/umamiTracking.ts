10627f0132e737763223acd44710b2e3c2da3704
Audun Skjelvan <audun.skjelvan@nav.no>
2025-04-29 15:11:08 +0200

    only track umami consent

--- src/app/_common/umami/umamiTracking.ts
+++ src/app/_common/umami/umamiTracking.ts
@@ -1,12 +1,19 @@
 "use client";
 
 import { getWebsiteId } from "./getWebsiteId";
+import { CookieBannerUtils } from "@navikt/arbeidsplassen-react";
 
 export interface UmamiTrackingData {
     [key: string]: string;
 }
 
 export function umamiTracking(name: string, data?: UmamiTrackingData) {
+    const consentValues = CookieBannerUtils.getConsentValues();
+    // If not agreed to Umami
+    if (!consentValues.analyticsConsent) {
+        return;
+    }
+
     const websiteId = getWebsiteId();
     const hostname = window.location.hostname;
     const screenResolution = `${window.screen.width}x${window.screen.height}`;
