32ddfa5e2f935d9490f5ecf3ce48d51be0b1e0eb
otenav <ola.toft-eriksen@nav.no>
2025-05-02 14:55:34 +0200

    Oppdaterer meta desc

--- src/app/(forside)/page.tsx
+++ src/app/(forside)/page.tsx
@@ -3,7 +3,7 @@ import Home from "@/app/(forside)/_components/Home";
 export const metadata = {
     title: "Arbeidsplassen.no - Alle ledige jobber samlet på én plass",
     description:
-        "På Arbeidsplassen.no kan du søke etter ledige jobber i hele Norge. Er du arbeidsgiver kan du enkelt og kostnadsfritt lyse ut stillinger",
+        "På Arbeidsplassen.no kan du søke etter ledige jobber i hele Norge. Er du arbeidsgiver kan du enkelt og kostnadsfritt lyse ut stillinger.",
     verification: {
         google: "I1DCqGuh-OEl_WXTcLI7NaNdS4-MjGo9nS_g2OQoajo",
     },
