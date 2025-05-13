3be638754fc4ca88dd87a8520b8ae610f1021250
Audun Skjelvan <audun.skjelvan@nav.no>
2025-05-13 12:48:07 +0200

    remove unused code

--- src/app/min-side/_common/components/tilgang/SkjulInnholdHvisIkkeTilgang.jsx
+++ src/app/min-side/_common/components/tilgang/SkjulInnholdHvisIkkeTilgang.jsx
@@ -4,10 +4,6 @@ import React, { useContext } from "react";
 import { PersonaliaContext } from "@/app/min-side/_common/components/context/PersonaliaContext";
 import UngIkkeTilgang from "@/app/min-side/_common/components/tilgang/UngIkkeTilgang";
 
-// function logout() {
-//     window.location.href = `/min-side/oauth2/logout?redirect=/`;
-// }
-
 export default function SkulInnholdHvisIkkeTilgang({ children }) {
     const personalia = useContext(PersonaliaContext);
 
@@ -17,35 +13,6 @@ export default function SkulInnholdHvisIkkeTilgang({ children }) {
 
     if (personalia.data && personalia.data.kanLoggePaa === false) {
         return <></>;
-        // return (
-        //     <Modal
-        //         width="medium"
-        //         onClose={logout}
-        //         header={{
-        //             heading: "Du kan dessverre ikke ta i bruk de innloggede tjenestene",
-        //             closeButton: false,
-        //         }}
-        //         open
-        //     >
-        //         <>
-        //             <Modal.Body>
-        //                 <BodyLong style={{ marginBottom: "2rem" }}>
-        //                     Personnummeret ditt kan ikke brukes for innloggede tjenester og vi må logge deg ut. Vi
-        //                     beklager dette. Du kan fortsatt søke etter stillinger og delta på jobbtreff selv om du ikke
-        //                     er innlogget.
-        //                 </BodyLong>
-        //                 <HStack justify="center">
-        //                     <WorriedFigure />
-        //                 </HStack>
-        //             </Modal.Body>
-        //             <Modal.Footer>
-        //                 <Button variant="primary" onClick={logout}>
-        //                     Logg ut
-        //                 </Button>
-        //             </Modal.Footer>
-        //         </>
-        //     </Modal>
-        // );
     }
 
     return <>{children}</>;
