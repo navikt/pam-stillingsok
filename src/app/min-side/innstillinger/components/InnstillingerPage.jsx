42ddf88ebd810830f67169de0ea45414945a7810
Audun Skjelvan <audun.skjelvan@nav.no>
2025-05-13 12:22:53 +0200

    remove text

--- src/app/min-side/innstillinger/components/InnstillingerPage.jsx
+++ src/app/min-side/innstillinger/components/InnstillingerPage.jsx
@@ -60,7 +60,7 @@ export default function InnstillingerPage() {
                         </Alert>
                     )}
                     <Heading level="1" size="xlarge" align="center" className="mb-12">
-                        Samtykker og innstillinger riktig side
+                        Samtykker og innstillinger
                     </Heading>
 
                     <VStack align="left" className="mb-8">
