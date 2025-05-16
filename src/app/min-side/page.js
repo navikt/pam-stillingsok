fb5c81f0a2dabece6f2b1d5bf11d159a955dd629
otenav <ola.toft-eriksen@nav.no>
2025-05-16 14:13:54 +0200

    Forenkler oppsett for metadata title

--- src/app/min-side/page.js
+++ src/app/min-side/page.js
@@ -1,6 +1,10 @@
 import MinSidePage from "@/app/min-side/_common/components/MinSidePage";
 import { ensureUserLoggedIn } from "@/app/min-side/_common/utils/ensureUserLoggedIn";
 
+export const metadata = {
+    title: "Min side",
+};
+
 export default async function Page() {
     await ensureUserLoggedIn();
     return <MinSidePage />;
