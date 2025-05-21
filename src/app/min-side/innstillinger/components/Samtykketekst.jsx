0a82e4bda4d557717eb14ed961989a0d4ec78464
Audun Skjelvan <audun.skjelvan@nav.no>
2025-05-21 10:17:55 +0200

    oppdater tekst

--- src/app/min-side/innstillinger/components/Samtykketekst.jsx
+++ src/app/min-side/innstillinger/components/Samtykketekst.jsx
@@ -1,4 +1,5 @@
-import { BodyLong, Heading, Link as DsLink } from "@navikt/ds-react";
+import Link from "next/link";
+import { BodyLong } from "@navikt/ds-react";
 import React from "react";
 
 export default function Samtykketekst() {
@@ -8,33 +9,14 @@ export default function Samtykketekst() {
                 Nav er ansvarlig for at personopplysninger blir behandlet etter personopplysningsloven. Det er frivillig
                 å ta i bruk tjenestene.
             </BodyLong>
-            <BodyLong className="mb-6">
-                I{" "}
-                <DsLink href="https://www.nav.no/personvernerklaering" inlineText>
-                    Navs personvernerklæring
-                </DsLink>{" "}
-                finner du generell informasjon om behandling av personopplysninger i Nav og hvilke rettigheter du har.
+            <BodyLong spacing>
+                Vi lagrer dine favoritter, søk med kriterier og e-postadresse (valgfritt). Det er kun du som kan se
+                dette, og vi deler ikke informasjonen med andre. Anonymiserte data kan brukes til statistikk.
             </BodyLong>
-            <Heading level="4" size="small">
-                Vi deler ikke din informasjon med andre
-            </Heading>
-            <BodyLong className="mb-6">
-                Det er kun du som som har tilgang til dine lagrede søk og favoritter. Vi deler hverken disse eller din
-                e-postadresse med andre.
-            </BodyLong>
-            <Heading level="4" size="small">
-                Anonymiserte data kan brukes til statistiske formål
-            </Heading>
-            <BodyLong className="mb-6">
-                Dine lagrede søk og favoritter kan, etter anonymisering, brukes til statistiske formål.
-            </BodyLong>
-            <Heading level="4" size="small">
-                Du kan når som helst slette samtykket ditt
-            </Heading>
             <BodyLong>
-                Du kan når som helst slette samtykket ditt inne på dine innstillinger. Når du sletter ditt samtykke vil
-                dine lagrede søk og favoritter slettes, i tillegg til e-postadressen for varsel dersom du har oppgitt
-                det.
+                Du kan når som helst trekke samtykket i innstillingene. Da slettes alle lagrede søk, favoritter og
+                eventuell e-postadresse du har oppgitt. Les mer i{" "}
+                <Link href="/personvern">Arbeidsplassens personvernerklæring</Link>.
             </BodyLong>
         </>
     );
