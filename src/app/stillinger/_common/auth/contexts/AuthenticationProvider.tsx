dcb8af4f3620b8283e3b4f0a0bd503d8594dba2b
Audun Skjelvan <audun.skjelvan@nav.no>
2025-05-22 09:41:25 +0200

    push state before logging out

--- src/app/stillinger/_common/auth/contexts/AuthenticationProvider.tsx
+++ src/app/stillinger/_common/auth/contexts/AuthenticationProvider.tsx
@@ -43,7 +43,8 @@ function AuthenticationProvider({ children }: AuthenticationProviderProps) {
     const [hasBeenLoggedIn, setHasBeenLoggedIn] = useState(false);
 
     const timeoutLogout = () => {
-        window.location.assign(`/oauth2/logout?redirect=${encodeURIComponent("/utlogget?timeout=true")}`);
+        window.history.pushState({}, "", window.location.href);
+        window.location.href = `/oauth2/logout?redirect=${encodeURIComponent("/utlogget?timeout=true")}`;
     };
     const markAsLoggedOut = () => {
         void deleteCookie("organizationNumber");
@@ -65,7 +66,8 @@ function AuthenticationProvider({ children }: AuthenticationProviderProps) {
 
     function logout() {
         void deleteCookie("organizationNumber");
-        window.location.assign(`/oauth2/logout?redirect=${encodeURIComponent("/utlogget")}`);
+        window.history.pushState({}, "", window.location.href);
+        window.location.href = `/oauth2/logout?redirect=${encodeURIComponent("/utlogget")}`;
     }
 
     const fetchIsAuthenticated = async () => {
