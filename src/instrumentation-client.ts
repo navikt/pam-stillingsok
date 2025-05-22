5c6f117dd1f8e94968d3a6950c8d1f185315c95e
Audun Skjelvan <audun.skjelvan@nav.no>
2025-05-22 11:32:38 +0200

    client sentry

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,20 +1,26 @@
+// src/instrumentation-client.ts
 import * as Sentry from "@sentry/nextjs";
-import { thirdPartyErrorFilterIntegration } from "@sentry/browser";
 
-export function register() {
-    Sentry.init({
-        dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-        allowUrls: ["arbeidsplassen.nav.no", "arbeidsplassen.intern.dev.nav.no"],
-        tracesSampleRate: 0.1,
-        debug: false,
-        release: process.env.SENTRY_RELEASE,
-        integrations: [
-            thirdPartyErrorFilterIntegration({
-                filterKeys: ["pam-stillingsok-app"],
-                behaviour: "drop-error-if-contains-third-party-frames",
-            }),
-        ],
-    });
+console.log("process.env.NEXT_PUBLIC_SENTRY_DSN", process.env.NEXT_PUBLIC_SENTRY_DSN);
+console.log("process.env.SENTRY_RELEASE", process.env.SENTRY_RELEASE);
+// Check if running in browser
+if (typeof window !== "undefined") {
+    try {
+        Sentry.init({
+            dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+            allowUrls: ["arbeidsplassen.nav.no", "arbeidsplassen.intern.dev.nav.no"],
+            tracesSampleRate: 0.1,
+            debug: true,
+            release: process.env.SENTRY_RELEASE,
+            // integrations: [
+            //     thirdPartyErrorFilterIntegration({
+            //         filterKeys: ["pam-stillingsok-app"],
+            //         behaviour: "drop-error-if-contains-third-party-frames",
+            //     }),
+            // ],
+        });
+        console.log("Sentry initialized successfully");
+    } catch (error) {
+        console.error("Sentry initialization failed:", error);
+    }
 }
-
-export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
