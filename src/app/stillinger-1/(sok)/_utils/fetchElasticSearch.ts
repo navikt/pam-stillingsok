dc1374c12ce0c5a4cc961345bfcf2c1cb225b336
Audun Skjelvan <audun.skjelvan@nav.no>
2025-06-24 14:07:58 +0200

    make it work

--- src/app/stillinger-1/(sok)/_utils/fetchElasticSearch.ts
+++ src/app/stillinger-1/(sok)/_utils/fetchElasticSearch.ts
@@ -3,13 +3,8 @@
 import elasticSearchRequestBody from "@/app/stillinger-1/(sok)/_utils/elasticSearchRequestBody";
 import { getDefaultHeaders } from "@/app/stillinger/_common/utils/fetch";
 import simplifySearchResponse from "@/app/stillinger/(sok)/_utils/simplifySearchResponse";
-import { unstable_cache } from "next/cache"; // eslint-disable-line
-import { elasticSearchDurationHistogram, incrementElasticSearchRequests } from "@/metrics";
-import {
-    fetchLocationsWithinDrivingDistance,
-    Locations,
-} from "@/app/stillinger/(sok)/_utils/fetchLocationsWithinDrivingDistance";
-import { StillingSoekResponseSchema } from "@/server/schemas/stillingSearchSchema";
+import { incrementElasticSearchRequests } from "@/metrics";
+import { Locations } from "@/app/stillinger/(sok)/_utils/fetchLocationsWithinDrivingDistance";
 import { FetchResult } from "@/app/stillinger/(sok)/_utils/fetchTypes";
 import { SearchResult } from "@/app/stillinger/_common/types/SearchResult";
 import { SearchQuery } from "@/app/stillinger/(sok)/_utils/query";
@@ -19,38 +14,13 @@ export type ExtendedQuery = SearchQuery & {
     withinDrivingDistance?: Locations | undefined;
 };
 
-export async function fetchElasticSearch(
-    query: SearchQuery,
-    headers: HeadersInit,
-    fetchOptions = {},
-    performSearchIfDrivingDistanceError = true,
-) {
+export async function fetchElasticSearch(query: SearchQuery, headers: HeadersInit, fetchOptions = {}) {
     const elasticSearchQuery: ExtendedQuery = { ...query };
-    const shouldLookupLocationsWithinDrivingDistance = elasticSearchQuery.postcode && elasticSearchQuery.distance;
-    const errors = [];
 
-    if (shouldLookupLocationsWithinDrivingDistance) {
-        const withinDrivingDistanceResult = await fetchLocationsWithinDrivingDistance(
-            elasticSearchQuery.postcode,
-            elasticSearchQuery.distance,
-        );
+    const body = await elasticSearchRequestBody(elasticSearchQuery);
 
-        if (withinDrivingDistanceResult.data) {
-            elasticSearchQuery.withinDrivingDistance = withinDrivingDistanceResult.data;
-        }
+    console.log(JSON.stringify(body, null, 2));
 
-        if (withinDrivingDistanceResult.errors) {
-            errors.push(...withinDrivingDistanceResult.errors);
-
-            if (!performSearchIfDrivingDistanceError) {
-                return { errors };
-            }
-        }
-    }
-
-    const measureSearchDuration = elasticSearchDurationHistogram.startTimer();
-
-    const body = elasticSearchRequestBody(elasticSearchQuery);
     const res = await fetch(`${process.env.PAMSEARCHAPI_URL}/api/ad/_search`, {
         method: "POST",
         headers,
@@ -58,20 +28,16 @@ export async function fetchElasticSearch(
         ...fetchOptions,
     });
 
-    measureSearchDuration();
     incrementElasticSearchRequests(res.ok);
 
-    return { errors, response: res };
+    return { undefined, response: res };
 }
 
-export const fetchCachedSimplifiedElasticSearch = unstable_cache(
-    async (query) => {
-        const headers = await getDefaultHeaders();
-        return fetchSimplifiedElasticSearch(query, headers);
-    },
-    ["elastic-search-query"],
-    { revalidate: 60 },
-);
+// eslint-disable-next-line
+export const fetchCachedSimplifiedElasticSearch = async (query) => {
+    const headers = await getDefaultHeaders();
+    return fetchSimplifiedElasticSearch(query, headers);
+};
 
 async function fetchSimplifiedElasticSearch(
     query: SearchQuery,
@@ -86,7 +52,7 @@ async function fetchSimplifiedElasticSearch(
     }
 
     const data = await response.json();
-    const parsedData = StillingSoekResponseSchema.safeParse(data);
+    const parsedData = data;
 
     if (!parsedData.success) {
         logZodError("søk", parsedData.error);
@@ -99,6 +65,7 @@ async function fetchSimplifiedElasticSearch(
 
     return {
         data: simplifySearchResponse(parsedData.data),
+        // eslint-disable-next-line
         errors: result.errors,
     };
 }
---
+++ src/app/stillinger-1/(sok)/_utils/getVector.ts
@@ -0,0 +1,26 @@
+import { AzureOpenAI } from "openai";
+
+// eslint-disable-next-line
+export async function getVector(query: any) {
+    const endpoint = "https://arbeidsmarked-dev.openai.azure.com";
+    const modelName = "text-embedding-3-large";
+    const apiKey = process.env.NEXT_PUBLIC_AZURE_OPENAI_KEY;
+    const apiVersion = "2024-04-01-preview";
+    const deployment = "arbeidsplassen-embedding-3-large";
+    const options = {
+        endpoint,
+        apiKey,
+        deployment,
+        apiVersion,
+        dangerouslyAllowBrowser: true,
+    };
+
+    const client = new AzureOpenAI(options);
+
+    const response = client.embeddings.create({
+        input: query,
+        model: modelName,
+    });
+
+    return response;
+}
