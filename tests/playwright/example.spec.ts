6406e036e737268c37c8359a2ff66d5cff2119d2
Audun Skjelvan <audun.skjelvan@nav.no>
2025-07-01 12:23:20 +0200

    test

--- tests/playwright/example.spec.ts
+++ tests/playwright/example.spec.ts
@@ -5,7 +5,7 @@ test.use({
 });
 
 test("has title", async ({ page }) => {
-    await page.goto("/");
+    await page.goto("/stillinger");
 
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle("Arbeidsplassen.no - Alle ledige jobber, samlet på én plass");
