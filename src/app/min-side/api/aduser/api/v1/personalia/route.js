ebcc51c08a8d1ef3046b21916f3ff7954943d934
Steffen Aas <steffen.aas@outlook.com>
2025-08-19 16:37:04 +0200

    fix route issue

--- src/app/min-side/api/aduser/api/v1/personalia/route.js
+++ src/app/min-side/api/aduser/api/v1/personalia/route.js
@@ -1,4 +1,5 @@
 export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
 import { exchangeToken } from "@/app/min-side/_common/utils/isTokenValid.node";
 import logger from "@/app/min-side/_common/utils/logger";
 import { v4 as uuidv4 } from "uuid";
