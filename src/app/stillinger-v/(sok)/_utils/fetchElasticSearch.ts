49cade188e9308d1f9abac841bff249f0a3dc4f1
Khiem Diep <khiem.harald.tu.diep@nav.no>
2025-09-05 09:13:01 +0200

    add pure vector search

---
+++ src/app/stillinger-v/(sok)/_utils/fetchElasticSearch.ts
@@ -0,0 +1,79 @@
+"use server";
+
+import elasticSearchRequestBody from "@/app/stillinger-v/(sok)/_utils/elasticSearchRequestBody";
+import { getDefaultHeaders } from "@/app/stillinger/_common/utils/fetch";
+import simplifySearchResponse from "@/app/stillinger/(sok)/_utils/simplifySearchResponse";
+import { incrementElasticSearchRequests } from "@/metrics";
+import { Locations } from "@/app/stillinger/(sok)/_utils/fetchLocationsWithinDrivingDistance";
+import { FetchResult } from "@/app/stillinger/(sok)/_utils/fetchTypes";
+import { SearchResult } from "@/app/stillinger/_common/types/SearchResult";
+import { SearchQuery } from "@/app/stillinger/(sok)/_utils/query";
+
+export type ExtendedQuery = SearchQuery & {
+    withinDrivingDistance?: Locations | undefined;
+};
+
+export async function fetchElasticSearch(query: SearchQuery, headers: HeadersInit, fetchOptions = {}) {
+    const elasticSearchQuery: ExtendedQuery = { ...query };
+
+    const body = await elasticSearchRequestBody(elasticSearchQuery);
+
+    // console.log("BODY", body);
+    // console.log(JSON.stringify(body, null, 2));
+
+    // console.log(`elastSearchRequestBody: ${JSON.stringify(body)}`);
+
+    const res = await fetch(`${process.env.PAMSEARCHAPI_URL}/api/ad/_search`, {
+        method: "POST",
+        headers,
+        body: JSON.stringify(body),
+        ...fetchOptions,
+    });
+    console.info(`ElasticSearch request took ${res.headers.get("x-response-time")} ms`);
+    if (!res.ok) {
+        console.info(`Search query failed: ${res.status} - ${res.statusText}`);
+    }
+
+    incrementElasticSearchRequests(res.ok);
+
+    return { undefined, response: res };
+}
+
+// @ts-expect-error nada
+export const fetchCachedSimplifiedElasticSearch = async (query) => {
+    const headers = await getDefaultHeaders();
+    return fetchSimplifiedElasticSearch(query, headers);
+};
+
+async function fetchSimplifiedElasticSearch(
+    query: SearchQuery,
+    headers: HeadersInit,
+): Promise<FetchResult<SearchResult>> {
+    const result = await fetchElasticSearch(query, headers);
+
+    const { response } = result;
+
+    if (!response?.ok) {
+        console.error(`Failed to fetch data from elastic search: ${response?.status}`);
+        throw new Error(`Failed to fetch data from elastic search: ${response?.status}`);
+    }
+
+    const data = await response.json();
+    const parsedData = data;
+    // console.log("DATA", parsedData);
+
+    // if (!parsedData.success) {
+    //     logZodError("søk", parsedData.error);
+
+    //     return {
+    //         data: simplifySearchResponse(data),
+    //         errors: [],
+    //     };
+    // }
+
+    return {
+        data: simplifySearchResponse(parsedData),
+        // @ts-expect-error nada
+        errors: result.errors,
+    };
+}
