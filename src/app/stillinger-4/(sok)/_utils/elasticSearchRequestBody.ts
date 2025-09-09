96d1c8502b9a50ba05d84d802f75045d06db7508
Christoffer Karlsson <christoffer.karlsson@nav.no>
2025-09-09 10:25:23 +0200

    Add a cutoff of 0.7 for keyword search

--- src/app/stillinger-4/(sok)/_utils/elasticSearchRequestBody.ts
+++ src/app/stillinger-4/(sok)/_utils/elasticSearchRequestBody.ts
@@ -838,6 +838,7 @@ const elasticSearchRequestBody = (query: ExtendedQuery) => {
         from: from || 0,
         size: size && ALLOWED_NUMBER_OF_RESULTS_PER_PAGE.includes(size) ? size : SEARCH_CHUNK_SIZE,
         track_total_hits: true,
+        min_score: 0.7,
         query: mainQueryTemplateFunc(q),
         post_filter: {
             bool: {
