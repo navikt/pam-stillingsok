288b0eba152e0f6d77b59886c5a07e6591b401e7
otenav <29566394+otenav@users.noreply.github.com>
2025-09-09 15:58:29 +0200

    Legger metadata i readmore panel

--- src/app/stillinger/(sok)/_components/searchResult/Debug.tsx
+++ src/app/stillinger/(sok)/_components/searchResult/Debug.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from "react";
-import { BodyShort, Box, HStack, VStack } from "@navikt/ds-react";
+import { BodyShort, Box, HStack, ReadMore, VStack } from "@navikt/ds-react";
 import { useSearchParams } from "next/navigation";
 import { QueryNames } from "@/app/stillinger/(sok)/_utils/QueryNames";
 import DebugExplain from "@/app/stillinger/(sok)/_components/searchResult/DebugExplain";
@@ -12,10 +12,10 @@ interface GroupItemProps {
 function GroupItem({ children }: GroupItemProps): ReactElement {
     return (
         <Box
-            background="surface-neutral-subtle"
+            background="surface-subtle"
             paddingInline="2"
             paddingBlock="05"
-            borderRadius="small"
+            borderRadius="medium"
             borderColor="border-subtle"
             borderWidth="1"
         >
@@ -36,73 +36,69 @@ function Debug({ ad }: DebugProps): ReactElement {
 
     return (
         <VStack gap="2">
-            {ad.score && (
-                <BodyShort size="medium" weight="semibold">
-                    Score: {ad.score.toFixed(2)}
-                </BodyShort>
-            )}
-
             {searchParams.has(QueryNames.SEARCH_STRING) && ad.explanation && (
-                <div>
-                    <BodyShort size="small" spacing>
-                        Explanation:
-                    </BodyShort>
-                    {<DebugExplain explanation={ad.explanation} />}
-                </div>
-            )}
-            <div>
-                <BodyShort size="small" spacing>
-                    categoryList:
-                </BodyShort>
-                <HStack gap="1" align="center">
-                    {ad.categoryList
-                        ?.map((category) => `${category.name} (${category.categoryType.toLowerCase()})`)
-                        .filter((value, index, array) => array.indexOf(value) === index) // remove duplicates
-                        .map((item) => (
-                            <GroupItem key={item}>{item}</GroupItem>
-                        ))}
-                </HStack>
-            </div>
-
-            {ad.searchtags && (
-                <div>
-                    <BodyShort size="small" spacing>
-                        searchtags:
-                    </BodyShort>
-
-                    <HStack gap="1" align="center">
-                        {ad?.searchtags?.map((tag) => (
-                            <GroupItem key={tag.label}>{tag.label}</GroupItem>
-                        ))}
-                    </HStack>
-                </div>
+                <ReadMore size="small" header="Score explanation" defaultOpen>
+                    <DebugExplain explanation={ad.explanation} />
+                </ReadMore>
             )}
 
-            {ad.searchtagsai && Array.isArray(ad.searchtagsai) && (
-                <div>
-                    <BodyShort size="small" spacing>
-                        searchtagsai:
-                    </BodyShort>
-                    <HStack gap="1" align="center">
-                        {ad.searchtagsai.map((searchTagAi: string) => (
-                            <GroupItem key={searchTagAi}>{searchTagAi}</GroupItem>
-                        ))}
-                    </HStack>
-                </div>
-            )}
+            <ReadMore size="small" header="Search tags">
+                <VStack gap="2">
+                    <div>
+                        <BodyShort size="small" spacing>
+                            categoryList:
+                        </BodyShort>
+                        <HStack gap="2" align="center">
+                            {ad.categoryList
+                                ?.map((category) => `${category.name} (${category.categoryType.toLowerCase()})`)
+                                .filter((value, index, array) => array.indexOf(value) === index) // remove duplicates
+                                .map((item) => (
+                                    <GroupItem key={item}>{item}</GroupItem>
+                                ))}
+                        </HStack>
+                    </div>
 
-            {keywords.length > 0 && (
-                <div>
-                    <BodyShort size="small" spacing>
-                        keywords:
-                    </BodyShort>
-                    <HStack gap="1" align="center">
-                        {keywords.map((keyword: string) => (
-                            <GroupItem key={keyword}>{keyword}</GroupItem>
-                        ))}
-                    </HStack>
-                </div>
-            )}
+                    {ad.searchtags && (
+                        <div>
+                            <BodyShort size="small" spacing>
+                                searchtags:
+                            </BodyShort>
+
+                            <HStack gap="2" align="center">
+                                {ad?.searchtags?.map((tag) => (
+                                    <GroupItem key={tag.label}>{tag.label}</GroupItem>
+                                ))}
+                            </HStack>
+                        </div>
+                    )}
+
+                    {ad.searchtagsai && Array.isArray(ad.searchtagsai) && (
+                        <div>
+                            <BodyShort size="small" spacing>
+                                searchtagsai:
+                            </BodyShort>
+                            <HStack gap="2" align="center">
+                                {ad.searchtagsai.map((searchTagAi: string) => (
+                                    <GroupItem key={searchTagAi}>{searchTagAi}</GroupItem>
+                                ))}
+                            </HStack>
+                        </div>
+                    )}
+
+                    {keywords.length > 0 && (
+                        <div>
+                            <BodyShort size="small" spacing>
+                                keywords:
+                            </BodyShort>
+                            <HStack gap="2" align="center">
+                                {keywords.map((keyword: string) => (
+                                    <GroupItem key={keyword}>{keyword}</GroupItem>
+                                ))}
+                            </HStack>
+                        </div>
+                    )}
+                </VStack>
+            </ReadMore>
         </VStack>
     );
 }
