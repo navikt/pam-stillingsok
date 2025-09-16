f2d9e3a0397137fd45175e2f3b31cceb23c43066
Christoffer Karlsson <christoffer.karlsson@nav.no>
2025-09-16 15:41:43 +0200

    Add cookie consent when sending data to Grafana

--- src/app/api/internal/metrics/route.ts
+++ src/app/api/internal/metrics/route.ts
@@ -9,6 +9,6 @@ export async function POST(request: Request): Promise<Response> {
     // There's no good way to get the dynamic path in Next with app router, so we're removing the ID's manually from the path,
     // to avoid having 20k+ different paths in the metrics.
     const path = data.path.replaceAll(UUID_PATTERN, "[id]");
-    httpRequests.inc({ method: data.method, path });
+    httpRequests.inc({ method: data.method, path, cookieConsent: data.cookieConsent });
     return new Response();
 }
