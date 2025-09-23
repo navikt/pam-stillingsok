4767df883825bd56e62a8481caf8b834c2c70fd6
Minh Ha Do <minh.ha.do@nav.no>
2025-09-23 12:03:18 +0200

    Add prefix length to fuzzy match to reduce matches

--- src/app/stillinger-3/(sok)/_utils/elasticSearchRequestBody.ts
+++ src/app/stillinger-3/(sok)/_utils/elasticSearchRequestBody.ts
@@ -773,6 +773,7 @@ function businessNameFreeTextSearchMatch(queries: string[]) {
                 query: q,
                 fuzziness: "AUTO",
                 max_expansions: 2,
+                prefix_length: 2,
                 operator: "and",
                 boost: 2,
             },
