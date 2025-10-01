35addb5ad4a3fcffef5ecfefc8a363ce9a442bd4
Audun Skjelvan <audun.skjelvan@nav.no>
2025-10-01 14:07:25 +0200

    extracted code to useSkyra

--- src/app/_common/skyra/SkyraSurvey.tsx
+++ src/app/_common/skyra/SkyraSurvey.tsx
@@ -1,78 +1,31 @@
 "use client";
 import { Button, Popover } from "@navikt/ds-react";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { createPortal } from "react-dom";
+import { useSkyra } from "@/app/_common/skyra/useSkyra";
 
 type SkyraSurveyProps = {
     buttonText: string;
     skyraSlug: string;
 };
 
-const SkyraSurvey = ({ buttonText, skyraSlug }: SkyraSurveyProps) => {
+export default function SkyraSurvey({ buttonText, skyraSlug }: SkyraSurveyProps): JSX.Element {
     const buttonRef = useRef<HTMLButtonElement>(null);
     const skyraSurveyRef = useRef<HTMLElement>(null);
-    const [openState, setOpenState] = useState(false);
-    const [initialCheckDone, setInitialCheckDone] = useState(false);
+    const [openState, setOpenState] = useState<boolean>(false);
 
-    useEffect(() => {
-        if (!skyraSurveyRef.current || !openState) {
-            setInitialCheckDone(false);
-            return;
-        }
-
-        const checkShadowContent = () => {
-            const element = skyraSurveyRef.current;
-            if (element && element.shadowRoot && element.shadowRoot.childElementCount > 0) {
-                return true;
-            }
-            return false;
-        };
-
-        const initialCheckTimeout = setTimeout(() => {
-            const hasShadowContent = checkShadowContent();
-
-            if (!hasShadowContent && openState) {
-                setOpenState(false);
-            }
-
-            setInitialCheckDone(true);
-        }, 250);
-
-        const observer = new MutationObserver(() => {
-            if (initialCheckDone && !checkShadowContent() && openState) {
-                setOpenState(false);
-                window.location.reload();
-            }
-        });
-
-        if (skyraSurveyRef.current) {
-            observer.observe(skyraSurveyRef.current, {
-                childList: true,
-                subtree: true,
-                attributes: true,
-            });
-
-            if (skyraSurveyRef.current.shadowRoot) {
-                observer.observe(skyraSurveyRef.current.shadowRoot, {
-                    childList: true,
-                    subtree: true,
-                });
-            }
-        }
-
-        return () => {
-            clearTimeout(initialCheckTimeout);
-            observer.disconnect();
-        };
-    }, [openState, initialCheckDone]);
+    useSkyra({
+        skyraSurveyRef,
+        openState,
+        setOpenState,
+        delayMs: 250,
+    });
 
     return (
         <>
             <Button
                 ref={buttonRef}
-                onClick={() => {
-                    setOpenState(!openState);
-                }}
+                onClick={() => setOpenState((prev) => !prev)}
                 aria-expanded={openState}
                 variant="tertiary"
             >
@@ -83,18 +36,12 @@ const SkyraSurvey = ({ buttonText, skyraSlug }: SkyraSurveyProps) => {
                 createPortal(
                     <Popover open={openState} onClose={() => setOpenState(false)} anchorEl={buttonRef.current}>
                         <Popover.Content className="skyra-popover-content">
-                            <div>
-                                {/* @ts-expect-error Ikke typet */}
-                                <skyra-survey ref={skyraSurveyRef} slug={skyraSlug}>
-                                    {/* @ts-expect-error Ikke typet */}
-                                </skyra-survey>
-                            </div>
+                            {/* @ts-expect-error Ikke typet */}
+                            <skyra-survey ref={skyraSurveyRef} slug={skyraSlug} />
                         </Popover.Content>
                     </Popover>,
                     document.body,
                 )}
         </>
     );
-};
-
-export default SkyraSurvey;
+}
