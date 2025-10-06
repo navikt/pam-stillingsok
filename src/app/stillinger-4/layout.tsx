d35a93696c08ef5dbfa9d2e2eaff36503b3725f9
Audun <67591449+audisaudisaudis@users.noreply.github.com>
2025-10-06 12:16:41 +0200

    Remove code used for vector test (#1059)

--- src/app/stillinger-4/layout.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import FavouritesProvider from "@/app/stillinger/favoritter/_components/FavouritesProvider";
-
-export default function StillingerLayout({ children }: { children: React.ReactNode }) {
-    return <FavouritesProvider>{children}</FavouritesProvider>;
-}
