9810089c3a3e6a526ee08e7f8f26c93f98bbbccd
Audun Skjelvan <audun.skjelvan@nav.no>
2025-10-13 09:15:27 +0200

    proper catch

--- src/app/_common/actions/metrics.ts
+++ src/app/_common/actions/metrics.ts
@@ -6,13 +6,11 @@ type MetricsData = {
     cookieConsent: string;
 };
 
-export async function trackMetrics(data: MetricsData) {
-    try {
-        fetch(`http://localhost:${process.env.PORT}/api/internal/metrics`, {
-            method: "POST",
-            body: JSON.stringify({ method: data.method, path: data.path, cookieConsent: data.cookieConsent }),
-        });
-    } catch (error) {
+export function trackMetrics(data: MetricsData) {
+    fetch(`http://localhost:${process.env.PORT}/api/internal/metrics`, {
+        method: "POST",
+        body: JSON.stringify({ method: data.method, path: data.path, cookieConsent: data.cookieConsent }),
+    }).catch((error) => {
         console.error("Error recording metric:", error);
-    }
+    });
 }
