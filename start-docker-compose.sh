46bf1e1721a4efde52a991ab753ce043e7eec9c0
Christoffer Karlsson <christoffer.karlsson@nav.no>
2025-11-05 11:01:00 +0100

    Use new search API in local setup

--- start-docker-compose.sh
+++ start-docker-compose.sh
@@ -4,7 +4,7 @@
 git -C ../pam-docker-compose-shared pull
 
 # Start shared docker compose services (must be sourced, or DOCKER_COMPOSE_COMMAND won't be available)
-. ../pam-docker-compose-shared/start-docker-compose.sh postgres mock-oauth2-server redis kafka
+. ../pam-docker-compose-shared/start-docker-compose.sh postgres mock-oauth2-server redis kafka pam-geografi
 
 # Create databases
 ../pam-docker-compose-shared/create-database.sh "pam-aduser"
