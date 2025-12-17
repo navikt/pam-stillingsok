ba8338554e0537dc9a54680b8c06ab2a94785866
Steffen Aas <steffen.aas@outlook.com>
2025-12-17 03:04:48 +0100

    fix article image

--- src/app/_common/article/ArticleBleedImage.tsx
+++ src/app/_common/article/ArticleBleedImage.tsx
@@ -10,7 +10,7 @@ function ArticleBleedImage(props: ArticleBleedImageProps) {
     if (props.figcaption) {
         return (
             <figure className="article-image-figure mb-12">
-                <Bleed marginInline={{ xs: "space-0", sm: "space-0", md: "space-128" }} className="mb-2 image-wrapper">
+                <Bleed marginInline={{ xs: "space-0", sm: "space-0", md: "space-96" }} className="mb-2 image-wrapper">
                     <Image {...props} className={cn("article-image", props.className)} fill quality={90} />
 
                     <BodyLong size="small" as="figcaption">
@@ -22,7 +22,7 @@ function ArticleBleedImage(props: ArticleBleedImageProps) {
     }
 
     return (
-        <Bleed marginInline={{ xs: "space-0", sm: "space-0", md: "space-128" }} className="mb-8 image-wrapper">
+        <Bleed marginInline={{ xs: "space-0", sm: "space-0", md: "space-96" }} className="mb-8 image-wrapper">
             <Image {...props} className={cn("article-image", props.className)} fill quality={90} />
         </Bleed>
     );
