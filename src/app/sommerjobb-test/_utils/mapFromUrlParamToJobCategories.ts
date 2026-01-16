b5ef935db70e1d53bb5b8725df433720033ce2cc
Minh Ha Do <minh.ha.do@nav.no>
2026-01-16 14:15:45 +0100

    wip duplicate summer job page for AI tag

---
+++ src/app/sommerjobb-test/_utils/mapFromUrlParamToJobCategories.ts
@@ -0,0 +1,16 @@
+import { SOMMERJOBB_CATEGORIES } from "@/app/sommerjobb/_utils/searchKeywords";
+
+function removeDuplicatesFromArray(array: string[]) {
+    return [...new Set(array)];
+}
+
+export default function mapFromUrlParamToJobCategories(params: string[]): string[] {
+    const allJobCategories = params.map((param: string) => {
+        const foundCategory = SOMMERJOBB_CATEGORIES.find((c) => c.label === param);
+        if (foundCategory) {
+            return foundCategory.values;
+        }
+        return [];
+    });
+    return removeDuplicatesFromArray(allJobCategories.flat());
+}
