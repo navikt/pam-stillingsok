f5787024dc4716025badb96f668adaae5a19bec4
Christoffer Karlsson <christoffer.karlsson@nav.no>
2026-02-03 14:06:56 +0100

    Verify UUID

--- src/app/stillinger/stilling/[id]/superrask-soknad/_actions/resendVerificationEmail.ts
+++ src/app/stillinger/stilling/[id]/superrask-soknad/_actions/resendVerificationEmail.ts
@@ -1,8 +1,15 @@
 "use server";
 
 import { getDefaultHeaders } from "@/app/stillinger/_common/utils/fetch";
+import { validate as uuidValidate } from "uuid";
 
 export async function resendVerificationEmail(applicationId: string): Promise<{ success: boolean }> {
+    if (!uuidValidate(applicationId)) {
+        return {
+            success: false,
+        };
+    }
+
     try {
         const headers = await getDefaultHeaders();
         const response = await fetch(
