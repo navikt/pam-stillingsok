bffe64d45c908e541da6c6edc8258772e4fcc46a
Steffen Aas <steffen.aas@outlook.com>
2026-02-20 11:27:19 +0100

    Fjern 90 fra next images qualities

--- next.config.mjs
+++ next.config.mjs
@@ -50,7 +50,7 @@ const baseConfig = {
         unoptimized: process.env.NEXT_PUBLIC_DISABLE_IMAGE_OPTIMIZATION === "true" ? true : false,
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-        qualities: [75, 90],
+        qualities: [25, 50, 75],
         path: "/_next/image",
         loader: "default",
     },
