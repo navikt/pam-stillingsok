73f0ace51891a1c704651d77c966fb3f65530e33
Steffen Aas <steffen.aas@outlook.com>
2026-03-26 10:24:30 +0100

    sjekket inn manglende d.ts fil

---
+++ src/types/theme.d.ts
@@ -0,0 +1,6 @@
+import type {} from "@navikt/core/react/types/theme";
+declare module "@navikt/ds-react/types/theme" {
+    export interface CustomAkselColor {
+        "ung-link-card-blue": never;
+    }
+}
