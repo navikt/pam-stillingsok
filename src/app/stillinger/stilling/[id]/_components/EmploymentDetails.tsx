0c6ed5fe03b131f08984a303f7e72b401fd833a8
otenav <29566394+otenav@users.noreply.github.com>
2026-04-16 10:40:45 +0200

    Endrer oppsett for hjemmekontor

--- src/app/stillinger/stilling/[id]/_components/EmploymentDetails.tsx
+++ src/app/stillinger/stilling/[id]/_components/EmploymentDetails.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useMemo, useState } from "react";
 import { BodyLong, Button, Heading, HelpText, HStack, Label, Tag } from "@navikt/ds-react";
 import "./AdDescriptionList.css";
