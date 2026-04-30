388a4c8e60774963912277aa8843e733f3354f69
otenav <29566394+otenav@users.noreply.github.com>
2026-04-30 11:09:35 +0200

    FIx etter review

--- src/app/ung/artikler/husk-dette-nar-du-bruker-ki-i-soknaden-din/page.tsx
+++ src/app/ung/artikler/husk-dette-nar-du-bruker-ki-i-soknaden-din/page.tsx
@@ -1,13 +1,13 @@
 import { PageInfo } from "@/app/(artikler)/pageInfoTypes";
 import { Metadata } from "next";
 import { buildPageMetadata } from "@/app/(artikler)/buildPageMetadata";
-import HuskDetteNarDuBrukerKIISoknadenDin from "@/app/ung/artikler/husk-dette-nar-du-bruker-ki-i-soknaden-din/HuskDetteNarDuBrukerKIISoknadenDin";
+import ArticlePage from "@/app/ung/artikler/husk-dette-nar-du-bruker-ki-i-soknaden-din/ArticlePage";
 
 const pageInfo: PageInfo = {
     title: "Dette må du huske når du bruker KI i søknaden din",
     language: "nb",
     proofread: true,
-    category: "jobseeker-guides",
+    category: "ung",
     description:
         "Mange bruker kunstig intelligens (KI) når de skal søke jobb, for eksempel Copilot og ChatGPT. Det kan være nyttig, men vi må som alltid bruke KI på en bevisst og kritisk måte.",
     updatedAt: "2026-04-30",
@@ -19,5 +19,5 @@ export const metadata: Metadata = buildPageMetadata({
 });
 
 export default function Page() {
-    return <HuskDetteNarDuBrukerKIISoknadenDin meta={pageInfo} />;
+    return <ArticlePage meta={pageInfo} />;
 }
