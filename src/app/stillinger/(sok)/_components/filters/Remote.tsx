33bc1fdcdbceabf7d23db978f1baed32a283abb9
Minh Ha Do <minh.ha.do@nav.no>
2026-05-12 09:07:07 +0200

    Use remote_facet for filter aggregation

--- src/app/stillinger/(sok)/_components/filters/Remote.tsx
+++ src/app/stillinger/(sok)/_components/filters/Remote.tsx
@@ -1,7 +1,6 @@
 import { BodyShort, Checkbox, CheckboxGroup } from "@navikt/ds-react";
 import type React from "react";
 import type { FilterAggregation } from "@/app/stillinger/_common/types/FilterAggregations";
-import getRemoteWorkValue from "@/app/stillinger/_common/utils/getRemoteWorkValue";
 import useQuery from "@/app/stillinger/(sok)/_components/QueryProvider";
 import mergeCount from "@/app/stillinger/(sok)/_components/utils/mergeCount";
 import moveFilterToBottom from "@/app/stillinger/(sok)/_components/utils/moveFilterToBottom";
@@ -39,7 +38,7 @@ export default function Remote({ initialValues, updatedValues }: RemoteProps) {
         >
             {values.map((item) => (
                 <Checkbox name="remote[]" key={item.key} value={item.key} onChange={handleChange}>
-                    {`${getRemoteWorkValue(item.key)} (${item.count})`}
+                    {`${item.key} (${item.count})`}
                 </Checkbox>
             ))}
         </CheckboxGroup>
