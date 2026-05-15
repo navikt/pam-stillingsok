465851b8a2193f2899a79202dddcf1dd5104d157
Christoffer Karlsson <christoffer.karlsson@nav.no>
2026-05-15 08:16:29 +0200

    Show link to my applications after verifying e-mail, if it was sent logged in

--- src/app/superrask-soknad/bekreft/page.tsx
+++ src/app/superrask-soknad/bekreft/page.tsx
@@ -1,10 +1,7 @@
-import { BodyLong, Heading } from "@navikt/ds-react";
-import { PageBlock } from "@navikt/ds-react/Page";
-import GiveFeedback from "@/app/stillinger/stilling/[id]/superrask-soknad/_components/GiveFeedback";
 import { notFound } from "next/navigation";
 import { Metadata } from "next";
 import { confirmEmail } from "./confirmEmail";
-import SoekFlereJobberKnapp from "@/app/superrask-soknad/bekreft/SoekFlereJobberKnapp";
+import EpostBekreftetPage from "@/app/superrask-soknad/bekreft/EpostBekreftetPage";
 
 export const metadata: Metadata = {
     title: "Superrask søknad",
@@ -32,28 +29,7 @@ export default async function Page(props: PageProps) {
         throw Error("Invalid JWE token format");
     }
 
-    await confirmEmail(token);
+    const { applicationSentByLoggedInApplicant } = await confirmEmail(token);
 
-    return (
-        <div className="mb-16 mt-16">
-            <PageBlock width="text" gutters>
-                <Heading level="1" size="large" spacing>
-                    Din e-post er bekreftet og søknaden er sendt til arbeidsgiver
-                </Heading>
-                <BodyLong spacing>
-                    Du vil straks få en bekreftelse på din e-post. Ønsker du å trekke din søknad, finner du informasjon
-                    om dette i e-posten.
-                </BodyLong>
-                <Heading level="2" spacing size="medium">
-                    Hva skjer nå?
-                </Heading>
-                <BodyLong className="mb-8">
-                    Bedriften vil vurdere din søknad og ta kontakt dersom de synes du passer for jobben. Du får beskjed
-                    på e-post så fort bedriften har gjort en vurdering.
-                </BodyLong>
-                <GiveFeedback />
-                <SoekFlereJobberKnapp />
-            </PageBlock>
-        </div>
-    );
+    return <EpostBekreftetPage applicationSentByLoggedInApplicant={applicationSentByLoggedInApplicant} />;
 }
