07fea3a794a97e7ad95aebbacab085fa53bfeda1
Khiem Diep <khiem.harald.tu.diep@nav.no>
2026-08-21 11:45:27 +0200

    fix unit test

--- src/app/stillinger/(sok)/_utils/query.test.ts
+++ src/app/stillinger/(sok)/_utils/query.test.ts
@@ -49,8 +49,8 @@ describe("createQuery", () => {
     });
 
     test("Should parse url version param", () => {
-        const query = createQuery({ from: "6" });
-        expect(query.v).toEqual(6);
+        const query = createQuery({ v: "5" });
+        expect(query.v).toEqual(5);
     });
 
     test("Should return default url version param if provided value is invalid", () => {
